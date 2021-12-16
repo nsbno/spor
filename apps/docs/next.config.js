654b9c8c7726c3ed760618124a19ae2b17375121
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-12-16 22:41:47 +0100

    Figure out dependencies to transpile at runtime

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,5 +1,17 @@
-const withTM = require("next-transpile-modules")(["@vygruppen/spor-react"]);
+const fs = require("fs");
+const path = require("path");
+
+const withTM = require("next-transpile-modules")(
+  getPackagesToTranspileOnTheFly()
+);
 
 module.exports = withTM({
   reactStrictMode: true,
 });
+
+function getPackagesToTranspileOnTheFly() {
+  return fs
+    .readdirSync(path.resolve(__dirname, "../../packages"))
+    .filter((pkg) => pkg.startsWith("spor-"))
+    .map((pkg) => `@vygruppen/${pkg}`);
+}

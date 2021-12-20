acb350df5978a7f3a1da8eae6176535b31869f31
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-12-20 14:59:43 +0100

    Downgrade to React 17
    Turns out, Chakra, or it's dependency emotion, does not support React 18 yet

--- apps/docs/pages/_document.tsx
+++ apps/docs/pages/_document.tsx
@@ -1,20 +1,16 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 import { FontPreloading } from "../features/font-loading/FontPreloading";
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="nb-NO">
-        <Head>
-          <FontPreloading />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="nb-NO">
+      <Head>
+        <FontPreloading />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;

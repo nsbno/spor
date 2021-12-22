eb9211029d3e47d4ac51a45cbbcc7d5ca14aed71
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-12-22 19:47:22 +0100

    Add support for layouts

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,10 +1,23 @@
 import { SporProvider } from "@vygruppen/spor-react";
+import type { NextPage } from "next";
 import { AppProps } from "next/app";
+import type { ReactElement, ReactNode } from "react";
+import { BaseLayout } from "../features/layouts/base-layout/BaseLayout";
 
-export default function SporDocsApp({ Component, pageProps }: AppProps) {
-  return (
-    <SporProvider>
-      <Component {...pageProps} />
-    </SporProvider>
-  );
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function SporDocsApp({
+  Component,
+  pageProps,
+}: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || getDefaultLayout;
+  return <SporProvider>{getLayout(<Component {...pageProps} />)}</SporProvider>;
 }
+
+const getDefaultLayout = (page: any) => <BaseLayout>{page}</BaseLayout>;

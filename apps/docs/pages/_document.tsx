487f45060615e68e77869d05d29801dff2223e5d
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-12-22 19:51:05 +0100

    Revert to old school _document style for now

--- apps/docs/pages/_document.tsx
+++ apps/docs/pages/_document.tsx
@@ -1,16 +1,18 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import { FontPreloading } from "../features/font-loading/FontPreloading";
 
-export default function Document() {
-  return (
-    <Html lang="nb-NO">
-      <Head>
-        <FontPreloading />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang="nb-NO">
+        <Head>
+          <FontPreloading />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }

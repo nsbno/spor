58fa4b6947ee38d029d664eb9b21ea001e8ae3b7
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-01-11 13:42:44 +0100

    Add a test route to develop new components
    This won't be linked to anywhere, and will just be an internal test tool for us

---
+++ apps/docs/pages/test.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "@chakra-ui/react";
+
+export default function TestPage() {
+  return (
+    <Center height="calc(100vh - 120px)">
+      <Text>Test stuff here</Text>
+    </Center>
+  );
+}

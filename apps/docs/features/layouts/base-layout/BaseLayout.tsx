c6ee939eff717f1f27b2c212ad016b3b46a10dd8
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-01-11 21:51:32 +0100

    Remove console log

--- apps/docs/features/layouts/base-layout/BaseLayout.tsx
+++ apps/docs/features/layouts/base-layout/BaseLayout.tsx
@@ -1,11 +1,10 @@
-import { Box, Flex, useTheme } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { SiteHeader } from "../../site-header/SiteHeader";
 
 type BaseLayoutProps = {
   children: React.ReactNode;
 };
 export const BaseLayout = ({ children }: BaseLayoutProps) => {
-  console.log("theme", useTheme());
   return (
     <Flex flexDirection="column" minHeight="100vh">
       <SiteHeader colorScheme="dark" />

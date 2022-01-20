409bebc74d2c3ecc41b68c086b908784a3446285
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-01-20 22:28:09 +0100

    Initializing Remix and deleting the next.js version

---
+++ apps/docs/app/root.tsx
@@ -0,0 +1,38 @@
+import { Language, SporProvider } from "@vygruppen/spor-react";
+import type { MetaFunction } from "remix";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "remix";
+import { BaseLayout } from "./features/layouts/base-layout/BaseLayout";
+
+export const meta: MetaFunction = () => {
+  return { title: "Spor - The design system of Vy" };
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <SporProvider language={Language.English}>
+          <BaseLayout>
+            <Outlet />
+          </BaseLayout>
+        </SporProvider>
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
--- apps/docs/pages/resources/getting-started.tsx
+++
@@ -1,7 +0,0 @@
-import { Heading } from "@chakra-ui/react";
-import { DocsLayout } from "../../features/layouts/docs-layout/DocsLayout";
-
-export default function GettingStartedPage() {
-  return <Heading as="h1">Getting started</Heading>;
-}
-GettingStartedPage.getLayout = (page: any) => <DocsLayout>{page}</DocsLayout>;

67fd2343563ba0c3879c7e761d9c5dcc5a420194
Ella Krossen <ellakrossen@Ellas-MacBook-Pro.local>
2022-01-24 10:15:39 +0100

    Add section card body

--- apps/docs/features/section-card/SectionCard.tsx
+++ apps/docs/features/section-card/SectionCard.tsx
@@ -1,25 +1,59 @@
-import { Link, Box, Stack, Text } from "@chakra-ui/react";
-import { Icon } from "@chakra-ui/react";
+import {
+  Box,
+  chakra,
+  SimpleGrid,
+  Stat,
+  StatLabel,
+  StatNumber,
+  useColorModeValue,
+  Stack,
+  Flex,
+  Text,
+  Icon,
+} from "@chakra-ui/react";
 
-type SectionCardProps = {
-  href: string;
-  icon: React.ReactNode;
+import { ReactElement } from "react";
+import { ColorScheme } from "../color-scheme/ColorSchemeContext";
+import { extendTheme } from "@chakra-ui/react";
+
+interface SectionCardProps {
   title: string;
-  description: string;
-};
+  text: string;
+  icon: ReactElement;
+  bgColor: string;
+}
 
-export const SectionCard = () => {
+export const SectionCard = ({
+  title,
+  text,
+  icon,
+
+  bgColor,
+}: SectionCardProps) => {
   return (
-    <Link>
-      <Box padding="lg">
-        <Box paddingBottom="lg">{<Icon />}</Box>
-        <Stack spacing="sm">
-          <Text as="h2" fontWeight="bold">
-            Test
-          </Text>
-          {/*  <Text>{description}</Text> */}
-        </Stack>
+    <Box
+      px={{ base: 3, md: 8 }}
+      py={"5"}
+      shadow={"sm"}
+      border={"1px solid"}
+      borderColor={"alias.darkGrey"}
+      rounded={"lg"}
+      backgroundColor="alias.white"
+    >
+      <Box
+        w={16}
+        h={16}
+        align={"center"}
+        justify={"center"}
+        color={"white"}
+        rounded={"full"}
+        bg={bgColor}
+        mb={1}
+      >
+        {icon}
       </Box>
-    </Link>
+      <Text fontWeight={600}>{title}</Text>
+      <Text color={"gray.600"}>{text}</Text>
+    </Box>
   );
 };

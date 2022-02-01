a41bace689849bd8f205bb1a30cbf88db5a615d4
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-02-01 15:31:02 +0100

    Make sure you pass in a color scheme when the variant is filled

--- packages/spor-card-react/src/Card.tsx
+++ packages/spor-card-react/src/Card.tsx
@@ -2,12 +2,14 @@ import { As, BoxProps, forwardRef, useStyleConfig } from "@chakra-ui/react";
 import { Box } from "@vygruppen/spor-layout-react";
 import React from "react";
 
-type CardProps = BoxProps & {
-  variant: "elevated" | "filled" | "outlined";
-  children: React.ReactNode;
-  /** Should only be used with variant="filled" */
-  colorScheme?: "blue" | "green" | "grey";
-};
+type CardProps = BoxProps &
+  (
+    | {
+        variant: "elevated" | "outlined";
+        children: React.ReactNode;
+      }
+    | { variant: "filled"; colorScheme: "blue" | "green" | "grey" }
+  );
 export const Card = forwardRef<CardProps, As<any>>(
   ({ variant, colorScheme = "grey", children, ...props }, ref) => {
     const styles = useStyleConfig("Card", {

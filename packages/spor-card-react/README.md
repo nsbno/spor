349511206b8e0b6f9e7ff49eef3d76c579189fe1
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-02-01 15:37:35 +0100

    Add some basic documentation to card components

--- packages/spor-card-react/README.md
+++ packages/spor-card-react/README.md
@@ -11,10 +11,19 @@ $ npm install @vygruppen/spor-card-react
 ## Usage
 
 ```tsx
-import { } from "@vygruppen/spor-card-react";
+import { Card } from "@vygruppen/spor-card-react";
 ```
 
-TODO: Add usage description
+Cards come in three different variants - `filled`, `outlined` and `elevated`.
+If you specify the `filled` variant, you need to specify a `colorScheme` as well. The available color schemes are `blue`, `green` and `grey`.
+
+```tsx
+<Card variant="elevated">I'm an elevated card</Card>
+<Card variant="outlined">I'm an outlined card</Card>
+<Card variant="filled" colorScheme="blue">I'm a filled card</Card>
+```
+
+You can also pass any style props you want, like padding or borderRadius.
 
 ## Development
 
--- packages/spor-card-react/src/Card.tsx
+++ packages/spor-card-react/src/Card.tsx
@@ -10,6 +10,23 @@ type CardProps = BoxProps &
       }
     | { variant: "filled"; colorScheme: "blue" | "green" | "grey" }
   );
+/**
+ * Cards come in three different variants - `filled`, `outlined` and `elevated`.
+ * If you specify the `filled` variant, you need to specify a `colorScheme` as well. The available color schemes are `blue`, `green` and `grey`.
+ * 
+```tsx
+<Card variant="elevated">
+  I'm an elevated card
+</Card>
+<Card variant="outlined">
+  I'm an outlined card
+</Card>
+<Card variant="filled" colorScheme="blue">
+  I'm a filled card
+</Card>
+```
+ * You can also pass any style props you want, like padding or borderRadius.
+ */
 export const Card = forwardRef<CardProps, As<any>>(
   ({ variant, colorScheme = "grey", children, ...props }, ref) => {
     const styles = useStyleConfig("Card", {

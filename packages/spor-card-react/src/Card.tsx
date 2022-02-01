fd78cc4365bf51442c4f9592718673562d490106
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-02-01 15:24:50 +0100

    Add initial version of Card component

--- packages/spor-card-react/src/Card.tsx
+++ packages/spor-card-react/src/Card.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import { As, BoxProps, forwardRef, useStyleConfig } from "@chakra-ui/react";
+import { Box } from "@vygruppen/spor-layout-react";
+import React from "react";
 
-type CardProps = {};
-export const Card = (props: CardProps) => {
-  return <div>Hi there</div>;
+type CardProps = BoxProps & {
+  variant: "elevated" | "filled" | "outlined";
+  children: React.ReactNode;
+  /** Should only be used with variant="filled" */
+  colorScheme?: "blue" | "green" | "grey";
 };
+export const Card = forwardRef<CardProps, As<any>>(
+  ({ variant, colorScheme = "grey", children, ...props }, ref) => {
+    const styles = useStyleConfig("Card", {
+      variant: variant as any,
+      colorScheme,
+    });
+    return (
+      <Box __css={styles} {...props} ref={ref}>
+        {children}
+      </Box>
+    );
+  }
+);

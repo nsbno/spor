99241cd00d80aba481c23a38213aa67d1cf37b36
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-02-03 15:22:34 +0100

    Fix build error

--- packages/spor-modal-react/src/Modal.tsx
+++ packages/spor-modal-react/src/Modal.tsx
@@ -4,14 +4,12 @@ export {
   ModalCloseButton,
   ModalContent,
   ModalFooter,
-  ModalHeader,
   ModalOverlay,
 } from "@chakra-ui/react";
 export type {
   ModalBodyProps,
   ModalContentProps,
   ModalFooterProps,
-  ModalHeaderProps,
   ModalOverlayProps,
   ModalProps,
 } from "@chakra-ui/react";

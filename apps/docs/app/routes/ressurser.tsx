c1503ca3766d9d87253e0b4fef43588cc9c806fd
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-02-07 23:56:55 +0100

    Use badge instead of locally sourced label

--- apps/docs/app/routes/ressurser.tsx
+++ apps/docs/app/routes/ressurser.tsx
@@ -1,31 +1,19 @@
-import { Flex, FlexProps } from "@vygruppen/spor-react";
+import { Badge } from "@vygruppen/spor-react";
 import { Outlet } from "remix";
 import { DocsLayout } from "~/features/layouts/docs-layout/DocsLayout";
 
 export default function ResourcesPage() {
   return (
     <DocsLayout>
-      <Label backgroundColor="alias.coralGreen" color="alias.darkGrey" mb={1}>
+      <Badge
+        variant="solid"
+        backgroundColor="alias.coralGreen"
+        color="alias.darkGrey"
+        mb={1}
+      >
         Ressurser
-      </Label>
+      </Badge>
       <Outlet />
     </DocsLayout>
-  );
-}
-
-type WithRequired<T, U extends keyof T> = Omit<T, U> & Required<Pick<T, U>>;
-
-type LabelProps = WithRequired<FlexProps, "backgroundColor" | "color">;
-function Label(props: LabelProps) {
-  return (
-    <Flex
-      display="inline-flex"
-      borderRadius="sm"
-      textStyle="xs"
-      fontWeight="bold"
-      height="24px"
-      px={2}
-      {...props}
-    />
   );
 }

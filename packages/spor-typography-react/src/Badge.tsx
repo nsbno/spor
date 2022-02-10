bed34b9b68dd0f11bfbbb688e90dff49b4525a84
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-02-10 14:25:00 +0100

    Add support for all color schemes and sizes for badges and code

--- packages/spor-typography-react/src/Badge.tsx
+++ packages/spor-typography-react/src/Badge.tsx
@@ -7,13 +7,51 @@ import {
 import React from "react";
 
 export type BadgeProps = Omit<ChakraBadgeProps, "variant" | "colorScheme"> & {
+  /**
+   * The color scheme of the badge.
+   */
+  colorScheme?:
+    | "yellow"
+    | "light-yellow"
+    | "red"
+    | "green"
+    | "orange"
+    | "blue"
+    | "grey"
+    | "white";
+  /** The design variant – "solid" by default.
+   *
+   * Can be specified as `outline` to render a border around the badge. */
   variant?: "solid" | "outline";
-  colorScheme?: "grey" | "teal" | "green" | "blue" | "yellow" | "orange";
+  /** The badge size – "sm" by default.
+   *
+   * Can be specified as `md` to render a larger badge.
+   */
+  size?: "sm" | "md";
 };
 /**
  * Shows some additional information about the component it's used within.
  *
- * You can specify two different variants - `solid` and `outline`. The default is `solid`, and there are no set rules about when to use what.
+ * You have to specify some content (icons should be placed to the left, if present), and a colorScheme.
+ *
+ * ```tsx
+ * <Badge colorScheme="green">Hello</Badge>
+ * ```
+ *
+ * You can specify the `size` (`sm` by default) and the `variant` (`solid` by default) props if required.
+ *
+ *
+ * ```tsx
+ * <Badge colorScheme="red" size="md" variant="outline">Special</Badge>
+ * ```
+ *
+ * If you want an icon, just pass it as a child:
+ *
+ * ```tsx
+ * <Badge colorScheme="blue">
+ *   <InformationOutline24Icon /> Information
+ * </Badge>
+ * ```
  */
 export const Badge = forwardRef<BadgeProps, As<any>>((props, ref) => (
   <ChakraBadge {...props} ref={ref} />

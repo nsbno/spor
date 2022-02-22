e6bea9d9dde49f4da926da49d3cf787ca9e6137c
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-02-22 14:48:33 +0100

    Fix broken link

--- apps/docs/app/features/routes/index/ActionLinks.tsx
+++ apps/docs/app/features/routes/index/ActionLinks.tsx
@@ -27,7 +27,7 @@ type LinkItem = {
 // These are the links that are rendered in the action links section.
 const links: LinkItem[] = [
   {
-    to: "/ressurser/kom-i-gang",
+    to: "/kom-i-gang",
     title: "Kom i gang",
     description: "Sett opp Spor i ditt prosjekt på få minutter",
     icon: HomeOutline30Icon,

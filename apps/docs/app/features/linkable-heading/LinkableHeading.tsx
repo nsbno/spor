5dc804dbf26165b4be45784f7721335e8febb0f3
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-02-22 12:55:00 +0100

    Add a table of contents feature

---
+++ apps/docs/app/features/linkable-heading/LinkableHeading.tsx
@@ -0,0 +1,35 @@
+import { Flex, useClipboard } from "@chakra-ui/react";
+import {
+  CopyOutline24Icon,
+  Heading,
+  HeadingProps,
+  IconButton,
+  SuccessOutline24Icon,
+} from "@vygruppen/spor-react";
+import { useLocation } from "react-router-dom";
+import { slugify } from "~/utils/stringUtils";
+
+type LinkableHeadingProps = HeadingProps & {};
+export const LinkableHeading = (props: LinkableHeadingProps) => {
+  const location = useLocation();
+  const id = props.id || slugify(props.children as string);
+  const { onCopy, hasCopied } = useClipboard(`/${location.pathname}#${id}`);
+  return (
+    <Flex position="relative" alignItems="center" data-group>
+      <Heading {...props} id={id} />
+      <IconButton
+        aria-label="Kopiér"
+        onClick={onCopy}
+        variant="ghost"
+        icon={hasCopied ? <SuccessOutline24Icon /> : <CopyOutline24Icon />}
+        color="alias.darkGrey"
+        visibility="hidden"
+        opacity="0"
+        ml={2}
+        transitionDuration="fast"
+        transitionProperty="common"
+        _groupHover={{ visibility: "visible", opacity: 1 }}
+      />
+    </Flex>
+  );
+};

89c50cc28d4f3d741fbfe7fe47874dbf8a3421e7
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-03-01 14:12:22 +0100

    Implement the kom i gang route as a sanity based route

---
+++ apps/docs/app/routes/$category.$slug.tsx
@@ -0,0 +1,47 @@
+import { PortableText } from "@portabletext/react";
+import { Badge, Box, Heading, Stack } from "@vygruppen/spor-react";
+import { Fragment } from "react";
+import { LoaderFunction, useLoaderData } from "remix";
+import { DocsLayout } from "~/features/layouts/docs-layout/DocsLayout";
+import { getClient } from "~/utils/sanity/client.server";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const initialData = await getClient().fetch(
+    `*[_type == "article" && category->slug.current == $categorySlug && slug.current == $articleSlug] {
+      title,
+      "slug": slug.current,
+      category->{
+        title,
+        "slug": slug.current
+      },
+      content
+    }`,
+    { categorySlug: params.category, articleSlug: params.slug }
+  );
+  return { initialData };
+};
+
+export default function GettingStartedPage() {
+  const { initialData } = useLoaderData();
+  return (
+    <DocsLayout>
+      {initialData?.map((data: any) => (
+        <Fragment key={data._id}>
+          {data.category?.title && (
+            <Badge colorScheme="green" mb={1}>
+              {data.category?.title}
+            </Badge>
+          )}
+          <Box>
+            <Heading as="h1" textStyle="xl-display" mb={2}>
+              {data.title}
+            </Heading>
+            <Stack spacing={6}>
+              <PortableText value={data.content} />
+            </Stack>
+          </Box>
+        </Fragment>
+      ))}
+    </DocsLayout>
+  );
+}

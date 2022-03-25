d46ecddf910b517c11f64a34628e855b398c11f7
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-03-25 14:27:31 +0100

    First take

--- packages/spor-theme-react/src/components/tabs.ts
+++ packages/spor-theme-react/src/components/tabs.ts
@@ -3,45 +3,57 @@ import type {
   PartsStyleFunction,
   PartsStyleInterpolation,
   PartsStyleObject,
+  StyleFunctionProps,
   SystemStyleFunction,
   SystemStyleObject,
 } from "@chakra-ui/theme-tools";
-import { getColor, mode } from "@chakra-ui/theme-tools";
 
 const baseStyleRoot: SystemStyleFunction = (props) => {
-  const { orientation } = props;
   return {
-    display: orientation === "vertical" ? "flex" : "block",
+    display: "flex",
+    flexDirection: "column",
   };
 };
 
 const baseStyleTab: SystemStyleFunction = (props) => {
-  const { isFitted } = props;
-
   return {
-    flex: isFitted ? 1 : undefined,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     transitionProperty: "common",
     transitionDuration: "normal",
     _focus: {
       zIndex: 1,
       boxShadow: "outline",
     },
+    _selected: {
+      backgroundColor: "salmon",
+    },
   };
 };
 
 const baseStyleTablist: SystemStyleFunction = (props) => {
-  const { align = "start", orientation } = props;
-
-  const alignments = {
-    end: "flex-end",
-    center: "center",
-    start: "flex-start",
-  };
-
   return {
-    justifyContent: alignments[align],
-    flexDirection: orientation === "vertical" ? "column" : "row",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    ...getColorSchemeProps(props),
   };
+};
+
+const getColorSchemeProps = (props: StyleFunctionProps) => {
+  switch (props.colorScheme) {
+    case "dark":
+      return { backgroundColor: "alias.darkTeal", color: "alias.white" };
+    case "light":
+      return { backgroundColor: "alias.white", color: "alias.darkGrey" };
+    case "green":
+      return { backgroundColor: "alias.mint", color: "alias.darkTeal" };
+    case "grey":
+      return { backgroundColor: "alias.platinum", color: "alias.darkGrey" };
+    default:
+      return {};
+  }
 };
 
 const baseStyleTabpanel: SystemStyleObject = {
@@ -58,152 +70,55 @@ const baseStyle: PartsStyleFunction<typeof parts> = (props) => ({
 const sizes: Record<string, PartsStyleObject<typeof parts>> = {
   sm: {
     tab: {
-      py: 1,
-      px: 4,
-      fontSize: "sm",
+      height: "26px",
+      px: 2,
     },
   },
   md: {
     tab: {
-      fontSize: "md",
-      py: 2,
-      px: 4,
+      height: "30px",
+      px: 2,
     },
   },
   lg: {
     tab: {
-      fontSize: "lg",
-      py: 3,
-      px: 4,
+      height: "36px",
+      px: 2,
+    },
+  },
+  xl: {
+    tab: {
+      height: "46px",
+      px: 3,
     },
   },
 };
 
-const variantLine: PartsStyleFunction<typeof parts> = (props) => {
-  const { colorScheme: c, orientation } = props;
-  const isVertical = orientation === "vertical";
-  const borderProp =
-    orientation === "vertical" ? "borderStart" : "borderBottom";
-  const marginProp = isVertical ? "marginStart" : "marginBottom";
-
-  return {
-    tablist: {
-      [borderProp]: "2px solid",
-      borderColor: "inherit",
-    },
-    tab: {
-      [borderProp]: "2px solid",
-      borderColor: "transparent",
-      [marginProp]: "-2px",
-      _selected: {
-        color: mode(`${c}.600`, `${c}.300`)(props),
-        borderColor: "currentColor",
-      },
-      _active: {
-        bg: mode("gray.200", "whiteAlpha.300")(props),
-      },
-      _disabled: {
-        opacity: 0.4,
-        cursor: "not-allowed",
-      },
-    },
-  };
-};
-
-const variantEnclosed: PartsStyleFunction<typeof parts> = (props) => {
-  const { colorScheme: c } = props;
-  return {
-    tab: {
-      borderTopRadius: "md",
-      border: "1px solid",
-      borderColor: "transparent",
-      mb: "-1px",
-      _selected: {
-        color: mode(`${c}.600`, `${c}.300`)(props),
-        borderColor: "inherit",
-        borderBottomColor: mode(`white`, `gray.800`)(props),
-      },
-    },
-    tablist: {
-      mb: "-1px",
-      borderBottom: "1px solid",
-      borderColor: "inherit",
-    },
-  };
-};
-
-const variantEnclosedColored: PartsStyleFunction<typeof parts> = (props) => {
-  const { colorScheme: c } = props;
-  return {
-    tab: {
-      border: "1px solid",
-      borderColor: "inherit",
-      bg: mode(`gray.50`, `whiteAlpha.50`)(props),
-      mb: "-1px",
-      _notLast: {
-        marginEnd: "-1px",
-      },
-      _selected: {
-        bg: mode("#fff", "gray.800")(props),
-        color: mode(`${c}.600`, `${c}.300`)(props),
-        borderColor: "inherit",
-        borderTopColor: "currentColor",
-        borderBottomColor: "transparent",
-      },
-    },
-    tablist: {
-      mb: "-1px",
-      borderBottom: "1px solid",
-      borderColor: "inherit",
-    },
-  };
-};
-
-const variantSoftRounded: PartsStyleFunction<typeof parts> = (props) => {
-  const { colorScheme: c, theme } = props;
-  return {
-    tab: {
-      borderRadius: "full",
-      fontWeight: "semibold",
-      color: "gray.600",
-      _selected: {
-        color: getColor(theme, `${c}.700`),
-        bg: getColor(theme, `${c}.100`),
-      },
-    },
-  };
-};
-
-const variantSolidRounded: PartsStyleFunction<typeof parts> = (props) => {
-  const { colorScheme: c } = props;
-  return {
-    tab: {
-      borderRadius: "full",
-      fontWeight: "semibold",
-      color: mode("gray.600", "inherit")(props),
-      _selected: {
-        color: mode(`#fff`, "gray.800")(props),
-        bg: mode(`${c}.600`, `${c}.300`)(props),
-      },
-    },
-  };
-};
-
-const variantUnstyled: PartsStyleObject<typeof parts> = {};
+const variantRound: PartsStyleInterpolation<typeof parts> = (props) => ({
+  tablist: {
+    borderRadius: "2xl",
+  },
+  tab: {
+    borderRadius: "50%",
+  },
+});
+const variantSquare: PartsStyleInterpolation<typeof parts> = (props) => ({
+  tablist: {
+    borderRadius: "12px",
+  },
+  tab: {
+    borderRadius: "9px",
+  },
+});
 
 const variants: Record<string, PartsStyleInterpolation<typeof parts>> = {
-  line: variantLine,
-  enclosed: variantEnclosed,
-  "enclosed-colored": variantEnclosedColored,
-  "soft-rounded": variantSoftRounded,
-  "solid-rounded": variantSolidRounded,
-  unstyled: variantUnstyled,
+  round: variantRound,
+  square: variantSquare,
 };
 
 const defaultProps = {
   size: "md",
-  variant: "line",
-  colorScheme: "blue",
+  variant: "round",
 };
 
 export default {

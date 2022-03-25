b437ba2104bc782f312bafe4e7f00d2e4bb431a4
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-03-25 09:27:38 +0100

    Fix a focus trap by adding escape and shift-escape support

--- apps/docs/app/features/code-block/CodeBlock.tsx
+++ apps/docs/app/features/code-block/CodeBlock.tsx
@@ -1,6 +1,7 @@
-import { DarkMode, useClipboard } from "@chakra-ui/react";
+import { DarkMode, forwardRef, useClipboard } from "@chakra-ui/react";
 import { Box, BoxProps, Button } from "@vygruppen/spor-react";
 import Highlight, { defaultProps } from "prism-react-renderer";
+import { useRef } from "react";
 import { theme } from "./codeTheme";
 
 type CodeBlockProps = Omit<BoxProps, "children"> & {
@@ -46,6 +47,19 @@ export const CodeBlockContainer = ({
   code,
   ...props
 }: CodeBlockContainerProps) => {
+  const copyButtonRef = useRef<HTMLButtonElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const handleKeyUp = (e: React.KeyboardEvent) => {
+    console.log(e.key, e.shiftKey);
+    if (e.key === "Escape") {
+      if (e.shiftKey) {
+        const previousElement = getPreviousFocusableElement();
+        previousElement.focus();
+      } else {
+        copyButtonRef.current?.focus();
+      }
+    }
+  };
   return (
     <Box
       borderRadius="sm"
@@ -59,32 +73,51 @@ export const CodeBlockContainer = ({
       zIndex="0"
       maxWidth="calc(100vw - var(--spor-space-4))"
       __css={{ "pre > div": { whiteSpace: "initial", maxWidth: "100%" } }}
+      onKeyUp={handleKeyUp}
+      ref={containerRef}
       {...props}
     >
       <Box width="100%" overflowX="hidden">
         {children}
       </Box>
-      <CopyCodeButton code={code} />
+      <CopyCodeButton ref={copyButtonRef} code={code} />
     </Box>
   );
 };
 
-type CopyCodeButtonProps = { code: string };
-export const CopyCodeButton = ({ code }: CopyCodeButtonProps) => {
-  const { onCopy, hasCopied } = useClipboard(code);
-  return (
-    <DarkMode>
-      <Button
-        variant="additional"
-        size="xs"
-        onClick={onCopy}
-        position="absolute"
-        top={2}
-        right={2}
-        fontFamily="body"
-      >
-        {hasCopied ? "Kopiert" : "Kopiér"}
-      </Button>
-    </DarkMode>
+function getPreviousFocusableElement() {
+  const allTabbableElements = Array.from(
+    document.querySelectorAll(
+      'a, button, input, select, textarea, [contenteditable], [tabindex]:not([tabindex^="-"])'
+    )
   );
-};
+
+  const currentlyFocusedElementIndex = allTabbableElements.findIndex(
+    (el) => el === document.activeElement
+  );
+
+  return allTabbableElements[currentlyFocusedElementIndex - 1] as HTMLElement;
+}
+
+type CopyCodeButtonProps = { code: string };
+export const CopyCodeButton = forwardRef<CopyCodeButtonProps, "button">(
+  ({ code }, ref) => {
+    const { onCopy, hasCopied } = useClipboard(code);
+    return (
+      <DarkMode>
+        <Button
+          variant="additional"
+          size="xs"
+          onClick={onCopy}
+          position="absolute"
+          top={2}
+          right={2}
+          fontFamily="body"
+          ref={ref}
+        >
+          {hasCopied ? "Kopiert" : "Kopiér"}
+        </Button>
+      </DarkMode>
+    );
+  }
+);

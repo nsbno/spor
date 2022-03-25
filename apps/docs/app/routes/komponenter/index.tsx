e548f87efa4d023230923772959cc7164a528b75
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-03-25 13:34:50 +0100

    WIP

--- apps/docs/app/routes/komponenter/index.tsx
+++ apps/docs/app/routes/komponenter/index.tsx
@@ -1,9 +1,46 @@
-import { Box, Heading, Stack, Text } from "@vygruppen/spor-react";
+import {
+  Box,
+  Card,
+  Heading,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@vygruppen/spor-react";
+import { Link, LoaderFunction, useLoaderData } from "remix";
+import { getClient } from "~/utils/sanity/client";
+
+type Data = {
+  _id: string;
+  name: string;
+  slug: string;
+  content: any[];
+  page?: { slug: string };
+};
+
+export const loader: LoaderFunction = async () => {
+  const query = `*[_type == "component"] {
+    _id,
+    name,
+    "slug": slug.current,
+    content,
+    "page": *[_type == "article" && references(^._id)] { 
+      "slug": slug.current 
+    }[0]
+  }`;
+  const components = await getClient().fetch<Data[]>(query);
+
+  if (!components || !components.length) {
+    throw new Response("Not Found", { status: 404 });
+  }
+
+  return components;
+};
 
 export default function ComponentsPage() {
+  const components = useLoaderData<Data[]>();
   return (
     <Box>
-      <Stack spacing={2}>
+      <Stack spacing={2} mb={6}>
         <Heading as="h1" textStyle="xl-display">
           Komponent&shy;oversikt
         </Heading>
@@ -13,6 +50,24 @@ export default function ComponentsPage() {
           komponentene.
         </Text>
       </Stack>
+      <SimpleGrid columns={[1, 2, 3]} gap={3}>
+        {components.map((component) => (
+          <Card
+            key={component._id}
+            as={Link}
+            to={
+              component.page ? `${component.page?.slug}#${component.slug}` : ""
+            }
+            variant="filled"
+            colorScheme="green"
+            p={3}
+          >
+            <Heading as="h2" textStyle="md" fontWeight="bold">
+              &lt;{component.name} /&gt;
+            </Heading>
+          </Card>
+        ))}
+      </SimpleGrid>
     </Box>
   );
 }

57f418248a4b38ca313169c840d01ab9adf7a2c4
Eivind Høiseth-Gilje <eivindgi@gmail.com>
2022-03-30 23:02:08 +0200

    Add tabs variants

---
+++ .changeset/eleven-ants-report.md
@@ -0,0 +1,7 @@
+---
+"@vygruppen/spor-react": minor
+"@vygruppen/spor-tab-react": minor
+"@vygruppen/spor-theme-react": minor
+---
+
+Added tabs component
--- packages/spor-theme-react/src/components/tabs.ts
+++ packages/spor-theme-react/src/components/tabs.ts
@@ -15,6 +15,14 @@ const baseStyleRoot: SystemStyleFunction = (props) => {
   };
 };
 
+const baseStyleTablist: SystemStyleFunction = (props) => {
+  return {
+    display: "flex",
+    alignItems: "center",
+    ...getTablistColorSchemeProps(props),
+  };
+};
+
 const baseStyleTab: SystemStyleFunction = (props) => {
   return {
     display: "flex",
@@ -22,26 +30,62 @@ const baseStyleTab: SystemStyleFunction = (props) => {
     alignItems: "center",
     transitionProperty: "common",
     transitionDuration: "normal",
+    my: 0,
+    mx: 0.5,
+    _first: {
+      mx: 0
+    },
+    _last: {
+      mx: 0
+    },
     _focus: {
       zIndex: 1,
       boxShadow: "outline",
     },
-    _selected: {
-      backgroundColor: "salmon",
-    },
+    ...getTabColorSchemeProps(props)
   };
 };
 
-const baseStyleTablist: SystemStyleFunction = (props) => {
-  return {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    ...getColorSchemeProps(props),
-  };
+const getTabColorSchemeProps = (props: StyleFunctionProps) => {
+  switch (props.colorScheme) {
+    case "dark":
+      return {
+        _selected: { backgroundColor: "alias.white", color: "alias.darkTeal" },
+        _hover: { backgroundColor: "alias.pine", color: "alias.white" },
+        _focus: { backgroundColor: "alias.darkTeal", color: "alias.white", borderColor: "alias.white" },
+        _pressed: { backgroundColor: "alias.celadon", color: "alias.white" },
+        _disabled: { backgroundColor: "alias.darkTeal", color: "alias.white", opacity: "20%" },
+      };
+    case "light":
+      return {
+        _selected: { backgroundColor: "alias.darkTeal", color: "alias.white" },
+        _hover: { backgroundColor: "alias.silver", color: "alias.darkGrey" },
+        _focus: { backgroundColor: "alias.white", color: "alias.darkGrey", borderColor: "alias.greenHaze" },
+        _pressed: { backgroundColor: "alias.mint", color: "alias.darkGrey" },
+        _disabled: { backgroundColor: "alias.white", color: "alias.silver" },
+      };
+    case "green":
+      return {
+        _selected: { backgroundColor: "alias.darkTeal", color: "alias.white" },
+        _hover: { backgroundColor: "alias.coralGreen", color: "alias.darkTeal" },
+        _focus: { backgroundColor: "alias.mint", color: "alias.darkTeal", borderColor: "alias.greenHaze" },
+        _pressed: { backgroundColor: "alias.seaMist", color: "alias.darkTeal" },
+        _disabled: { backgroundColor: "alias.mint", color: "alias.coralGreen" },
+      };
+    case "grey":
+      return {
+        _selected: { backgroundColor: "alias.darkTeal", color: "alias.white" },
+        _hover: { backgroundColor: "alias.silver", color: "alias.darkGrey" },
+        _focus: { backgroundColor: "alias.platinum", color: "alias.darkGrey", borderColor: "alias.greenHaze" },
+        _pressed: { backgroundColor: "alias.lightGrey", color: "alias.darkGrey" },
+        _disabled: { backgroundColor: "alias.platinum", color: "alias.steel" },
+      };
+    default:
+      return {};
+  }
 };
 
-const getColorSchemeProps = (props: StyleFunctionProps) => {
+const getTablistColorSchemeProps = (props: StyleFunctionProps) => {
   switch (props.colorScheme) {
     case "dark":
       return { backgroundColor: "alias.darkTeal", color: "alias.white" };
@@ -69,56 +113,104 @@ const baseStyle: PartsStyleFunction<typeof parts> = (props) => ({
 
 const sizes: Record<string, PartsStyleObject<typeof parts>> = {
   sm: {
+    tablist: {
+      height: "30px",
+      p: '2px',
+    },
     tab: {
       height: "26px",
       px: 2,
+      py: 3,
     },
   },
   md: {
+    tablist: {
+      height: "36px",
+      p: 0.5,
+    },
     tab: {
       height: "30px",
       px: 2,
+      py: 3,
     },
   },
   lg: {
+    tablist: {
+      height: "42px",
+      p: 0.5,
+    },
     tab: {
       height: "36px",
       px: 2,
+      py: 3,
     },
   },
   xl: {
+    tablist: {
+      height: "54px",
+      p: '4px',
+    },
     tab: {
       height: "46px",
-      px: 3,
+      p: 3,
     },
   },
 };
 
-const variantRound: PartsStyleInterpolation<typeof parts> = (props) => ({
+const variantRoundDynamic: PartsStyleInterpolation<typeof parts> = {
+    tablist: {
+      borderRadius: "42px",
+    },
+    tab: {
+      flexGrow: 1,
+      borderRadius: "xl",
+    },
+};
+
+const variantRoundCompact: PartsStyleInterpolation<typeof parts> = {
   tablist: {
-    borderRadius: "2xl",
+    width: "fit-content",
+    justifyContent: "center",
+    borderRadius: "42px",
   },
   tab: {
-    borderRadius: "50%",
+    flexGrow: 0,
+    borderRadius: "xl",
   },
-});
-const variantSquare: PartsStyleInterpolation<typeof parts> = (props) => ({
+};
+
+const variantSquareDynamic: PartsStyleInterpolation<typeof parts> = {
   tablist: {
-    borderRadius: "12px",
+    borderRadius: "sm",
   },
   tab: {
+    flexGrow: 1,
     borderRadius: "9px",
   },
-});
+};
+
+const variantSquareCompact: PartsStyleInterpolation<typeof parts> = {
+  tablist: {
+    width: "fit-content",
+    justifyContent: "center",
+    borderRadius: "sm",
+  },
+  tab: {
+    flexGrow: 0,
+    borderRadius: "9px",
+  },
+};
 
 const variants: Record<string, PartsStyleInterpolation<typeof parts>> = {
-  round: variantRound,
-  square: variantSquare,
+  "round-dynamic": variantRoundDynamic,
+  "round-compact": variantRoundCompact ,
+  "square-dynamic": variantSquareDynamic,
+  "square-compact": variantSquareCompact,
 };
 
 const defaultProps = {
   size: "md",
-  variant: "round",
+  variant: "round-dynamic",
 };
 
 export default {

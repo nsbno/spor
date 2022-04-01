e0cb1b6161b2b1c5065030aa81edbcc328a0cf59
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-04-01 09:25:27 +0200

    Improve the menu navigation

--- apps/docs/app/features/content-menu/MenuItem.tsx
+++ apps/docs/app/features/content-menu/MenuItem.tsx
@@ -1,5 +1,6 @@
+import { forwardRef } from "@chakra-ui/react";
 import { Flex, FlexProps } from "@vygruppen/spor-react";
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "remix";
 
 type MenuItemProps = FlexProps & {
@@ -11,44 +12,110 @@ type MenuItemProps = FlexProps & {
 /**
  * Menu item in the `ContentMenu`, and search result in the `SearchResults`.
  */
-export const MenuItem = ({
-  href,
-  children,
-  isActive,
-  isDisabled,
-  ...rest
-}: MenuItemProps) => (
-  <Flex
-    key={href}
-    as={Link}
-    to={isDisabled ? "" : href}
-    px={2}
-    disabled={isDisabled}
-    _disabled={{
-      pointerEvents: "none",
-      textDecoration: "line-through",
-      color: "alias.osloGrey",
-      "&:hover, &:focus, &:active": {
-        backgroundColor: "transparent",
-        outlineColor: "transparent",
-      },
-    }}
-    fontSize="mobile.xs"
-    borderRadius="sm"
-    alignItems="center"
-    backgroundColor={isActive ? "alias.mint" : "transparent"}
-    _hover={{
-      backgroundColor: "alias.mint",
-    }}
-    _focus={{
-      backgroundColor: "alias.mint",
-      outlineColor: "alias.greenHaze",
-    }}
-    _active={{
-      backgroundColor: "alias.seaMist",
-    }}
-    {...rest}
-  >
-    {children}
-  </Flex>
+export const MenuItem = forwardRef<MenuItemProps, "a">(
+  ({ href, children, isActive, isDisabled, ...rest }, externalRef) => {
+    const internalRef = useRef<HTMLAnchorElement>(null);
+    const handleKeyUp = (e: React.KeyboardEvent) => {
+      if (!internalRef || typeof internalRef === "function") {
+        return;
+      }
+      switch (e.key) {
+        case "ArrowUp": {
+          getPreviousFocusableSibling(internalRef.current)?.focus();
+          break;
+        }
+        case "ArrowDown": {
+          getNextFocusableSibling(internalRef.current)?.focus();
+          break;
+        }
+      }
+    };
+    // Stop up and down arrows from scrolling
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+      if (["ArrowUp", "ArrowDown"].includes(e.key)) {
+        e.preventDefault();
+      }
+    };
+    const linkProps: any = isDisabled
+      ? { as: "a", "aria-disabled": true }
+      : { as: Link, to: href };
+    return (
+      <Flex
+        key={href}
+        {...linkProps}
+        px={2}
+        _disabled={{
+          pointerEvents: "none",
+          textDecoration: "line-through",
+          color: "alias.osloGrey",
+          "&:hover, &:focus, &:active": {
+            backgroundColor: "transparent",
+            outlineColor: "transparent",
+          },
+        }}
+        fontSize="mobile.xs"
+        borderRadius="sm"
+        alignItems="center"
+        backgroundColor={isActive ? "alias.mint" : "transparent"}
+        _hover={{
+          backgroundColor: "alias.mint",
+        }}
+        _focus={{
+          backgroundColor: "alias.mint",
+          outline: "2px solid",
+          outlineColor: "alias.greenHaze",
+        }}
+        _active={{
+          backgroundColor: "alias.seaMist",
+        }}
+        ref={(el) => {
+          if (externalRef) {
+            (externalRef as any).current = el;
+          }
+          (internalRef as any).current = el;
+        }}
+        onKeyUp={handleKeyUp}
+        onKeyDown={handleKeyDown}
+        {...rest}
+      >
+        {children}
+      </Flex>
+    );
+  }
 );
+
+const getNextFocusableSibling = (
+  element: HTMLElement | null
+): HTMLElement | null => {
+  if (!element) {
+    return null;
+  }
+  const nextSibling = element.nextElementSibling as HTMLElement;
+  if (!nextSibling) {
+    return null;
+  }
+  if (isFocusable(nextSibling)) {
+    return nextSibling;
+  }
+  return getNextFocusableSibling(nextSibling);
+};
+
+const getPreviousFocusableSibling = (
+  element: HTMLElement | null
+): HTMLElement | null => {
+  if (!element) {
+    return null;
+  }
+  const previousSibling = element.previousElementSibling as HTMLElement;
+  if (!previousSibling) {
+    return null;
+  }
+  if (isFocusable(previousSibling)) {
+    return previousSibling;
+  }
+  return getPreviousFocusableSibling(previousSibling);
+};
+
+const isFocusable = (element: HTMLElement) => {
+  return element.ariaDisabled !== "true";
+};
--- apps/docs/app/features/content-menu/SearchResults.tsx
+++ apps/docs/app/features/content-menu/SearchResults.tsx
@@ -1,30 +1,37 @@
+import { forwardRef } from "@chakra-ui/react";
 import { Box, Text } from "@vygruppen/spor-react";
-import { matchSorter } from "match-sorter";
-import React from "react";
-import { searchableMenuStructure } from "../content-menu/menuStructure";
 import { MenuItem } from "./MenuItem";
 
-export type SearchResultsProps = { query: string };
-/** Given a query, this view shows any hits in the menu structure */
-export const SearchResults = ({ query }: SearchResultsProps) => {
-  const hits = matchSorter(searchableMenuStructure, query, {
-    keys: ["title", "category", "keywords"],
-  });
-  return (
-    <Box mt={2}>
-      {hits.map((item) => (
-        <MenuItem
-          key={item.href}
-          href={item.href}
-          height={5}
-          isDisabled={!item.isAvailable}
-        >
-          {item.category}: {item.title}
-        </MenuItem>
-      ))}
-      {hits.length === 0 && (
-        <Text textAlign="center">Ingen treff på "{query}"</Text>
-      )}
-    </Box>
-  );
+export type SearchResultsProps = {
+  query: string;
+  hits: {
+    category: string;
+    title: string;
+    href: string;
+    isAvailable?: boolean;
+  }[];
 };
+/** Given a query, this view shows any hits in the menu structure */
+export const SearchResults = forwardRef<SearchResultsProps, "button">(
+  ({ query, hits }, ref) => {
+    const firstFocusableIndex = hits.findIndex((item) => item.isAvailable);
+    return (
+      <Box mt={2}>
+        {hits.map((item, index) => (
+          <MenuItem
+            ref={index === firstFocusableIndex ? ref : undefined}
+            key={item.href}
+            href={item.href}
+            height={5}
+            isDisabled={!item.isAvailable}
+          >
+            {item.category}: {item.title}
+          </MenuItem>
+        ))}
+        {hits.length === 0 && (
+          <Text textAlign="center">Ingen treff på "{query}"</Text>
+        )}
+      </Box>
+    );
+  }
+);

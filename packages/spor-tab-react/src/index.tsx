a486c260ec4648fc698c4d7241c6139c9a9872d3
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-04-04 14:45:41 +0200

    Set the correct Tabs types

--- packages/spor-tab-react/src/index.tsx
+++ packages/spor-tab-react/src/index.tsx
@@ -1,1 +1,9 @@
-export { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+export { Tab, TabList, TabPanel, TabPanels } from "@chakra-ui/react";
+export type {
+  TabListProps,
+  TabPanelProps,
+  TabPanelsProps,
+  TabProps,
+} from "@chakra-ui/react";
+export { Tabs } from "./Tabs";
+export type { TabsProps } from "./Tabs";

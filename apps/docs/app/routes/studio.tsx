599450d2348d745250b9125b6c219a1d0874f189
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-04-05 13:51:56 +0200

    Redirect to the local version if locally

--- apps/docs/app/routes/studio.tsx
+++ apps/docs/app/routes/studio.tsx
@@ -2,5 +2,8 @@ import { LoaderFunction, redirect } from "remix";
 
 // Redirect users to the Studio app.
 export const loader: LoaderFunction = async () => {
+  if (process.env.NODE_ENV === "development") {
+    return redirect("http://localhost:3333");
+  }
   return redirect("https://spor.sanity.studio");
 };

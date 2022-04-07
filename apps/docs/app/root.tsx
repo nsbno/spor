003eb51b3a80e1f652d52d1c2f72d393224911fe
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-04-07 20:47:08 +0200

    Specify that this is a Norwegian site

--- apps/docs/app/root.tsx
+++ apps/docs/app/root.tsx
@@ -123,7 +123,7 @@ const Document = withEmotionCache(
     }, []);
 
     return (
-      <html lang="en">
+      <html lang="nb-no">
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width,initial-scale=1" />

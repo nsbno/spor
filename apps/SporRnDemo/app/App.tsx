0b285dc992d5698b5b1939f4400c290a852e5a4a
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-05-03 14:33:27 +0200

    Bootstrap a basic react-native app
    We need a react-native app to develop and demo the different components. It won't be deployed or published, but should be run when you develop react native components

---
+++ apps/SporRnDemo/app/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+/**
+ * The entry point of the Spor RN demo app
+ */
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Spor</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;

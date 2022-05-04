52dce222941bbafa4c724a23893827c97619b7ea
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-05-04 14:43:34 +0200

    Reformat smoke test

--- apps/SporRnDemo/__tests__/App-test.tsx
+++ apps/SporRnDemo/__tests__/App-test.tsx
@@ -1,14 +1,9 @@
-/**
- * @format
- */
-
-import 'react-native';
-import React from 'react';
-import App from '../app/App';
-
+import React from "react";
+import "react-native";
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from "react-test-renderer";
+import App from "../app/App";
 
-it('renders correctly', () => {
+it("renders correctly", () => {
   renderer.create(<App />);
 });

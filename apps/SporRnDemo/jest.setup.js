ebb4fbca182990366f47de1d2f82db138b68639f
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-05-09 14:10:24 +0200

    Fix broken test setup

---
+++ apps/SporRnDemo/jest.setup.js
@@ -0,0 +1,9 @@
+jest.mock("@shopify/restyle", () => {
+  const RealModule = jest.requireActual("@shopify/restyle");
+  const RN = jest.requireActual("react-native");
+  RealModule.createText = () => RN.Text;
+  RealModule.createBox = () => RN.View;
+  RealModule.createRestyleComponent = (f, c) => c || RN.View;
+  RealModule.ThemeProvider = RN.View;
+  return RealModule;
+});

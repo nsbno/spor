fac4888ceb3cce9927d4f27d05558b4550bfb6cd
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-05-13 14:04:03 +0200

    Remove testing from the test app

--- apps/SporRnDemo/app/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "react-native";
-import renderer from "react-test-renderer";
-import App from "./App";
-
-it("renders correctly", () => {
-  renderer.create(<App />);
-});

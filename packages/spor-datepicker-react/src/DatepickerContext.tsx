c0ac8fc8e1eeac41e5086767016d106d91823917
Eivind Høiseth-Gilje <eivindgi@gmail.com>
2022-05-27 11:30:06 +0200

    Datepicker: Components and logic

---
+++ packages/spor-datepicker-react/src/DatepickerContext.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface DatepickerContextType {
+  focusedDate: Date | null;
+  isDateFocused: (date: Date) => boolean;
+  isDateSelected: (date: Date) => boolean;
+  isDateHovered: (date: Date) => boolean;
+  isDateBlocked: (date: Date) => boolean;
+  isFirstOrLastSelectedDate: (date: Date) => boolean;
+  onDateFocus: (date: Date) => void;
+  onDateHover: (date: Date) => void;
+  onDateSelect: (date: Date) => void;
+}
+
+const datepickerContextDefaultValue: DatepickerContextType = {
+  focusedDate: null,
+  isDateFocused: (date: Date) => false,
+  isDateSelected: (date: Date) => false,
+  isDateHovered: (date: Date) => false,
+  isDateBlocked: (date: Date) => false,
+  isFirstOrLastSelectedDate: (date: Date) => false,
+  onDateFocus: (date: Date) => {},
+  onDateHover: (date: Date) => {},
+  onDateSelect: (date: Date) => {},
+};
+
+export const DatepickerContext = React.createContext(
+  datepickerContextDefaultValue
+);

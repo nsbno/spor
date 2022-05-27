863ca4959994c2fe73993ef5d5873b8c40835582
Eivind Høiseth-Gilje <eivindgi@gmail.com>
2022-05-27 16:31:04 +0200

    Datepicker: style weekday labels

--- packages/spor-theme-react/src/components/datepicker.ts
+++ packages/spor-theme-react/src/components/datepicker.ts
@@ -1,38 +1,47 @@
 import {ComponentMultiStyleConfig} from "@chakra-ui/react";
-import {anatomy} from "@chakra-ui/theme-tools";
+import {anatomy, PartsStyleObject} from "@chakra-ui/theme-tools";
 
-const parts = anatomy("datepciker").parts('month', 'day')
+const parts = anatomy("datepicker").parts('calendar', 'weekdayLabel', 'weekendLabel', 'day')
 
+const baseStyle: PartsStyleObject<typeof parts> = {
+  calendar: {
+  },
+  weekdayLabel: {
+    fontWeight: 'bold',
+    color: 'alias.darkGrey',
+  },
+  weekendLabel : {
+    fontWeight: 'bold',
+    color: 'alias.greenHaze',
+  },
+  day: {
+    backgroundColor: 'alias.white',
+    color: 'alias.darkGrey',
+    borderRadius: '50%',
+    height: '36px',
+    width: '36px',
+    _hover: {
+      borderStyle: 'solid',
+      borderWidth: '2px',
+      borderColor: 'alias.osloGrey',
+    },
+    _focus: {
+      borderStyle: 'solid',
+      borderWidth: '2px',
+      borderColor: 'alias.greenHaze',
+    },
+    _active: {
+      backgroundColor: 'alias.mint'
+    },
+    _selected: {
+      backgroundColor: 'alias.darkTeal'
+    }
+  }
+};
 const Datepicker: ComponentMultiStyleConfig = {
   parts: parts.keys,
   defaultProps: {},
-  baseStyle: {
-    month: {
-    },
-    day: {
-      backgroundColor: 'alias.white',
-      color: 'alias.darkGrey',
-      borderRadius: '50%',
-      height: '36px',
-      width: '36px',
-      _hover: {
-        borderStyle: 'solid',
-        borderWidth: '2px',
-        borderColor: 'alias.osloGrey',
-      },
-      _focus: {
-        borderStyle: 'solid',
-        borderWidth: '2px',
-        borderColor: 'alias.greenHaze',
-      },
-      _active: {
-        backgroundColor: 'alias.mint'
-      },
-      _selected: {
-        backgroundColor: 'alias.darkTeal'
-      }
-    }
-  },
+  baseStyle: baseStyle,
   sizes: {},
   variants: {}
 }

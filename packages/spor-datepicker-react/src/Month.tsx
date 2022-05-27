1600a145a0b91a69c9872c0bffd66856136bfab4
Eivind Høiseth-Gilje <eivindgi@gmail.com>
2022-05-27 16:31:04 +0200

    Datepicker: style weekday labels

--- packages/spor-datepicker-react/src/Month.tsx
+++ packages/spor-datepicker-react/src/Month.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { FirstDayOfWeek, MonthType, useMonth } from "@datepicker-react/hooks";
-import {
-  Button,
-  Grid,
-  GridItem,
-  useStyles,
-} from "@chakra-ui/react";
+import { Button, Grid, GridItem, useStyles } from "@chakra-ui/react";
 import { Card } from "@vygruppen/spor-card-react";
 import { Text } from "@vygruppen/spor-typography-react";
 import { Day } from "./Day";
@@ -22,13 +17,13 @@ export const Month: React.VFC<{
 
   const styles = useStyles();
   return (
-    <Card variant="outlined" p={1} __css={styles.month}>
+    <Card variant="outlined" p={1} __css={styles.calendar}>
       <Button>{"<-"}</Button>
       {monthLabel}
       <Button>{"->"}</Button>
       <Grid templateColumns="repeat(7, 1fr)" gap={2}>
-        {weekdayLabels.map((dayLabel) => (
-          <GridItem key={dayLabel}>
+        {weekdayLabels.map((dayLabel, idx) => (
+          <GridItem key={dayLabel} __css={idx > 4 ? styles.weekendLabel : styles.weekdayLabel}>
             <Text>{dayLabel}</Text>
           </GridItem>
         ))}

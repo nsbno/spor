ff254a78bc49f04f040b89b006995ba0013b3376
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-06-03 10:24:15 +0200

    Add inline loaders and full screen loaders

--- packages/spor-loader-react-native/src/index.tsx
+++ packages/spor-loader-react-native/src/index.tsx
@@ -1,2 +1,7 @@
+export * from "./ColorInlineLoader";
 export * from "./ContentLoader";
+export * from "./DarkFullScreenLoader";
+export * from "./DarkInlineLoader";
+export * from "./LightFullScreenLoader";
+export * from "./LightInlineLoader";
 export * from "./Spinner";

67b11b5c1cd41cdbc12984a0d0ee02abcacc6d8b
Eivind Høiseth-Gilje <eivindgi@gmail.com>
2022-06-03 11:59:12 +0200

    Datepicker: support for mobile and desktop variants, small and large size

--- packages/spor-datepicker-react/src/Datepicker.tsx
+++ packages/spor-datepicker-react/src/Datepicker.tsx
@@ -5,17 +5,34 @@ import { DatepickerProvider } from "./DatepickerContext";
 import { Calendar } from "./Calendar";
 import { DateInput } from "./DateInput";
 
-type DatepickerProps = {
-  variant: "sm" | "lg";
+export type DatepickerProps = {
+  size?: "sm" | "lg";
+  variant?: "mobile" | "desktop";
+};
+
+const getDefaultProps: ({
+  size,
+  variant,
+}: DatepickerProps) => Required<DatepickerProps> = ({
+  size,
+  variant,
+}: DatepickerProps) => {
+  if (size && variant) return { size, variant };
+  if (size && !variant)
+    return { size, variant: size === "lg" ? "desktop" : "mobile" };
+  if (variant && !size)
+    return { variant, size: variant === "desktop" ? "lg" : "sm" };
+  return { size: "lg", variant: "desktop" };
 };
 
 const SporDatepicker: React.VFC<DatepickerProps> = (props) => {
-  const styles = useMultiStyleConfig("Datepicker", props);
+  const { size, variant } = getDefaultProps(props);
+  const styles = useMultiStyleConfig("Datepicker", { size, variant });
 
   return (
     <DatepickerProvider>
       <StylesProvider value={styles}>
-        <DateInput />
+        <DateInput variant={variant} />
         <Calendar />
       </StylesProvider>
     </DatepickerProvider>
--- packages/spor-theme-react/src/components/datepicker.ts
+++ packages/spor-theme-react/src/components/datepicker.ts
@@ -1,54 +1,110 @@
-import {ComponentMultiStyleConfig} from "@chakra-ui/react";
-import {anatomy, PartsStyleObject} from "@chakra-ui/theme-tools";
+import { ComponentMultiStyleConfig } from "@chakra-ui/react";
+import {
+  anatomy,
+  PartsStyleInterpolation,
+  PartsStyleObject,
+} from "@chakra-ui/theme-tools";
 
-const parts = anatomy("datepicker").parts('calendar', 'label', 'weekendLabel', 'button')
+const parts = anatomy("datepicker").parts(
+  "input",
+  "calendarButton",
+  "calendar",
+  "label",
+  "weekendLabel",
+  "button"
+);
+
+const elementStateStyles = {
+  _hover: {
+    borderStyle: "solid",
+    borderWidth: "2px",
+    borderColor: "alias.osloGrey",
+  },
+  _focus: {
+    borderStyle: "solid",
+    borderWidth: "2px",
+    borderColor: "alias.greenHaze",
+  },
+  _active: {
+    backgroundColor: "alias.mint",
+  },
+  _disabled: {
+    color: "alias.osloGrey",
+    border: "none",
+  },
+};
 
 const baseStyle: PartsStyleObject<typeof parts> = {
+  input: {
+    ...elementStateStyles,
+  },
+  calendarButton: {
+    borderStyle: "solid",
+    borderWidth: "1px",
+    borderRadius: "sm",
+    borderLeftRadius: "0px",
+    padding: "15px",
+    ...elementStateStyles,
+  },
   calendar: {
-    borderRadius: 'md',
+    borderRadius: "md",
     boxShadow: "0px 1px 5px rgba(0, 0, 0, 0.2)",
     height: "348px",
-    width: "375px"
+    width: "375px",
   },
   label: {
-    fontWeight: 'bold',
-    color: 'alias.darkGrey',
+    fontWeight: "bold",
+    color: "alias.darkGrey",
   },
-  weekendLabel : {
-    fontWeight: 'bold',
-    color: 'alias.greenHaze',
+  weekendLabel: {
+    fontWeight: "bold",
+    color: "alias.greenHaze",
   },
   button: {
-    backgroundColor: 'alias.white',
-    color: 'alias.darkGrey',
-    borderRadius: '50%',
-    height: '36px',
-    width: '36px',
-    _hover: {
-      borderStyle: 'solid',
-      borderWidth: '2px',
-      borderColor: 'alias.osloGrey',
-    },
-    _focus: {
-      borderStyle: 'solid',
-      borderWidth: '2px',
-      borderColor: 'alias.greenHaze',
-    },
-    _active: {
-      backgroundColor: 'alias.mint'
-    },
-    _disabled: {
-      color: 'alias.osloGrey',
-      border: 'none'
-    },
-  }
+    backgroundColor: "alias.white",
+    color: "alias.darkGrey",
+    borderRadius: "50%",
+    height: "36px",
+    width: "36px",
+    ...elementStateStyles,
+  },
 };
+
+const sizes: Record<string, PartsStyleInterpolation<typeof parts>> = {
+  sm: {
+    input: {
+      height: "56px",
+      minWidth: "172px",
+    },
+    calendarButton: {
+      height: "56px",
+    },
+  },
+  lg: {
+    input: {
+      height: "60px",
+      minWidth: "180px",
+    },
+    calendarButton: {
+      height: "60px",
+    },
+  },
+};
+
+const variants: Record<string, PartsStyleInterpolation<typeof parts>> = {
+  mobile: {
+    input: {
+      borderRightRadius: 0,
+    },
+  },
+};
+
 const Datepicker: ComponentMultiStyleConfig = {
   parts: parts.keys,
   defaultProps: {},
   baseStyle: baseStyle,
-  sizes: {},
-  variants: {}
-}
+  sizes,
+  variants,
+};
 
-export default Datepicker
+export default Datepicker;

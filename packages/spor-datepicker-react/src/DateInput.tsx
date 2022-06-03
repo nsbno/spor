20f2caaa24cfc533df0cbe8e6ce208b6afaf27b9
Eivind Høiseth-Gilje <eivindgi@gmail.com>
2022-06-03 11:59:12 +0200

    Datepicker: support for mobile and desktop variants, small and large size

--- packages/spor-datepicker-react/src/DateInput.tsx
+++ packages/spor-datepicker-react/src/DateInput.tsx
@@ -1,20 +1,38 @@
-import { CalendarOutline30Icon } from "@vygruppen/spor-icon-react/tmp";
+import { CalendarOutline24Icon } from "@vygruppen/spor-icon-react/tmp";
 import { Input } from "@vygruppen/spor-input-react";
 import React from "react";
 import { useDatepicker } from "./DatepickerContext";
+import { Flex, InputGroup, InputRightAddon, useStyles } from "@chakra-ui/react";
+import { IconButton } from "@vygruppen/spor-button-react";
+import { DatepickerProps } from "./Datepicker";
 
-export const DateInput: React.FC = () => {
+export const DateInput: React.VFC<DatepickerProps> = ({ variant }) => {
   const { selectedDate, setSelectedDate } = useDatepicker();
+  const styles = useStyles();
 
   return (
-    <Input
-      leftIcon={<CalendarOutline30Icon />}
-      label="dato"
-      width="196px"
-      value={selectedDate?.toLocaleDateString()}
-      onChange={(event) => {
-        setSelectedDate(new Date(event.target.value));
-      }}
-    />
+    <InputGroup>
+      <Flex>
+        <Input
+          leftIcon={variant === "desktop" && <CalendarOutline24Icon />}
+          label="dato"
+          value={selectedDate?.toLocaleDateString()}
+          onChange={(event) => {
+            setSelectedDate(new Date(event.target.value));
+          }}
+          sx={styles.input}
+        />
+        {variant === "mobile" && (
+          <InputRightAddon>
+            <IconButton
+              variant="additional"
+              icon={<CalendarOutline24Icon />}
+              aria-label="Kalender"
+              sx={styles.calendarButton}
+            />
+          </InputRightAddon>
+        )}
+      </Flex>
+    </InputGroup>
   );
 };

a75cf2afe4433d47088eba6ac1326792a2a87799
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-06-08 10:17:01 +0200

    Add some improved types to a layout component

--- packages/spor-layout-react-native/src/Stack.tsx
+++ packages/spor-layout-react-native/src/Stack.tsx
@@ -13,7 +13,7 @@ export const Stack = (props: StackProps) => {
   const { spacing = 2, children, ...rest } = props;
   return (
     <Box {...rest}>
-      {React.Children.map(children, (child, index) =>
+      {React.Children.map(children, (child: any, index: number) =>
         React.cloneElement(child as any, {
           marginTop: index === 0 ? 0 : spacing,
         })

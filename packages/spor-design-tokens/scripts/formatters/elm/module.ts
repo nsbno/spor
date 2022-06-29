7c859c805721331738096b9a876e76c4370dd2f5
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-06-29 13:12:51 +0200

    Auto-generate opaque types per module, with a function to unwrap to elm-css.

--- packages/spor-design-tokens/scripts/formatters/elm/module.ts
+++ packages/spor-design-tokens/scripts/formatters/elm/module.ts
@@ -1,14 +1,18 @@
-import { Format, Named, TransformedToken, formatHelpers } from 'style-dictionary';
+import { Format, Named, File, TransformedToken, formatHelpers } from 'style-dictionary';
 
 export const elmFormatter: Named<Format> = {
   name: 'elm/module',
   formatter: function({ dictionary, file }) {
     const moduleName = generateModuleName(file.destination);
 
-    const exposing = dictionary
-      .allProperties
-      .map((prop) => prop.name)
-      .join(', ');
+    const moduleType = generateModuleType(file.destination);
+
+    const exposing = moduleType.exposings().concat(
+        dictionary
+            .allProperties
+            .map((prop) => prop.name)
+    )
+    .join(', ');
     
     const fileHeader = formatHelpers.fileHeader({
       file: file,
@@ -20,12 +24,12 @@ export const elmFormatter: Named<Format> = {
       }
     });
 
-    const imports = generateImportStatements(dictionary.allProperties);
-
-    const transformedTokens = dictionary
-      .allProperties
-      .map(generateElmConstant);
-
+    const definitions = moduleType.definitions().concat(
+        dictionary
+            .allProperties
+            .map((prop) => generateElmConstant(prop, moduleType))
+            .flat()
+    );
     
     return [
       `module ${moduleName} exposing (${exposing})`,
@@ -34,8 +38,8 @@ export const elmFormatter: Named<Format> = {
       '',
       `{-| @docs ${exposing} -}`,
       '',
-    ].concat(...imports)
-     .concat(...transformedTokens)
+    ].concat(...moduleType.imports(), '')
+     .concat(...definitions)
      .join('\n');
   },
 };
@@ -43,150 +47,105 @@ export const elmFormatter: Named<Format> = {
 const moduleNamePrefix = 'Spor.Token.';
 const defaultIndentation = '    ';
 
-function generateModuleName(file: string): string {
-    return moduleNamePrefix + file.replace('\.elm', '').replace('\/', '\.');
+function generateModuleName(fileName: string): string {
+    return moduleNamePrefix + fileName.replace(/\.elm$/, '').replace('\/', '\.');
 }
 
-function generateImportStatements(tokens: Array<TransformedToken>): Array<string> {
-    const imports = tokens.map(typeAndConstructor).map((tc) => tc.type.imports);
-    const processedImports = processImports(imports);
+class ModuleType {
+    name: string;
+    wrappedType: ModuleTypeConstruction;
+
+    constructor(name: string, wrappedType: ModuleTypeConstruction) {
+        this.name = name;
+        this.wrappedType = wrappedType;
+    }
+
+    imports(): Array<string> {
+        return [
+            'import Css'
+        ];
+    }
+
+    exposings(): Array<string> {
+        return [
+            this.name,
+            'toCss'
+        ];
+    }
+
+    definitions(): Array<string> {
+        return [
+            '{-| -}',
+            `type ${this.name} =`,
+            `${defaultIndentation}${this.name} ${this.wrappedType.innerType}`,
+            '',
+            `{-| Convert ${this.name} into an elm-css value -}`,
+            `toCss : ${this.name} -> ${this.wrappedType.innerType}`,
+            `toCss (${this.name} value) =`,
+            `${defaultIndentation}value`,
+            ''
+        ];
+    }
+};
+
+function generateModuleType(fileName: string): ModuleType {
+    const type = fileName.replace(/^\w+\//, '').replace(/\.elm$/, '');
+    const wrappedType = moduleTypeInnerType.get(type) || stringTypeConstruction;
     
-    return formatImports(processedImports);
+    return new ModuleType(type, wrappedType);
 }
 
-function processImports(configs: Array<ImportConfig>): ImportConfig {
-    const output: ImportConfig = {};
+type ModuleTypeConstruction = {
+    innerType: string;
+    construct: (input: string) => string;
+};
 
-    for (let config of configs) {
-        for (let key in config) {
-            const value = config[key];
-
-            if (key in output) {
-                const existingValue = output[key];
-                existingValue.alias = existingValue.alias || value.alias;
-
-                for (let exposing in value.exposings) {
-                    existingValue.exposings.add(exposing);
-                }
-            } else {
-                output[key] = value;
-            }
+const colorTypeConstruction: ModuleTypeConstruction = {
+    innerType: 'Css.Color',
+    
+    construct(input: string) {
+        if (input.startsWith('#')) {
+            return `Css.hex ${asString(input)}`
+        } else if (input.startsWith('rgba')) {
+            // remove the first five characters 'rgba(' and the last character ')
+            const args = input.slice(5, -1).split(',').map((s: string) => s.trim());
+            return `Css.rgba ${args[0]} ${args[1]} ${args[2]} ${args[3]}`
         }
-    }
-    
-    return output;
-}
-
-function formatImports(imports: ImportConfig): Array<string> {
-    const output = [];
-
-    for (let key in imports) {
-        const value = imports[key];
         
-        const alias = value.alias ? ` as ${value.alias}` : '';
-        const exposingList = Array.from(value.exposings).join(', ');
-        const exposings = exposingList.length > 0 ? ` exposing (${exposingList})` : '';
-        
-        output.push(`import ${key}${alias}${exposings}`);
+        throw new Error(`Don\'t know how to handle this color value: ${input}`);
     }
+};
 
-    // Make sure there's an empty line before Elm definitions
-    output.push('');
+const stringTypeConstruction: ModuleTypeConstruction = {
+    innerType: 'String',
+    construct: asString
+};
 
-    return output;
-}
+const moduleTypeInnerType: Map<string, ModuleTypeConstruction> = new Map([
+    ['Alias', colorTypeConstruction],
+    ['Background', colorTypeConstruction],
+    ['Detail', colorTypeConstruction],
+    ['Error', colorTypeConstruction],
+    ['Linjetag', colorTypeConstruction],
+    ['Main', colorTypeConstruction],
+    ['Outline', colorTypeConstruction],
+    ['Palette', colorTypeConstruction],
+    ['Product', colorTypeConstruction],
+    ['Text', colorTypeConstruction]
+]);
 
-function generateElmConstant(token: TransformedToken): Array<string> {
+
+function generateElmConstant(token: TransformedToken, moduleType: ModuleType): Array<string> {
     const name = token.name;
-    const typeInfo = typeAndConstructor(token);
     
     return [
         `{-| ${token.comment || ''} -}`,
-        `${name} : ${typeInfo.type.name}`,
+        `${name} : ${moduleType.name}`,
         `${name} =`,
-        `${defaultIndentation}${typeInfo.constructor(token.value)}`,
+        `${defaultIndentation}${moduleType.name} <| ${moduleType.wrappedType.construct(token.value)}`,
         '',
         ''
     ];
-}
-
-type TypeAndConstructor = {
-    type: TypeInfo;
-    constructor: (input: any) => string;
-};
-
-type TypeInfo = {
-    name: string;
-    imports: ImportConfig;
-};
-
-type ImportConfig = Record<string, {
-    alias: string | null;
-    exposings: Set<string>;
-}>;
-
-function typeAndConstructor(token: TransformedToken): TypeAndConstructor {
-    if (token.path[0] === 'color') {
-        if (token.value.startsWith('#')) {
-            return {
-                type: {
-                    name: 'Color',
-                    imports: { 
-                        'Spor.Token.Color': {
-                            alias: null,
-                            exposings: new Set([ 'Color' ])
-                        },
-                        'Spor.Token.Internal.Types': {
-                            alias: 'Types', 
-                            exposings: new Set()
-                        },
-                        'Css': {
-                            alias: null,
-                            exposings: new Set()
-                        }
-                    }
-                },
-                constructor: (input) => {
-                    return `Types.Color <| Css.hex ${asString(input)}`
-                }
-            };
-        } else if (token.value.startsWith('rgba')) {
-            return {
-                type: {
-                    name: 'Color',
-                    imports: { 
-                        'Spor.Token.Color': {
-                            alias: null,
-                            exposings: new Set([ 'Color' ])
-                        },
-                        'Spor.Token.Internal.Types': {
-                            alias: 'Types', 
-                            exposings: new Set() 
-                        },
-                        'Css': {
-                            alias: null,
-                            exposings: new Set()
-                        }
-                    }
-                },
-                constructor: (input: string) => {
-                    // remove the first five characters 'rgba(' and the last character ')
-                    const args = input.slice(5, -1).split(',').map((s) => s.trim());
-                    return `Types.Color <| Css.rgba ${args[0]} ${args[1]} ${args[2]} ${args[3]}`
-                }
-            };
-        } else {
-            throw new Error(`Don\'t know how to handle this color value: ${token.value}`);
-        }
-    }
-    
-    return {
-        type: {
-            name: 'String',
-            imports: {}
-        },
-        constructor: asString
-    };
 }
 
 function asString(input: any): string {

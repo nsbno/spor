bc95e783f2539e4dcd6dca763f514b59f8aa99e4
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-06-30 13:06:59 +0200

    Setup spor-elm package.

---
+++ packages/spor-elm/review/src/ReviewConfig.elm
@@ -0,0 +1,39 @@
+module ReviewConfig exposing (config)
+
+{-| Do not rename the ReviewConfig module or the config function, because
+`elm-review` will look for these.
+
+To add packages that contain rules, add them to this review project using
+
+    `elm install author/packagename`
+
+when inside the directory containing this file.
+
+-}
+
+import NoExposingEverything
+import NoImportingEverything
+import NoMissingTypeExpose
+import NoUnused.CustomTypeConstructorArgs
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
+import Review.Rule exposing (Rule)
+
+
+config : List Rule
+config =
+    [ NoUnused.CustomTypeConstructorArgs.rule
+    , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.Dependencies.rule
+    , NoUnused.Modules.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
+    , NoUnused.Variables.rule
+    , NoMissingTypeExpose.rule
+    , NoExposingEverything.rule
+    , NoImportingEverything.rule []
+    ]

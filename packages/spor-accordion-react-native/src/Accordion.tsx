128a324b20cefe1170fe4e76622e0270947204e3
annelinegjersem <annelinegj@gmail.com>
2022-07-06 14:39:39 +0200

    Ny pakke accordion

---
+++ packages/spor-accordion-react-native/src/Accordion.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+type AccordionProps = {};
+export const Accordion = (props: AccordionProps) => {
+  return <Text>Hi there</Text>;
+};

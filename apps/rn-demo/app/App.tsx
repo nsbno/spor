08989317f7439ed37c8f0db2db8e93a60803f28f
Magnus <Magnus.Eckhoff@bekk.no>
2022-07-12 13:39:53 +0200

    update app.tsx for testing

--- apps/rn-demo/app/App.tsx
+++ apps/rn-demo/app/App.tsx
@@ -1,15 +1,18 @@
 import {
+  Card,
   Heading,
   SporProvider,
   Stack,
   Text,
 } from "@vygruppen/spor-react-native";
+import { BusOutline18Icon } from "@vygruppen/spor-icon-react-native";
 import React from "react";
 import { SafeAreaView } from "react-native";
 /**
  * The entry point of the Spor RN demo app
  */
 const App = () => {
+  const [isSelected, setIsSelected] = React.useState(false);
   return (
     <SporProvider>
       <SafeAreaView>
@@ -27,6 +30,28 @@ const App = () => {
             Velkommen! Denne appen brukes til demonstrasjon og utvikling av
             forskjellige komponenter i Spor sitt designsystem for React Native.
           </Text>
+          <Card
+            colorScheme={"white"}
+            size="sm"
+            selected={isSelected}
+            onPress={() => {
+              setIsSelected(!isSelected);
+            }}
+            onClose={() => {
+              console.log("close");
+            }}
+          >
+            <Text variant="md">
+              120 kr
+              {/* Velkommen! Denne appen brukes til demonstrasjon og utvikling av
+              forskjellige komponenter i Spor sitt designsystem for React
+              Native.Velkommen! Denne appen brukes til demonstrasjon og
+              utvikling av forskjellige komponenter i Spor sitt designsystem for
+              React Native.Velkommen! Denne appen brukes til demonstrasjon og
+              utvikling av forskjellige komponenter i Spor sitt designsystem for
+              React Native. */}
+            </Text>
+          </Card>
         </Stack>
       </SafeAreaView>
     </SporProvider>

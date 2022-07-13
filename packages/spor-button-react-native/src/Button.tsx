4e1764d51bc50985f289e4fdbb7d5583414b9c81
Magnus <Magnus.Eckhoff@bekk.no>
2022-07-13 08:51:00 +0200

    bytt tilbake til tab length 2

--- packages/spor-button-react-native/src/Button.tsx
+++ packages/spor-button-react-native/src/Button.tsx
@@ -1,153 +1,153 @@
 import {
-    composeRestyleFunctions,
-    createVariant,
-    spacing,
-    SpacingProps,
-    useRestyle,
-    useTheme,
-    VariantProps,
+  composeRestyleFunctions,
+  createVariant,
+  spacing,
+  SpacingProps,
+  useRestyle,
+  useTheme,
+  VariantProps,
 } from "@shopify/restyle";
 import type { Theme } from "@vygruppen/spor-theme-react-native";
 import { ColorInlineLoader } from "@vygruppen/spor-loader-react-native";
 import React, { useState } from "react";
 import {
-    Pressable,
-    StyleProp,
-    StyleSheet,
-    Text,
-    TextStyle,
+  Pressable,
+  StyleProp,
+  StyleSheet,
+  Text,
+  TextStyle,
 } from "react-native";
 import { Box } from "@vygruppen/spor-layout-react-native";
 
 const variants = createVariant({
-    themeKey: "buttonVariants",
-    property: "variant",
+  themeKey: "buttonVariants",
+  property: "variant",
 });
 
 type ButtonVariant =
-    | "primary"
-    | "control"
-    | "secondary"
-    | "tertiary"
-    | "additional"
-    | "ghost";
+  | "primary"
+  | "control"
+  | "secondary"
+  | "tertiary"
+  | "additional"
+  | "ghost";
 
 type RestyleProps = SpacingProps<Theme> &
-    VariantProps<Theme, "buttonVariants", "variant"> &
-    VariantProps<Theme, "buttonSizes", "size">;
+  VariantProps<Theme, "buttonVariants", "variant"> &
+  VariantProps<Theme, "buttonSizes", "size">;
 
 const sizes = createVariant({ themeKey: "buttonSizes", property: "size" });
 
 const restyleFunctions = composeRestyleFunctions<Theme, RestyleProps>([
-    spacing,
-    sizes,
-    variants,
+  spacing,
+  sizes,
+  variants,
 ]);
 
 type ButtonProps = Exclude<RestyleProps, "variant"> & {
-    onPress: () => void;
-    variant: ButtonVariant;
-    children?: string;
-    isDisabled?: boolean;
-    isLoading?: boolean;
-    accessibilityLabel?: string;
-    leftIcon?: JSX.Element;
-    rightIcon?: JSX.Element;
+  onPress: () => void;
+  variant: ButtonVariant;
+  children?: string;
+  isDisabled?: boolean;
+  isLoading?: boolean;
+  accessibilityLabel?: string;
+  leftIcon?: JSX.Element;
+  rightIcon?: JSX.Element;
 };
 
 /** A button. */
 export const Button = ({
-    onPress,
-    variant,
-    isDisabled = false,
-    isLoading = false,
-    accessibilityLabel,
-    children,
-    leftIcon,
-    rightIcon,
-    ...rest
+  onPress,
+  variant,
+  isDisabled = false,
+  isLoading = false,
+  accessibilityLabel,
+  children,
+  leftIcon,
+  rightIcon,
+  ...rest
 }: ButtonProps) => {
-    const [isPressed, setIsPressed] = useState(false);
+  const [isPressed, setIsPressed] = useState(false);
 
-    const { buttonVariantsActive, buttonVariantsDisabled } = useTheme();
-    const { style: restyleStyle } = useRestyle(restyleFunctions, {
-        variant,
-        ...rest,
-    });
-    const activeStyle = isPressed ? buttonVariantsActive[variant] : {};
-    const disabledAndLoadingStyle =
-        isDisabled || isLoading ? buttonVariantsDisabled : {};
+  const { buttonVariantsActive, buttonVariantsDisabled } = useTheme();
+  const { style: restyleStyle } = useRestyle(restyleFunctions, {
+    variant,
+    ...rest,
+  });
+  const activeStyle = isPressed ? buttonVariantsActive[variant] : {};
+  const disabledAndLoadingStyle =
+    isDisabled || isLoading ? buttonVariantsDisabled : {};
 
-    const style = [
-        restyleStyle,
-        activeStyle,
-        disabledAndLoadingStyle,
-    ] as StyleProp<TextStyle>;
+  const style = [
+    restyleStyle,
+    activeStyle,
+    disabledAndLoadingStyle,
+  ] as StyleProp<TextStyle>;
 
-    const flatStyles = StyleSheet.flatten(style);
-    const { fontSize, fontWeight, color, backgroundColor } = flatStyles;
+  const flatStyles = StyleSheet.flatten(style);
+  const { fontSize, fontWeight, color, backgroundColor } = flatStyles;
 
-    return (
-        <Pressable
-            accessibilityRole="button"
-            accessibilityState={{ disabled: isDisabled, busy: isLoading }}
-            onPressIn={() => setIsPressed(true)}
-            onPressOut={() => setIsPressed(false)}
-            onPress={onPress}
-            disabled={isDisabled || isLoading}
-            accessibilityLabel={accessibilityLabel}
-            style={style}
-            android_ripple={{
-                color: backgroundColor,
+  return (
+    <Pressable
+      accessibilityRole="button"
+      accessibilityState={{ disabled: isDisabled, busy: isLoading }}
+      onPressIn={() => setIsPressed(true)}
+      onPressOut={() => setIsPressed(false)}
+      onPress={onPress}
+      disabled={isDisabled || isLoading}
+      accessibilityLabel={accessibilityLabel}
+      style={style}
+      android_ripple={{
+        color: backgroundColor,
+      }}
+      {...rest}
+    >
+      <Box
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          opacity: isLoading ? 0 : 1,
+        }}
+      >
+        {leftIcon && (
+          <Box
+            style={{
+              marginLeft: children ? -6 : 0,
+              marginRight: children ? 6 : 0,
             }}
-            {...rest}
+          >
+            <leftIcon.type {...leftIcon.props} {...{ color }} />
+          </Box>
+        )}
+        <Text
+          style={{
+            color,
+            fontSize,
+            fontWeight,
+          }}
         >
-            <Box
-                style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    opacity: isLoading ? 0 : 1,
-                }}
-            >
-                {leftIcon && (
-                    <Box
-                        style={{
-                            marginLeft: children ? -6 : 0,
-                            marginRight: children ? 6 : 0,
-                        }}
-                    >
-                        <leftIcon.type {...leftIcon.props} {...{ color }} />
-                    </Box>
-                )}
-                <Text
-                    style={{
-                        color,
-                        fontSize,
-                        fontWeight,
-                    }}
-                >
-                    {children}
-                </Text>
-                {rightIcon && (
-                    <Box marginLeft={1} style={{ marginRight: -6 }}>
-                        <rightIcon.type {...rightIcon.props} {...{ color }} />
-                    </Box>
-                )}
-            </Box>
+          {children}
+        </Text>
+        {rightIcon && (
+          <Box marginLeft={1} style={{ marginRight: -6 }}>
+            <rightIcon.type {...rightIcon.props} {...{ color }} />
+          </Box>
+        )}
+      </Box>
 
-            {isLoading && (
-                <Box
-                    style={{
-                        position: "absolute",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        top: 0,
-                        bottom: 0,
-                    }}
-                >
-                    <ColorInlineLoader height="75%" />
-                </Box>
-            )}
-        </Pressable>
-    );
+      {isLoading && (
+        <Box
+          style={{
+            position: "absolute",
+            justifyContent: "center",
+            alignItems: "center",
+            top: 0,
+            bottom: 0,
+          }}
+        >
+          <ColorInlineLoader height="75%" />
+        </Box>
+      )}
+    </Pressable>
+  );
 };

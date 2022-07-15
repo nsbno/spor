e12364190e835977e060915c76e0865edf8cab6d
annelinegjersem <annelinegj@gmail.com>
2022-07-15 15:36:38 +0200

    Fjerne ekstra mellomrom

--- packages/spor-theme-react-native/src/components/expandable.tsx
+++ packages/spor-theme-react-native/src/components/expandable.tsx
@@ -31,7 +31,6 @@ export function getExpandableVariantPressedState(
     ...expandableVariantPressed[variant],
   };
 }
-
 const expandableVariantPressed = {
   defaults: {
     backgroundColor: colors.mint,

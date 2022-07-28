3519f49c000b380b2c681efe7469222b06325a2e
Magnus <Magnus.Eckhoff@bekk.no>
2022-07-28 09:29:04 +0200

    change to flex grow.

--- packages/spor-card-react-native/src/Card.tsx
+++ packages/spor-card-react-native/src/Card.tsx
@@ -174,9 +174,9 @@ export const Card = ({
         onPressIn={handlePressIn}
         onPressOut={handlePressOut}
         onPress={onPress}
-        style={[style as any, { flex: 1 }]}
+        style={[style as any, { flexGrow: 1 }]}
       >
-        <Box flexDirection="row" flex={1}>
+        <Box flexDirection="row">
           {selectedIconIfEnabled}
           {childrenBox}
           {closeButtonIfEnabled}
@@ -186,7 +186,7 @@ export const Card = ({
   }
 
   return (
-    <Box style={style as any} flexDirection="row" flex={1}>
+    <Box style={style as any} flexDirection="row" flexGrow={1}>
       {childrenBox}
       {closeButtonIfEnabled}
     </Box>

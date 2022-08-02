7fa75dfb37de5c39d413ad53e41456c86956c877
karendahlaarhus <aarhus.karen.dahl@vy.no>
2022-08-02 10:34:32 +0200

    app.tsx cleanup

--- apps/rn-demo/app/App.tsx
+++ apps/rn-demo/app/App.tsx
@@ -1,74 +1,35 @@
 import {
+  Heading,
   SporProvider,
   Stack,
-  Button,
-  BottomDrawer,
-  DrawerHeader,
   Text,
-  Box,
-  DrawerFooter,
 } from "@vygruppen/spor-react-native";
-import React, { useState } from "react";
+import React from "react";
 import { SafeAreaView } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+
 /**
  * The entry point of the Spor RN demo app
  */
 
 const App = () => {
-  const [drawerVisible, setDrawerVisible] = useState(true);
   return (
     <SporProvider>
-      <SafeAreaProvider>
-        <SafeAreaView>
-          <Stack
-            backgroundColor="white"
-            height="100%"
-            p={2}
-            justifyContent="center"
-            spacing={3}
-          >
-            <Button
-              variant="primary"
-              size="md"
-              onPress={() => setDrawerVisible(true)}
-            >
-              åpne meg
-            </Button>
-
-            <BottomDrawer
-              isVisible={drawerVisible}
-              onClose={() => setDrawerVisible(false)}
-            >
-              <DrawerHeader textAlign="left" size="lg" icon={true}>
-                tittel
-              </DrawerHeader>
-              <Text>
-                Dette er et eksempel på en lang melding i en skuff. Del gjerne
-                opp innholdet over flere avsnitt, for å gjøre meldingen lettere
-                å lese. Dette er et eksempel på en lang melding i en skuff. Del
-                gjerne opp innholdet over flere avsnitt, for å gjøre meldingen
-                lettere å lese. Dette er et eksempel på en lang melding i en
-                skuff. Del gjerne opp innholdet over flere avsnitt.
-              </Text>
-
-              <DrawerFooter>
-                <Box flexDirection="row" justifyContent="space-around">
-                  <Button
-                    variant="primary"
-                    size="md"
-                    onPress={() => {}}
-                    marginRight={2}
-                    flex={1}
-                  >
-                    knapp
-                  </Button>
-                </Box>
-              </DrawerFooter>
-            </BottomDrawer>
-          </Stack>
-        </SafeAreaView>
-      </SafeAreaProvider>
+      <SafeAreaView>
+        <Stack
+          backgroundColor="white"
+          height="100%"
+          p={2}
+          justifyContent="center"
+        >
+          <Heading color="darkGrey" variant="2xl" textAlign="center">
+            Spor Demo app
+          </Heading>
+          <Text color="darkGrey" variant="md" textAlign="center">
+            Velkommen! Denne appen brukes til demonstrasjon og utvikling av
+            forskjellige komponenter i Spor sitt designsystem for React Native.
+          </Text>
+        </Stack>
+      </SafeAreaView>
     </SporProvider>
   );
 };

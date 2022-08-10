262fd2266fa5880e5b612c72e11c187e41531b7d
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-08-10 09:41:03 +0200

    Don't render a border if there isn't a border to render

--- packages/spor-linjetag-react-native/src/LineTagIcon.tsx
+++ packages/spor-linjetag-react-native/src/LineTagIcon.tsx
@@ -13,10 +13,9 @@ export const LineTagIcon = ({ variant, size, ...rest }: LineIconProps) => {
   return (
     <Box
       backgroundColor={styles.mainBackgroundColor}
-      borderWidth={1}
+      borderWidth={styles.borderColor ? 1 : 0}
       borderStyle="solid"
       borderColor={styles.borderColor ?? "transparent"}
-      p={1}
       {...rest}
     >
       <Icon color={styles.iconColor} />

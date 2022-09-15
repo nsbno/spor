f1718e236c852525bfcb7c9146b7ccfabdf841f9
heisand <heisand_p@hotmail.com>
2022-09-15 14:59:22 +0200

    Add missing documentation comment

--- packages/spor-elm/src/Spor/LineTag/Types.elm
+++ packages/spor-elm/src/Spor/LineTag/Types.elm
@@ -1,6 +1,14 @@
-module Spor.LineTag.Types exposing (Size(..), Variant(..))
+module Spor.LineTag.Types exposing (Variant(..), Size(..))
+
+{-| The `Variant` type is used for configuring the variant of the line tag component (LocalTrain is default).
+The size is used to configure the size of the line tag (Md is default).
+
+@docs Variant, Size
+
+-}
 
 
+{-| -}
 type Variant
     = LocalTrain
     | RegionTrain

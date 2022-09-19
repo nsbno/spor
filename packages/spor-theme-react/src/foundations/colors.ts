b30d7873c40c66070036c11bf1d9b370428370ed
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-09-19 15:15:44 +0200

    Simplify how the react theme gets its colors

--- packages/spor-theme-react/src/foundations/colors.ts
+++ packages/spor-theme-react/src/foundations/colors.ts
@@ -1,30 +1,10 @@
 import tokens from "@vygruppen/spor-design-tokens";
 
-type Token = { value: string };
+export type ColorsType = typeof tokens.color.alias &
+  typeof tokens.color.palette & { linjetag: typeof tokens.color.linjetag };
 
-type FlattenValue<T> = T extends { value: string }
-  ? string
-  : { [K in keyof T]: FlattenValue<T[K]> };
-type TransformColorTokens<T> = {
-  [K in keyof T]: FlattenValue<T[K]>;
+export const colors: ColorsType = {
+  ...tokens.color.alias,
+  ...tokens.color.palette,
+  linjetag: tokens.color.linjetag,
 };
-
-type FinalColors = TransformColorTokens<typeof tokens.color>;
-
-export const colors: FinalColors = Object.entries(tokens.color).reduce(
-  (allColors, [colorName, tokenOrScale]) => {
-    if ("value" in tokenOrScale) {
-      return { ...allColors, [colorName]: tokenOrScale.value };
-    } else {
-      const scale = Object.entries(tokenOrScale).reduce(
-        (acc, [scaleNumber, scaleToken]) => ({
-          ...acc,
-          [scaleNumber]: (scaleToken as Token).value,
-        }),
-        {} as Record<number, string>
-      );
-      return { ...allColors, [colorName]: scale };
-    }
-  },
-  {} as FinalColors
-);

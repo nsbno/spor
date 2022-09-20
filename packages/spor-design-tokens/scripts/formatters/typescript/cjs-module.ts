295f839e4c4f126696083c4bac950bc9cd33994a
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-09-20 09:35:33 +0200

    Remove copy-pasta comment

--- packages/spor-design-tokens/scripts/formatters/typescript/cjs-module.ts
+++ packages/spor-design-tokens/scripts/formatters/typescript/cjs-module.ts
@@ -3,8 +3,6 @@ import { simplifyTokens } from "../../utils/simplifyTokens";
 
 /**
  * This formatter creates a JavaScript file that exports the bare minimum of the design token object.
- *
- * It also uses the ES Modules syntax, so we can tree-shake out any unused tokens.
  */
 export const cjsModuleFormatter: Named<Format> = {
   name: "typescript/cjs-module",

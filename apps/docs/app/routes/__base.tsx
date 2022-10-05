bbab6cb5201babfdcbd728cbd234ff244169efe6
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-10-05 20:53:45 +0200

    Create a common layout for most routes

---
+++ apps/docs/app/routes/__base.tsx
@@ -0,0 +1,36 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { LoaderFunction } from "@remix-run/node";
+import { Outlet } from "@remix-run/react";
+import { LeftSidebar } from "~/features/layouts/docs-layout/left-sidebar/LeftSidebar";
+import TableOfContent from "~/features/table-of-contents/TableOfContents";
+import { useHeadings } from "~/features/table-of-contents/useHeadings";
+import { getClient } from "~/utils/sanity/client";
+
+/* This loader isn't use here directly, but from within the left sidebar component tree. Don't remove it, even if it isn't used here.  */
+export const loader: LoaderFunction = async () => {
+  const menu = await getClient().fetch(
+    `*[_type == "menu" && slug.current == "side-menu"][0] { menuItems }`
+  );
+  return { menu };
+};
+
+export default function BaseLayout() {
+  const { headings, contentRef } = useHeadings();
+  return (
+    <Flex flex="1">
+      <LeftSidebar />
+      <Box
+        as="main"
+        flex="1"
+        mt={6}
+        mx={[3, 6, 10]}
+        mb={["60px", "120px", "180px"]}
+        maxWidth="924px"
+        ref={contentRef}
+      >
+        <Outlet />
+      </Box>
+      <TableOfContent headings={headings} />
+    </Flex>
+  );
+}

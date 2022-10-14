1650d4e00810a09d3190ad03bed545df1244d30f
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-10-14 14:57:20 +0200

    Add some tests for buttons

---
+++ packages/spor-button-react/src/Button.test.tsx
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import { vi } from "vitest";
+import { axe } from "vitest-axe";
+import { Button } from ".";
+
+describe("<Button />", () => {
+  it("works like a button", async () => {
+    const handleClick = vi.fn();
+    const { getByRole } = render(
+      <Button variant="primary" onClick={handleClick}>
+        Click me
+      </Button>
+    );
+    getByRole("button").click();
+    expect(handleClick).toHaveBeenCalled();
+  });
+
+  it("is accessible", async () => {
+    const { container } = render(<Button variant="primary">Click me</Button>);
+    expect(await axe(container)).toHaveNoViolations();
+  });
+});

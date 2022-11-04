53c99b120ba5701a76d368f7e4202c7fce448eac
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-11-04 10:17:10 +0100

    Add content type for component sections and new introduction

--- apps/studio/schemas/documents/article.tsx
+++ apps/studio/schemas/documents/article.tsx
@@ -73,9 +73,61 @@ export const article = defineType({
       ],
     }),
     defineField({
+      name: "introduction",
+      title: "Introduction",
+      description: "A quick summary of what this page is about",
+      type: "array",
+      of: [{ type: "block", styles: [{ title: "Text", value: "normal" }] }],
+    }),
+    defineField({
       name: "content",
       title: "Content",
       type: "content",
+      // TODO: Hide this for components once everything is migrated
+    }),
+    defineField({
+      name: "componentSections",
+      title: "Component sections",
+      description: "This is the new way to document components.",
+      hidden: (args) => {
+        const COMPONENTS_CATEGORY_ID = "ab982447-e71f-4a1f-85ef-d6d6deacddfe";
+        // TODO: This is a hack to hide this field when the article isn't a component
+        // The UUID is the ID of the component category in Sanity.
+        return (
+          (args.document?.category as any)?._ref !== COMPONENTS_CATEGORY_ID
+        );
+      },
+      type: "array",
+      of: [
+        {
+          type: "object",
+          name: "componentSection",
+          title: "Component section",
+          fields: [
+            {
+              name: "title",
+              type: "string",
+              title: "Title",
+              initialValue: "guidelines",
+              options: {
+                list: [
+                  { key: "guidelines", title: "Guidelines" },
+                  { key: "react", title: "React" },
+                  { key: "react-native", title: "React Native" },
+                  { key: "other", title: "Other" },
+                ],
+              },
+            },
+            {
+              name: "customTitle",
+              type: "string",
+              title: "Please specify the title you want",
+              hidden: ({ document }) => document?.title !== "other",
+            },
+            { name: "content", type: "content", title: "Content" },
+          ],
+        },
+      ],
     }),
   ],
   preview: {

6fd2530c4ea0b8c2eb23905e3e10826e49d915e0
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-11-10 13:19:30 +0100

    Add new package spor-util-react

---
+++ .changeset/twelve-mangos-pull.md
@@ -0,0 +1,6 @@
+---
+"@vygruppen/spor-util-react": minor
+"@vygruppen/spor-react": patch
+---
+
+Add new package spor-util-react
---
+++ packages/spor-util-react/src/externals.tsx
@@ -0,0 +1,20 @@
+/** This file works as a proxy for all Chakra UI exports */
+export {
+  useBreakpointValue,
+  useClipboard,
+  useControllableProp,
+  useDisclosure,
+  useMediaQuery,
+  useMergeRefs,
+  useOutsideClick,
+  usePrefersReducedMotion,
+  useTheme,
+  useToken,
+} from "@chakra-ui/react";
+export type {
+  UseBreakpointOptions,
+  UseClipboardOptions,
+  UseDisclosureProps,
+  UseMediaQueryOptions,
+  UseOutsideClickProps,
+} from "@chakra-ui/react";

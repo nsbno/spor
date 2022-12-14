d448a20f6f6a0d798f7d491d6b9e6197c6ede56e
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-12-14 13:23:53 +0100

    Implement an info select component

---
+++ packages/spor-input-react/src/ListBox.tsx
@@ -0,0 +1,103 @@
+import { Box, BoxProps, chakra, useMultiStyleConfig } from "@chakra-ui/react";
+import type { AriaListBoxOptions } from "@react-aria/listbox";
+import type { Node } from "@react-types/shared";
+import React, { forwardRef, RefObject, useContext, useRef } from "react";
+import { useListBox, useOption } from "react-aria";
+import type { ListState } from "react-stately";
+
+type ListBoxProps = AriaListBoxOptions<unknown> & {
+  listBoxRef?: React.RefObject<HTMLUListElement>;
+  state: ListState<unknown>;
+} & BoxProps;
+
+type OptionProps = {
+  item: Node<unknown>;
+  state: ListState<unknown>;
+};
+
+export const ListBox = forwardRef<HTMLUListElement, ListBoxProps>(
+  (props, ref) => {
+    const { state, ...rest } = props;
+    const styles = useMultiStyleConfig("ListBox", {});
+    const internalRef = useRef<HTMLUListElement>(null);
+    const listBoxRef = (ref ?? internalRef) as RefObject<HTMLUListElement>;
+    let { listBoxProps } = useListBox(props, state, listBoxRef);
+
+    return (
+      <Box
+        as="ul"
+        {...listBoxProps}
+        sx={styles.container}
+        ref={listBoxRef as RefObject<any>}
+        {...rest}
+      >
+        {Array.from(state.collection).map((item) => (
+          <Option key={item.key} item={item} state={state} />
+        ))}
+      </Box>
+    );
+  }
+);
+
+type OptionContextValue = {
+  labelProps: React.HTMLAttributes<HTMLElement>;
+  descriptionProps: React.HTMLAttributes<HTMLElement>;
+};
+
+const OptionContext = React.createContext<OptionContextValue>({
+  labelProps: {},
+  descriptionProps: {},
+});
+const useOptionContext = () => {
+  return useContext(OptionContext);
+};
+
+const Option = ({ item, state }: OptionProps) => {
+  const ref = useRef<HTMLLIElement>(null);
+  const styles = useMultiStyleConfig("ListBox", {});
+  const { optionProps, labelProps, descriptionProps } = useOption(
+    { key: item.key },
+    state,
+    ref
+  );
+
+  return (
+    <chakra.li {...optionProps} ref={ref} sx={styles.item}>
+      <OptionContext.Provider value={{ labelProps, descriptionProps }}>
+        {item.rendered}
+      </OptionContext.Provider>
+    </chakra.li>
+  );
+};
+
+// The Label and Description components will be used within an <Item>.
+// They receive props from the OptionContext defined above.
+// This ensures that the option is ARIA labelled by the label, and
+// described by the description, which makes for better announcements
+// for screen reader users.
+
+export function SelectItemLabel({ children }: { children: React.ReactNode }) {
+  let { labelProps } = useOptionContext();
+  const styles = useMultiStyleConfig("ListBox", {});
+  return (
+    <Box {...labelProps} sx={styles.label}>
+      {children}
+    </Box>
+  );
+}
+
+export function SelectItemDescription({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  let { descriptionProps } = useOptionContext();
+  const styles = useMultiStyleConfig("ListBox", {});
+  return (
+    <Box {...descriptionProps} sx={styles.description}>
+      {children}
+    </Box>
+  );
+}
+// TODO: Make SelectItem stylable
+export { Item as SelectItem } from "react-stately";
---
+++ packages/spor-theme-react/src/components/listbox.ts
@@ -0,0 +1,48 @@
+import { anatomy } from "@chakra-ui/anatomy";
+import { createMultiStyleConfigHelpers } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+const parts = anatomy("ListBox").parts(
+  "container",
+  "item",
+  "label",
+  "description"
+);
+
+const helpers = createMultiStyleConfigHelpers(parts.keys);
+
+const config = helpers.defineMultiStyleConfig({
+  baseStyle: (props) => ({
+    container: {
+      background: mode("white", "darkGrey")(props),
+      boxShadow: "sm",
+      overflowY: "auto",
+      width: "100%",
+      listStyle: "none",
+    },
+    item: {
+      paddingX: 3,
+      paddingY: 2,
+      color: mode("darkGrey", "white")(props),
+      _hover: {
+        background: mode("mint", "darkTeal")(props),
+        outline: "none",
+      },
+      _focus: {
+        outline: "none",
+        background: mode("mint", "darkTeal")(props),
+      },
+      _selected: {
+        background: mode("pine", "pine")(props),
+        color: mode("white", "white")(props),
+      },
+    },
+    label: {},
+    description: {
+      fontSize: ["mobile.sm", "desktop.sm"],
+      color: mode("darkGrey", "whiteAlpha.600")(props),
+    },
+  }),
+});
+
+export default config;

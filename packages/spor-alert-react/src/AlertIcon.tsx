17029122f0ee6c62d3a21cad17a8a26253d76415
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-12-22 12:24:35 +0100

    Add aria-labels to the different alert icons

--- packages/spor-alert-react/src/AlertIcon.tsx
+++ packages/spor-alert-react/src/AlertIcon.tsx
@@ -1,3 +1,4 @@
+import { createTexts, useTranslation } from "@vygruppen/spor-i18n-react";
 import {
   AltTransportOutline24Icon,
   ErrorOutline24Icon,
@@ -14,7 +15,10 @@ type AlertIconProps = { variant: BaseAlertProps["variant"] };
  */
 export const AlertIcon = ({ variant }: AlertIconProps) => {
   const Icon = getIcon(variant);
-  return <Icon aria-hidden="true" marginRight={1} color="darkGrey" />;
+  const { t } = useTranslation();
+  return (
+    <Icon aria-label={t(texts[variant])} marginRight={1} color="darkGrey" />
+  );
 };
 
 const getIcon = (variant: BaseAlertProps["variant"]) => {
@@ -31,3 +35,36 @@ const getIcon = (variant: BaseAlertProps["variant"]) => {
       return ErrorOutline24Icon;
   }
 };
+
+const texts = createTexts({
+  info: {
+    nb: "Informasjon",
+    nn: "Informasjon",
+    sv: "Information",
+    en: "Information",
+  },
+  success: {
+    nb: "Suksess",
+    nn: "Suksess",
+    sv: "Succé",
+    en: "Success",
+  },
+  warning: {
+    nb: "Advarsel",
+    nn: "Advarsel",
+    sv: "Varning",
+    en: "Warning",
+  },
+  error: {
+    nb: "Feil",
+    nn: "Feil",
+    sv: "Error",
+    en: "Error",
+  },
+  "alt-transport": {
+    nb: "Alternativ transport",
+    nn: "Alternativ transport",
+    sv: "Alternativ transport",
+    en: "Alternative transport",
+  },
+});

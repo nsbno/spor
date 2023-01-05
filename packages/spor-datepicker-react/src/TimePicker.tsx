765719f9b4bee185cac1110fb21cbf328e56f100
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2023-01-05 09:17:21 +0100

    Add JSDoc notes to the TimePicker component

--- packages/spor-datepicker-react/src/TimePicker.tsx
+++ packages/spor-datepicker-react/src/TimePicker.tsx
@@ -14,14 +14,50 @@ import { TimeField } from "./TimeField";
 import { getCurrentTime, useCurrentLocale } from "./utils";
 
 type TimePickerProps = Omit<BoxProps, "defaultValue"> & {
+  /** The label. Defaults to a localized version of "Time" */
   label?: string;
+  /** The name of the form field, if used in a regular form */
   name?: string;
+  /** The controlled value, if any.
+   *
+   * A `new Time(hours, minutes)` should be passed
+   **/
   value?: TimeValue;
+  /** A default value, if any.
+   *
+   * A `new Time(hours, minutes)` should be passed. Defaults to the current time if not provided.
+   **/
   defaultValue?: TimeValue;
+  /** Callback for when the time changes */
   onChange?: (value: TimeValue) => void;
+  /** The maxiumum number of minutes to move when the step buttons are used.
+   *
+   * Defaults to 5 minutes.
+   *
+   * An example: If the time is at 13:37 and the stepGranularity is 15, clicking the step button forwards will move the time to 13:45. Next click will move it to 14:00.
+   */
   stepGranularity?: number;
+  /** Whether or not the field is disabled */
   isDisabled?: boolean;
 };
+/** A time picker component.
+ * 
+ * This lets the user select a time of day, either through typing it in, using the up and down arrows to select the hour and minute, or by clicking the step buttons to move the time forwards or backwards in pre-defined increments.
+ * 
+ * ```tsx
+ * <TimePicker />
+ * ```
+ * 
+ * It can also be controlled:
+ * 
+ * ```tsx
+ * <TimePicker value={new Time(13, 37)} onChange={setTime} />
+ * ```
+ * 
+ * Note that the TimePicker uses the `Time` class to represent the time. This is a class that is part of the `@internationalized/date` package.
+ * 
+ * @see https://spor.cloud.vy.no/komponenter/timepicker
+ */
 export const TimePicker = ({
   label,
   value,

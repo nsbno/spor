990531d2cae472b059603fbd972353440391a4b3
Kristofer Giltvedt Selbekk <selbekk@users.noreply.github.com>
2023-02-03 12:44:39 +0100

    Legg til states på card selects (#618)
    * Add card select states
    
    * Add a withChevron prop to CardSelect, and tweak size styling

--- packages/spor-theme-react/src/components/card-select.ts
+++ packages/spor-theme-react/src/components/card-select.ts
@@ -13,7 +13,6 @@ const config = helpers.defineMultiStyleConfig({
       appearance: "none",
       display: "flex",
       alignItems: "center",
-      borderRadius: "sm",
       _expanded: {
         backgroundColor: mode("mint", "night")(props),
       },
@@ -35,21 +34,95 @@ const config = helpers.defineMultiStyleConfig({
     },
   }),
   variants: {
-    ghost: {
-      trigger: {},
-    },
+    ghost: (props) => ({
+      trigger: {
+        _hover: {
+          backgroundColor: mode("seaMist", "pine")(props),
+        },
+        _active: {
+          backgroundColor: mode("mint", "whiteAlpha.200")(props),
+        },
+      },
+    }),
     outline: (props) => ({
       trigger: {
         boxShadow: getBoxShadowString({
           borderColor: mode("blackAlpha.400", "whiteAlpha.400")(props),
         }),
+        ...focusVisible({
+          notFocus: { boxShadow: "none" },
+          focus: {
+            boxShadow: getBoxShadowString({
+              borderColor: "greenHaze",
+              borderWidth: 3,
+            }),
+            outline: "none",
+          },
+        }),
+        _hover: {
+          boxShadow: getBoxShadowString({
+            borderColor: mode("darkGrey", "white")(props),
+            borderWidth: 3,
+          }),
+        },
+        _active: {
+          backgroundColor: mode("mint", "whiteAlpha.200")(props),
+          boxShadow: getBoxShadowString({
+            borderColor: mode("darkGrey", "whiteAlpha.400")(props),
+            borderWidth: 1,
+          }),
+        },
+
+        _expanded: {
+          _hover: {
+            backgroundColor: mode("seaMist", "todo")(props),
+            boxShadow: "none",
+          },
+          _active: {
+            backgroundColor: mode("mint", "todo")(props),
+            boxShadow: getBoxShadowString({
+              borderColor: mode("blackAlpha.400", "whiteAlpha.400")(props),
+              borderWidth: 1,
+            }),
+          },
+        },
       },
     }),
-    card: {
+    card: (props) => ({
       trigger: {
+        backgroundColor: mode("transparent", "whiteAlpha.100")(props),
         boxShadow: "sm",
+        transition: "all .1s ease-out",
+        _hover: {
+          backgroundColor: mode("seaMist", "pine")(props),
+          boxShadow: getBoxShadowString({
+            borderColor: mode("silver", "whiteAlpha.400")(props),
+            borderWidth: 1,
+            baseShadow: "sm",
+          }),
+        },
+        _active: {
+          backgroundColor: mode("mint", "whiteAlpha.100")(props),
+          boxShadow: getBoxShadowString({
+            borderColor: mode("silver", "whiteAlpha.400")(props),
+            borderWidth: mode(0, 1)(props),
+            baseShadow: "sm",
+          }),
+        },
+        _expanded: {
+          _hover: {
+            boxShadow: getBoxShadowString({
+              borderColor: "darkGrey",
+              borderWidth: 3,
+            }),
+          },
+          _active: {
+            backgroundColor: mode("mint", "whiteAlpha.200")(props),
+            boxShadow: "none",
+          },
+        },
       },
-    },
+    }),
   },
   sizes: {
     sm: {
@@ -57,6 +130,8 @@ const config = helpers.defineMultiStyleConfig({
         paddingX: 1.5,
         paddingY: 1,
         minHeight: "1.25rem",
+        fontSize: "xs",
+        borderRadius: "sm",
       },
     },
     md: {
@@ -64,6 +139,8 @@ const config = helpers.defineMultiStyleConfig({
         paddingX: 2,
         paddingY: 1.5,
         minHeight: "2.625rem",
+        fontSize: "sm",
+        borderRadius: "sm",
       },
     },
     lg: {
@@ -71,6 +148,8 @@ const config = helpers.defineMultiStyleConfig({
         paddingX: 3,
         paddingY: 2,
         minHeight: "3.375rem",
+        fontSize: "sm",
+        borderRadius: "sm",
       },
     },
   },

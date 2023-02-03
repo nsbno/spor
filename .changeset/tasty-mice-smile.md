b71959a019a5a3443c8e0424d07b53441972ac05
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2023-02-03 12:03:33 +0100

    Add card select states

---
+++ .changeset/tasty-mice-smile.md
@@ -0,0 +1,6 @@
+---
+"@vygruppen/spor-theme-react": patch
+"@vygruppen/spor-input-react": patch
+---
+
+Add hover and active styles to card selects
--- packages/spor-theme-react/src/components/card-select.ts
+++ packages/spor-theme-react/src/components/card-select.ts
@@ -35,21 +35,95 @@ const config = helpers.defineMultiStyleConfig({
     },
   }),
   variants: {
-    ghost: {
-      trigger: {},
-    },
+    ghost: (props) => ({
+      trigger: {
+        _hover: {
+          backgroundColor: mode("seaMist", "pine")(props),
+        },
+        _active: {
+          backgroundColor: mode("mint", "whiteAlpha.200")(props),
+        },
+      },
+    }),
     outline: (props) => ({
       trigger: {
         boxShadow: getBoxShadowString({
           borderColor: mode("blackAlpha.400", "whiteAlpha.400")(props),
         }),
+        ...focusVisible({
+          notFocus: { boxShadow: "none" },
+          focus: {
+            boxShadow: getBoxShadowString({
+              borderColor: "greenHaze",
+              borderWidth: 3,
+            }),
+            outline: "none",
+          },
+        }),
+        _hover: {
+          boxShadow: getBoxShadowString({
+            borderColor: mode("darkGrey", "white")(props),
+            borderWidth: 3,
+          }),
+        },
+        _active: {
+          backgroundColor: mode("mint", "whiteAlpha.200")(props),
+          boxShadow: getBoxShadowString({
+            borderColor: mode("darkGrey", "whiteAlpha.400")(props),
+            borderWidth: 1,
+          }),
+        },
+
+        _expanded: {
+          _hover: {
+            backgroundColor: mode("seaMist", "todo")(props),
+            boxShadow: "none",
+          },
+          _active: {
+            backgroundColor: mode("mint", "todo")(props),
+            boxShadow: getBoxShadowString({
+              borderColor: mode("blackAlpha.400", "whiteAlpha.400")(props),
+              borderWidth: 1,
+            }),
+          },
+        },
       },
     }),
-    card: {
+    card: (props) => ({
       trigger: {
+        backgroundColor: mode("transparent", "whiteAlpha.100")(props),
         boxShadow: "sm",
+        transition: "all .1s ease-out",
+        _hover: {
+          backgroundColor: mode("seaMist", "pine")(props),
+          boxShadow: getBoxShadowString({
+            borderColor: mode("silver", "whiteAlpha.400")(props),
+            borderWidth: 1,
+            baseShadow: "sm",
+          }),
+        },
+        _active: {
+          backgroundColor: mode("mint", "whiteAlpha.100")(props),
+          boxShadow: getBoxShadowString({
+            borderColor: mode("silver", "whiteAlpha.400")(props),
+            borderWidth: mode(0, 1)(props),
+            baseShadow: "sm",
+          }),
+        },
+        _expanded: {
+          _hover: {
+            boxShadow: getBoxShadowString({
+              borderColor: "darkGrey",
+              borderWidth: 3,
+            }),
+          },
+          _active: {
+            backgroundColor: mode("mint", "whiteAlpha.200")(props),
+            boxShadow: "none",
+          },
+        },
       },
-    },
+    }),
   },
   sizes: {
     sm: {

a9216787cf2616419707c6c7006aa10058145137
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2023-05-24 20:42:35 +0200

    Reimplement the i18n functionality internally

---
+++ .changeset/angry-dingos-cry.md
@@ -0,0 +1,7 @@
+---
+"@vygruppen/spor-react": patch
+---
+
+Implement all of i18n locally instead of using an external library.
+
+This also adds support for nested hierarchies of keys, as well as fetching the current language through the useTranslation hook
--- packages/spor-react/src/i18n/index.tsx
+++ packages/spor-react/src/i18n/index.tsx
@@ -1,4 +1,4 @@
-import { initLobot } from "@leile/lobo-t";
+import React, { createContext, useContext } from "react";
 
 export enum Language {
   NorwegianBokmal = "nb",
@@ -7,18 +7,108 @@ export enum Language {
   English = "en",
 }
 
-export const { LanguageProvider, useTranslation } = initLobot<typeof Language>(
-  Language.NorwegianBokmal
-);
-
-type LanguageObject = {
-  [key in Language]: string;
+type TranslationObject = {
+  [key in Language]: string | React.ReactElement;
 };
-type LanguageFunction = (...args: (string | number)[]) => LanguageObject;
+type TranslationFunction = (...args: (string | number)[]) => TranslationObject;
 
+type Translation = TranslationObject | TranslationFunction;
 export type Translations = {
-  [key: string]: LanguageObject | LanguageFunction;
+  [key: string]: Translation | Translations;
 };
+
+const LanguageContext = createContext<Language | undefined>(undefined);
+type LanguageProviderProps = {
+  language: Language;
+  children: React.ReactElement;
+};
+/**
+ * A language provider component.
+ *
+ * This component should wrap your entire application. It will provide the language to all components that use it.
+ *
+ * This is done by the SporProvider component, so most likely, you won't need to use it directly, unless you want to use a specific language for a specific part of your application.
+ *
+ * ```tsx
+ * import { LanguageProvider, Language } from "@vygruppen/spor-react";
+ *
+ * const App = () => {
+ *   return (
+ *     <LanguageProvider language={Language.NorwegianBokmal}>
+ *       <Routes />
+ *     </LanguageProvider>
+ *   );
+ * }
+ * ```
+ *
+ */
+export function LanguageProvider({
+  language,
+  children,
+}: LanguageProviderProps) {
+  return (
+    <LanguageContext.Provider value={language}>
+      {children}
+    </LanguageContext.Provider>
+  );
+}
+
+/**
+ * Gets the language from the language context.
+ *
+ * @internal
+ */
+function useLanguage() {
+  const language = useContext(LanguageContext);
+  if (!language) {
+    throw new Error("Please wrap your application in a LanguageProvider");
+  }
+  return language;
+}
+
+function isLanguageFunction(
+  translation: Translation
+): translation is TranslationFunction {
+  return typeof translation === "function";
+}
+
+/**
+ * A hook that returns translation utilities. Typically used to translate text.
+ *
+ * ```tsx
+ * const Example = () => {
+ *   const { t } = useTranslation();
+ *   return <p>{t(texts.greeting)}</p>;
+ * }
+ * const texts = {
+ *   greeting: {
+ *     nb: "Hei",
+ *     nn: "Hei",
+ *     sv: "Hej",
+ *     en: "Hello",
+ *   }
+ * }
+ * ```
+ *
+ * You can also use it to fetch the current language:
+ *
+ * ```tsx
+ * const Example = () => {
+ *   const { language } = useTranslation();
+ *   return <p>{language}</p>;
+ * };
+ * ```
+ */
+export function useTranslation() {
+  const language = useLanguage();
+  const t = (text: Translation) => {
+    if (isLanguageFunction(text)) {
+      return (...args: Array<string | number>) => text(...args)[language];
+    }
+    return text[language];
+  };
+  return { t, language } as const;
+}
 
 /** Utility function that creates type safe text objects with useTranslation
  *
@@ -29,6 +119,14 @@ export type Translations = {
  *   nn: "Døme",
  *   sv: "Exempel",
  *   en: "Example",
+ *  },
+ *  another: {
+ *    example: {
+ *      nb: <strong>Eksempel</strong>,
+ *      nn: <strong>Døme</strong>,
+ *      sv: <strong>Exempel</strong>,
+ *      en: <strong>Example</strong>,
+ *    }
  *  }
  * })
  * ```

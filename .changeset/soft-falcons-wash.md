ecdb5d274d49e85109ee8dd29773a595e73a2072
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2023-06-05 12:23:21 +0200

    Fix a bug with onSelectionChange and nested lists

---
+++ .changeset/soft-falcons-wash.md
@@ -0,0 +1,5 @@
+---
+"@vygruppen/spor-react": patch
+---
+
+Autosuggest: Fix a bug where the onSelectionChanged was called with no item on nested lists
--- packages/spor-react/src/input/Autosuggest.tsx
+++ packages/spor-react/src/input/Autosuggest.tsx
@@ -99,6 +99,28 @@ type AutosuggestProps<T> = {
  *   );
  * };
  * ```
+ *
+ * The `fetcher` function can be any function that returns an iterable of items. This means that you can use any API library you want, as long as it returns an iterable of items.
+ *
+ * The items need to have a `key` property, which is used to identify the item. The `key` property can be any type, but it needs to be unique for each item.
+ *
+ * ```tsx
+ * [{ key: 'some-key', ...}, { key: 'some-other-key', ... }]
+ * ```
+ *
+ * You can also return a set of nested items, which will be rendered as a sub-list (or section). This is useful if you want to group your items. These items need to have a title prop (for labelling the section), as well as a `children` prop, which in turn will contain an iterable of items:
+ *
+ * ```tsx
+ * [
+ *   {
+ *    title: 'The title of the section',
+ *    children: [{ key: 'some-key', ... }]
+ *   },
+ *   {...}
+ * ]
+ * ```
+ *
+ * The `onSelectionChanged` will return the correct `item` (the one with the matching `key`), even if the item is in a sub-list.
  */
 export function Autosuggest<T extends object>({
   label,
@@ -114,11 +136,26 @@ export function Autosuggest<T extends object>({
       };
     },
   });
+
   const handleSelectionChange = (key: React.Key) => {
     if (!onSelectionChange) {
       return;
     }
-    return onSelectionChange(list.getItem(key));
+
+    let selectedItem = list.getItem(key);
+    if (!selectedItem) {
+      // If the item is not in the list, it might be in a sub-list
+      selectedItem = list.items
+        .flatMap((item) =>
+          "children" in item && Array.isArray(item.children)
+            ? item.children
+            : []
+        )
+        .find((child) => child.key === key);
+    }
+    if (selectedItem) {
+      onSelectionChange(selectedItem);
+    }
   };
   return (
     <Combobox

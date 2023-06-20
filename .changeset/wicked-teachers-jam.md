6c1f3d087afb8915bd2c27d5b294e836203922c1
Aksel Wester <aksel@wester.co>
2023-06-20 10:47:37 +0200

    Required heading level in Heading (#744)
    * Make `as` prop required in Heading
    
    * Add changeset
    
    * Update .changeset/wicked-teachers-jam.md
    
    Co-authored-by: Kristofer Giltvedt Selbekk <selbekk@users.noreply.github.com>
    
    * Update JSDoc for breaking change in Heading
    
    ---------
    
    Co-authored-by: Kristofer Giltvedt Selbekk <selbekk@users.noreply.github.com>

---
+++ .changeset/wicked-teachers-jam.md
@@ -0,0 +1,7 @@
+---
+"@vygruppen/spor-react": major
+---
+
+Heading: Make `as` prop required
+
+This is a breaking change. Previously, this defaulted to "h2". Go through all usages and add `as="h1"` or whatever is correct for your context.
--- packages/spor-react/src/typography/Heading.tsx
+++ packages/spor-react/src/typography/Heading.tsx
@@ -4,29 +4,28 @@ import React from "react";
 
 type TextStyles = keyof typeof tokens.font.style;
 
-export type HeadingProps = Exclude<ChakraHeadingProps, "textStyle"> & {
+export type HeadingProps = Omit<ChakraHeadingProps, "textStyle" | "as"> & {
+  /** The heading level, e.g. h1, h2, h3... **/
+  as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   /** The size and style of the heading */
   variant?: TextStyles;
 };
 /**
  * Create your own fancy headings with this component.
  *
- * You can specify the variant, which is one of "xs", "sm", "md", "lg", "xl-sans", "xs-serif" and "2xl". The default is "xl-sans".
- *
- * ```tsx
- * <Heading variant="2xl">Look at me!</Heading>
- * ```
- *
- * You can also specify what level of heading you want. You do this with the `as` prop. The default is "h2".
+ * You have to specify what level of heading you want, depending on the context you are using the heading in.
+ * You do this with the `as` prop. The options are h1, h2, h3, h4, h5 and h6.
  *
  * ```tsx
  * <Heading as="h1">Page heading</Heading>
  * ```
+ *
+ * You can specify the variant, which is one of "xs", "sm", "md", "lg", "xl-sans", "xs-serif" and "2xl". The default is "xl-sans".
+ *
+ * ```tsx
+ * <Heading as="h1" variant="2xl">Look at me!</Heading>
+ * ```
  */
-export const Heading = ({
-  variant = "xl-display",
-  as = "h2",
-  ...props
-}: any) => {
+export const Heading = ({ as, variant = "xl-display", ...props }: any) => {
   return <Text as={as} textStyle={variant} {...props} />;
 };

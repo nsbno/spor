8785dc438d176a4ee11c1fc4cbe799e81d922e5d
Mcklien Ross Onoya <76947297+mcklien@users.noreply.github.com>
2023-08-07 09:21:28 +0200

    eksportere typen til Chakra direkte

--- packages/spor-react/src/loader/SkeletonText.tsx
+++ packages/spor-react/src/loader/SkeletonText.tsx
@@ -1,9 +1,7 @@
-import { BoxProps, SkeletonText as ChakraSkeletonText, SkeletonProps } from "@chakra-ui/react";
+import { SkeletonText as ChakraSkeletonText, SkeletonTextProps as ChakraSkeletonTextProps } from "@chakra-ui/react";
 import React from "react";
 
-export type SkeletonTextProps = BoxProps & {
-  skeletonHeight?: SkeletonProps["height"]
-};
+export type SkeletonTextProps = ChakraSkeletonTextProps
   
 /**
  * SkeletonText renders a loading animation for a given text. It works great as a placeholder to avoid layout shifts.

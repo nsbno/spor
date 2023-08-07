2a9313a13e8372aecc32c041595131885d74b46b
Mcklien Ross Onoya <76947297+mcklien@users.noreply.github.com>
2023-08-07 09:29:26 +0200

    Revert "Legg til skeletonHeight"

--- packages/spor-react/src/loader/SkeletonText.tsx
+++ packages/spor-react/src/loader/SkeletonText.tsx
@@ -1,8 +1,7 @@
-import { SkeletonText as ChakraSkeletonText, SkeletonTextProps as ChakraSkeletonTextProps } from "@chakra-ui/react";
+import { BoxProps, SkeletonText as ChakraSkeletonText } from "@chakra-ui/react";
 import React from "react";
 
-export type SkeletonTextProps = ChakraSkeletonTextProps
-  
+export type SkeletonTextProps = BoxProps;
 /**
  * SkeletonText renders a loading animation for a given text. It works great as a placeholder to avoid layout shifts.
  */

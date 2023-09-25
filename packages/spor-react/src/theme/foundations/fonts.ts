9358f2a367c9eddc037ab3212863d99e4eb969d2
Kristofer Giltvedt Selbekk <selbekk@users.noreply.github.com>
2023-09-25 13:54:30 +0200

    Add fallback fonts (#849)

--- packages/spor-react/src/theme/foundations/fonts.ts
+++ packages/spor-react/src/theme/foundations/fonts.ts
@@ -1,7 +1,7 @@
 import tokens from "@vygruppen/spor-design-tokens";
 
 export const fonts = {
-  body: tokens.font.family.body,
-  heading: tokens.font.family.heading,
-  mono: tokens.font.family.monospace,
+  body: `${tokens.font.family.body}, sans-serif`,
+  heading: `${tokens.font.family.heading}, sans-serif`,
+  mono: `${tokens.font.family.monospace}, monospace`,
 };

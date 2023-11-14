07de1983465aa24122be50f531416c5b36fb3edd
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2023-11-14 15:29:22 +0100

    Fix list styling

--- packages/spor-react/src/theme/components/list.ts
+++ packages/spor-react/src/theme/components/list.ts
@@ -1,21 +1,21 @@
 import { listAnatomy as parts } from "@chakra-ui/anatomy";
-import {
-  createMultiStyleConfigHelpers,
-  defineStyle,
-} from "@chakra-ui/styled-system";
+import { createMultiStyleConfigHelpers } from "@chakra-ui/styled-system";
 
 const { defineMultiStyleConfig, definePartsStyle } =
   createMultiStyleConfigHelpers(parts.keys);
 
-const baseStyleIcon = defineStyle({
-  marginEnd: "2",
-  display: "inline",
-  verticalAlign: "text-bottom",
-  fontFamily: "body",
-});
-
 const baseStyle = definePartsStyle({
-  icon: baseStyleIcon,
+  container: {
+    fontSize: ["mobile.sm", "desktop.sm"],
+  },
+  item: {
+    fontFamily: "body",
+  },
+  icon: {
+    marginEnd: "2",
+    display: "inline",
+    verticalAlign: "text-bottom",
+  },
 });
 
 export default defineMultiStyleConfig({

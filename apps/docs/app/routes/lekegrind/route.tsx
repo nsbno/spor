dbd95302830741e33aa81903cfc8aaaef3776041
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2023-11-14 13:37:39 +0100

    Simplify the playground experience

--- apps/docs/app/routes/lekegrind/route.tsx
+++ apps/docs/app/routes/lekegrind/route.tsx
@@ -1,53 +1,9 @@
-import { ActionArgs, json, LoaderArgs } from "@remix-run/node";
-import { useFetcher, useLoaderData } from "@remix-run/react";
 import { Stack } from "@vygruppen/spor-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { LivePreview } from "react-live";
 import { LiveEditor } from "~/features/portable-text/interactive-code/LiveEditor";
 import { LiveError } from "~/features/portable-text/interactive-code/LiveError";
 import { LiveProvider } from "~/features/portable-text/interactive-code/LiveProvider";
-import { debounce } from "~/utils/debounce";
-import { getPlaygroundDataSession } from "~/utils/playgroundData.server";
-
-export const action = async ({ request }: ActionArgs) => {
-  const { setPlaygroundData, commit } = await getPlaygroundDataSession(request);
-  const formData = await request.formData();
-  const playgroundData = formData.get("playgroundData");
-  if (playgroundData === null) {
-    throw json({ error: "No playground data provided." }, { status: 400 });
-  }
-  if (typeof playgroundData !== "string") {
-    throw json(
-      {
-        error: `Invalid playground data type ${typeof playgroundData} provided`,
-      },
-      { status: 400 }
-    );
-  }
-  setPlaygroundData(playgroundData);
-  return json(
-    { success: true },
-    {
-      headers: {
-        "Set-Cookie": await commit(),
-      },
-    }
-  );
-};
-
-export const loader = async ({ request }: LoaderArgs) => {
-  const session = await getPlaygroundDataSession(request);
-  const playgroundData = session.getPlaygroundData();
-
-  return json(
-    { playgroundData },
-    {
-      headers: {
-        "Set-Cookie": await session.commit(),
-      },
-    }
-  );
-};
 
 const defaultCode = `<Stack textAlign="center">
   <Heading>Velkommen til lekegrinden</Heading>
@@ -55,25 +11,16 @@ const defaultCode = `<Stack textAlign="center">
   <Text>Alle komponentene er tilgjengelige, så du trenger ikke importere noe.</Text>
 </Stack>`;
 
-const saveToCookie = debounce((newCode: string, fetcher: any) => {
-  if (fetcher.state === "submitting") {
-    return;
-  }
-  const formData = new FormData();
-  formData.set("playgroundData", newCode);
-  fetcher.submit(formData, { method: "POST", action: "/lekegrind" });
-}, 2000);
-
 export default function PlaygroundPage() {
-  const { playgroundData: initialPlaygroundDataFromCookie } =
-    useLoaderData<typeof loader>();
-  const [playgroundData, setPlaygroundData] = useState(
-    initialPlaygroundDataFromCookie || defaultCode
-  );
-  const fetcher = useFetcher();
+  const [playgroundData, setPlaygroundData] = useState(() => "");
+  useEffect(() => {
+    const storedData = localStorage.getItem("playgroundData");
+    setPlaygroundData(defaultCode);
+  }, []);
+
   const handleChange = (newCode: string) => {
     setPlaygroundData(newCode);
-    saveToCookie(newCode, fetcher);
+    localStorage.setItem("playgroundData", newCode);
   };
   return (
     <LiveProvider code={playgroundData}>

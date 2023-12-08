f3394fb3c0db31fff8a4cd6b2f65510919733ab2
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2023-12-08 21:57:01 +0100

    Make DatePicker handle null values correctly

---
+++ .changeset/polite-eyes-laugh.md
@@ -0,0 +1,5 @@
+---
+"@vygruppen/spor-react": patch
+---
+
+DatePicker: Fix bug where field went from uncontrolled to controleld
--- packages/spor-react/src/datepicker/DateField.tsx
+++ packages/spor-react/src/datepicker/DateField.tsx
@@ -64,7 +64,7 @@ export const DateField = forwardRef<HTMLDivElement, DateFieldProps>(
         </Flex>
         <input
           type="hidden"
-          value={state.value?.toString()}
+          value={state.value?.toString() ?? ""}
           name={props.name}
         />
       </Box>

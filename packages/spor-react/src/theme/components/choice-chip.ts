05d4ee655ec987ef36dd86102cb889834c3581c6
ithen15 <hansjacoben@hotmail.com>
2024-02-07 12:06:42 +0100

    lagt til accent og refaktorert base

--- packages/spor-react/src/theme/components/choice-chip.ts
+++ packages/spor-react/src/theme/components/choice-chip.ts
@@ -1,6 +1,5 @@
 import { createMultiStyleConfigHelpers } from "@chakra-ui/react";
-import { anatomy, mode } from "@chakra-ui/theme-tools";
-import { getBoxShadowString } from "../utils/box-shadow-utils";
+import { anatomy } from "@chakra-ui/theme-tools";
 import { focusVisibleStyles } from "../utils/focus-util";
 import {
   accentBackground,
@@ -8,7 +7,7 @@ import {
   brandBackground,
 } from "../utils/background-utils";
 import { baseText, accentText } from "../utils/text-utils";
-import { baseBorder, accentBorder,brandBorder } from "../utils/border-utils";
+import { baseBorder, accentBorder, brandBorder } from "../utils/border-utils";
 
 const parts = anatomy("choice-chip").parts("container", "icon", "label");
 
@@ -17,9 +16,6 @@ const helpers = createMultiStyleConfigHelpers(parts.keys);
 const config = helpers.defineMultiStyleConfig({
   baseStyle: (props) => ({
     container: {
-      ...baseBackground("default", props),
-      ...baseBorder("default", props),
-      ...baseText("default", props),
       display: "inline-flex",
       alignItems: "center",
       fontSize: "16px",
@@ -37,29 +33,59 @@ const config = helpers.defineMultiStyleConfig({
           ...baseText("selected", props),
           ...brandBackground("active", props),
           ...brandBorder("active", props),
-        }
-      },
-      "input:focus-visible + &": focusVisibleStyles(props)._focusVisible,
-      "@media (hover:hover)": {
-        _hover: {
-          ...baseText("default", props),
-          ...baseBorder("hover", props),
-          ...baseBackground("hover", props),
-          cursor: "pointer",
         },
       },
-      _active: {
-        ...baseBackground("active", props),
-        ...baseBorder("default", props),
-      },
+      "input:focus-visible + &": focusVisibleStyles(props)._focusVisible,
     },
     icon: {
       mr: props.hasLabel ? 1 : 0,
     },
   }),
   variants: {
-    base: (props) => ({}),
-    accent: (props) => ({}),
+    base: (props) => ({
+      container: {
+        ...baseBackground("default", props),
+        ...baseBorder("default", props),
+        ...baseText("default", props),
+        "@media (hover:hover)": {
+          _hover: {
+            ...baseText("default", props),
+            ...baseBorder("hover", props),
+            ...baseBackground("hover", props),
+            cursor: "pointer",
+          },
+        },
+        _active: {
+          ...baseBackground("active", props),
+          ...baseBorder("default", props),
+        },
+      },
+    }),
+    accent: (props) => ({
+      container: {
+        ...accentBackground("default", props),
+        ...accentText("default", props),
+        ...accentBorder("default", props),
+        "@media (hover:hover)": {
+          _hover: {
+            ...accentBackground("hover", props),
+            ...accentBorder("hover", props),
+            ...accentText("default", props),
+          },
+        },
+        _active: {
+          ...accentText("default", props),
+          ...accentBorder("active", props),
+          ...accentBackground("active", props),
+        },
+      },
+      backgroundColor: "red",
+      _active: {
+        ...accentText("default", props),
+        ...accentBorder("active", props),
+        backgroundColor: "red",
+      },
+    }),
   },
   sizes: {
     sm: {

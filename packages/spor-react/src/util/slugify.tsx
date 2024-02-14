b2c6701959ef74bd6066b8dd43f3b12831550dc0
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2024-02-14 14:56:27 +0100

    Make the max length configurable

--- packages/spor-react/src/util/slugify.tsx
+++ packages/spor-react/src/util/slugify.tsx
@@ -1,12 +1,21 @@
 /**
- * Makes a slug-version of any string, with a maximum length of 50 characters
+ * Makes a slug-version of any string.
+ *
+ * By default, the maximum length of the slug is 50 characters. You can override this with the `maxLength` parameter.
+ *
+ * ```tsx
+ * slugify("Hello, world!"); // hello-world
+ * slugify("Hello, world!", 6); // hello-
  **/
-export function slugify(text: string | string[]): string {
+export function slugify(text: string | string[], maxLength = 50): string {
   if (!text) {
     return text;
   }
   if (Array.isArray(text)) {
     text = text.join(" ");
+  }
+  if (maxLength < 1) {
+    throw new Error("The maxLength parameter must be a positive number");
   }
   return (
     text
@@ -22,6 +31,6 @@ export function slugify(text: string | string[]): string {
       .replace(/--+/g, "-") // Replace multiple - with single -
       .replace(/^-+/, "") // Trim - from start of text
       .replace(/-+$/, "")
-      .substring(0, 50)
+      .substring(0, maxLength)
   );
 }

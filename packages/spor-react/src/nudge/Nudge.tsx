9f13908b1d38aedd2d82170c95ec3eddd167abf2
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2024-02-19 22:27:59 +0100

    Make the nudges look beautiful

--- packages/spor-react/src/nudge/Nudge.tsx
+++ packages/spor-react/src/nudge/Nudge.tsx
@@ -1,23 +1,93 @@
-import { Tooltip } from "@chakra-ui/react";
+import {
+  Box,
+  DarkMode,
+  Popover,
+  PopoverAnchor,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+} from "@chakra-ui/react";
 import React from "react";
+import { Button, ButtonGroup, createTexts, useTranslation } from "..";
 import { TooltipProps } from "../tooltip";
 
 export type NudgeProps = {
-  introducedTimestamp: number;
-  /** The name of the nudge */
+  /**
+   * The ISO-formatted date of when the Nudge was introduced.
+   *
+   * After this date has been surpassed by 30 days, the nudge will no longer be shown,
+   * and an error will be logged in development mode.
+   *
+   * ```tsx
+   * <Nudge introducedDate="2024-02-19" {...otherProps} />
+   * ```
+   **/
+  introducedDate: string;
+  /**
+   * The name of the nudge.
+   * This will be used to check whether or not the nudge has been viewed.
+   **/
   name: string;
-} & Omit<TooltipProps, "name">;
+  /**
+   * The anchor of the nudge.
+   *
+   * The element that should be nudged toward.
+   *
+   * ```tsx
+   * <Nudge content="This is a great new feature! Try it out.">
+   *  <MyFeature />
+   * </Nudge>
+   * ```
+   * */
+  children: React.ReactNode;
+  /**
+   * The content of the nudge.
+   */
+  content: React.ReactNode;
+  /**
+   * Any actions you want to provide. Defaults to a close button and a "Show me" button.
+   */
+  actions?: React.ReactNode;
+} & Omit<
+  TooltipProps,
+  | "name"
+  | "triggerElement"
+  | "children"
+  | "withCloseButton"
+  | "defaultIsOpen"
+  | "size"
+  | "borderRadius"
+>;
 
 const EXPIRATION_DELAY = 1000 * 60 * 60 * 24 * 30; // 30 days
 
 /** A nudge.
  *
- * A nudge is a tooltip that is shown to the user to guide them through a new feature.
+ * A nudge is a way to hint of a new feature.
  *
- * You're required to set an `introducedTimestamp` to the nudge, which is the timestamp of when the nudge was introduced. After 30 days, the nudge will no longer be shown, and an error will be logged in development mode.
+ * You're required to set an `introducedDate` to the nudge, which is the timestamp of when the nudge was introduced. After 30 days, the nudge will no longer be shown, and an error will be logged in development mode.
+ *
+ * ```tsx
+ * <Nudge
+ *   introducedDate="2024-02-19"
+ *  name="my-nudge"
+ *  content="Check out this enormous new feature!"
+ * >
+ *   <Card variant="base" padding={2} width="fit-content">My new feature</Card>
+ * </Nudge>
+ * ```
  */
-export const Nudge = ({ introducedTimestamp, name, ...props }: NudgeProps) => {
-  if (introducedTimestamp + EXPIRATION_DELAY > Date.now()) {
+export const Nudge = ({
+  introducedDate,
+  name,
+  children,
+  content,
+  actions,
+  ...props
+}: NudgeProps) => {
+  const { t } = useTranslation();
+  if (new Date(introducedDate).getTime() + EXPIRATION_DELAY < Date.now()) {
     if (process.env.NODE_ENV === "development") {
       console.warn(
         `The nudge ${name} has been used for longer than 30 days. Please remove it from the codebase.
@@ -27,5 +97,50 @@ export const Nudge = ({ introducedTimestamp, name, ...props }: NudgeProps) => {
     }
     return null;
   }
-  return <Tooltip {...props} />;
+  return (
+    <DarkMode>
+      <Popover
+        arrowSize={12}
+        arrowShadowColor="none"
+        defaultIsOpen={true}
+        {...props}
+      >
+        <PopoverAnchor>{children}</PopoverAnchor>
+        <PopoverContent borderRadius="sm">
+          <PopoverArrow />
+          <PopoverCloseButton />
+          <PopoverBody margin={1}>
+            <Box marginRight={4}>{content}</Box>
+            <Box marginTop={1.5}>
+              {actions ?? (
+                <ButtonGroup>
+                  <Button variant="tertiary" size="xs">
+                    {t(texts.close)}
+                  </Button>
+                  <Button variant="secondary" size="xs" fontWeight="bold">
+                    {t(texts.showMe)}
+                  </Button>
+                </ButtonGroup>
+              )}
+            </Box>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+    </DarkMode>
+  );
 };
+
+const texts = createTexts({
+  close: {
+    nb: "Lukk",
+    nn: "Lukk",
+    sv: "Stäng",
+    en: "Close",
+  },
+  showMe: {
+    nb: "Vis meg",
+    nn: "Vis meg",
+    sv: "Visa mig",
+    en: "Show me",
+  },
+});
--- packages/spor-react/src/nudge/WizardNudge.tsx
+++ packages/spor-react/src/nudge/WizardNudge.tsx
@@ -1,117 +1,72 @@
-import {
-  DarkMode,
-  Popover,
-  PopoverAnchor,
-  PopoverArrow,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverProps,
-  chakra,
-} from "@chakra-ui/react";
-
-import { PopoverBody, usePopoverContext } from "@chakra-ui/react";
+import { usePopoverContext } from "@chakra-ui/react";
 import { ArrowRightFill18Icon } from "@vygruppen/spor-icon-react";
-import React, { Children, useState } from "react";
+import React, { useState } from "react";
 import {
-  Box,
   Button,
   Flex,
+  Nudge,
+  NudgeProps,
   ProgressIndicator,
-  Stack,
   createTexts,
   useTranslation,
 } from "..";
 
-export type WizardNudgeProps = Omit<PopoverProps, "triggerElement"> & {
+export type WizardNudgeProps = Omit<NudgeProps, "actions" | "content"> & {
   /** Steps in the wizard. Each item is its own step. Should only be Step components */
-  children: React.ReactElement<WizardNudgeStepProps>[];
-  /** The element the wizard is anchored in */
-  anchorElement: React.ReactNode;
+  content: React.ReactNode[];
   /**
    * Where the nudge should be placed by default.
    *
    * Note - this is a suggestion, and may be overridden by space concerns.
    */
   placement?: "top" | "bottom" | "left" | "right";
-  /** Should the nudge have a close button? */
-  withCloseButton?: boolean;
 };
 /**
  * A nudge that displays its children one at a time, with a step indicator.
  *
- * Wrap each step in a `<WizardNudgeStep />` component.
- *
  * ```tsx
- * <WizardNudge triggerElement={<Button>Click me</Button>}>
- *   <WizardNudgeStep>
- *     <Text>First step</Text>
- *   </WizardNudgeStep>
- *   <WizardNudgeStep>
- *     <Text>Second step</Text>
- *   </WizardNudgeStep>
- *   <WizardNudgeStep>
- *     <Text>Last step</Text>
- *   </WizardNudgeStep>
+ * <WizardNudge
+ *  content={["First step", "Second step"]}
+ *  name="my-wizard-nudge"
+ *  introducedDate="2024-02-19"
+ * >
+ *   <Card width="fit-content" padding={2}>
+ *    My new feature
+ *  </Card>
  * </WizardNudge>
  * ```
  */
 export const WizardNudge = ({
   children,
-  anchorElementRef,
-  withCloseButton = false,
+  name,
+  onClose,
+  content,
+  ...props
 }: WizardNudgeProps) => {
-  const [isOpen, setOpen] = useState(true);
   const [currentStep, setCurrentStep] = useState(1);
-  const totalSteps = Children.count(children);
+  const totalSteps = content.length;
   const isLastStep = totalSteps === currentStep;
   const onNext = () => setCurrentStep((prev) => prev + 1);
 
-  const anchorRef = Array.isArray(anchorElementRef)
-    ? anchorElementRef[currentStep - 1]
-    : anchorElementRef;
-
   return (
-    <DarkMode>
-      <Popover size="lg" isOpen={isOpen} onClose={() => setOpen(false)}>
-        <PopoverAnchor>
-          <chakra.div
-            ref={anchorRef}
-            tabIndex={-1}
-            aria-hidden={true}
-            width="fit-content"
+    <Nudge
+      onClose={onClose}
+      name={name}
+      content={content[currentStep - 1]}
+      actions={
+        <Flex gap={3} alignItems="center">
+          <ProgressIndicator
+            activeStep={currentStep}
+            numberOfSteps={totalSteps}
           />
-        </PopoverAnchor>
-        <PopoverContent>
-          <PopoverArrow />
-          {withCloseButton && <PopoverCloseButton />}
-          <PopoverBody>
-            <Stack spacing={1.5}>
-              <Box>{Children.toArray(children)[currentStep - 1]}</Box>
-              <Flex gap={3} alignItems="center">
-                <ProgressIndicator
-                  activeStep={currentStep}
-                  numberOfSteps={totalSteps}
-                />
-                <NextOrCloseButton isLastStep={isLastStep} onNext={onNext} />
-              </Flex>
-            </Stack>
-          </PopoverBody>
-        </PopoverContent>
-      </Popover>
-    </DarkMode>
+          <NextOrCloseButton isLastStep={isLastStep} onNext={onNext} />
+        </Flex>
+      }
+      {...props}
+    >
+      {children}
+    </Nudge>
   );
-};
-
-type WizardNudgeStepProps = {
-  children: React.ReactNode;
-};
-/** A step in a WizardNudge.
- *
- * This component doesn't do anything special, except making things a bit more
- * explicit code-wise. It just renders a div.
- */
-export const WizardNudgeStep = ({ children }: WizardNudgeStepProps) => {
-  return <Box>{children}</Box>;
 };
 
 type NextOrCloseButtonProps = {
@@ -124,7 +79,7 @@ const NextOrCloseButton = ({ isLastStep, onNext }: NextOrCloseButtonProps) => {
   return (
     <Button
       variant="tertiary"
-      size="sm"
+      size="xs"
       color="white"
       minWidth="fit-content"
       leftIcon={isLastStep ? undefined : <ArrowRightFill18Icon />}

8c4e9f65bfa1cef3eaa6f094e7ecc2c80db793a3
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2024-03-08 22:58:04 +0100

    Use the new tokens for focus styles

--- packages/spor-react/src/theme/utils/focus-util.ts
+++ packages/spor-react/src/theme/utils/focus-util.ts
@@ -3,7 +3,7 @@ import { mode } from "@chakra-ui/theme-tools";
 export const focusVisibleStyles = (props: any) => ({
   _focusVisible: {
     outlineWidth: "2px",
-    outlineColor: mode("greenHaze", "azure")(props),
+    outlineColor: mode("outline.focus.light", "outline.focus.dark")(props),
     outlineStyle: "solid",
     outlineOffset: "1px",
   },

11f05dc44bb84e89a63c99d24429179e854fa8f6
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2024-03-19 13:32:48 +0100

    Add arial as a fallback font

--- packages/spor-react/src/theme/foundations/fonts.ts
+++ packages/spor-react/src/theme/foundations/fonts.ts
@@ -1,7 +1,7 @@
 import tokens from "@vygruppen/spor-design-tokens";
 
 export const fonts = {
-  body: `${tokens.font.family.body}, sans-serif`,
-  heading: `${tokens.font.family.heading}, sans-serif`,
+  body: `${tokens.font.family.body}, arial, sans-serif`,
+  heading: `${tokens.font.family.heading}, arial, sans-serif`,
   mono: `${tokens.font.family.monospace}, monospace`,
 };

9571397a1383a0c251718e5f48b00cc3a3705f83
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2024-03-22 14:51:03 +0100

    Add CargoNet brand

---
+++ .changeset/late-buckets-travel.md
@@ -0,0 +1,6 @@
+---
+"@vygruppen/spor-design-tokens": minor
+"@vygruppen/spor-react": minor
+---
+
+Add support for Brand.CargoNet
--- packages/spor-react/src/theme/index.ts
+++ packages/spor-react/src/theme/index.ts
@@ -1,10 +1,12 @@
 import { theme as defaultTheme } from "@chakra-ui/theme";
+import tokens from "@vygruppen/spor-design-tokens";
 import * as components from "./components";
 import * as foundations from "./foundations";
 
 export enum Brand {
   VyDigital = "VyDigital",
   VyUtvikling = "VyUtvikling",
+  CargoNet = "CargoNet",
 }
 
 export const theme = {
@@ -23,12 +25,14 @@ export const brandTheme = {
       accent: {
         surface: {
           default: {
-            light: foundations.colors.seaMist,
             dark: foundations.colors.darkGrey,
           },
         },
       },
     },
+  },
+  [Brand.CargoNet]: {
+    colors: tokens.color.cargonet as any,
   },
 };
 

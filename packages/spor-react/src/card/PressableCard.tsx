b80706736d62f23f87ef95979f824ae959d49d4a
KarlAmdam <Karl.Amdam@Gmail.com>
2024-04-12 13:00:16 +0200

    changed description for pressable card

--- packages/spor-react/src/card/PressableCard.tsx
+++ packages/spor-react/src/card/PressableCard.tsx
@@ -11,12 +11,20 @@ type PressableCardProps = Omit<BoxProps, "as"> & {
  * The most basic version looks like this:
  *
  * ```tsx
- * <PressableCard colorScheme="white">
+ * <PressableCard>
  *   Content
  * </PressableCard>
  * ```
  *
- *Pressable cards can also be rendered as whatever DOM element you want – like a li (list item) or an article. You do this by specifying the `as` prop:
+ * There are lots of color schemes available. You can also set the size as either `sm` or `lg`. The default is `sm`.
+ *
+ * ```tsx
+ * <PressableCard colorScheme="orange" size="lg">
+ *   A smaller card
+ * </PressableCard>
+ * ```
+ *
+ * Pressable cards can also be rendered as button, link or label – like a li (list item) or an article. You do this by specifying the `as` prop:
  *
  * ```tsx
  * <PressableCard colorScheme="green" as="section">

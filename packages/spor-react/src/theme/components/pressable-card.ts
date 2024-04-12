281f7c8d7f1cf0438b9a9d2f05a3a3b95ce8c8a1
KarlAmdam <Karl.Amdam@Gmail.com>
2024-04-12 08:37:46 +0200

    pressable card wip

--- packages/spor-react/src/theme/components/pressable-card.ts
+++ packages/spor-react/src/theme/components/pressable-card.ts
@@ -1,7 +1,8 @@
 import { defineStyleConfig } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 import { colors } from "../foundations";
-import { baseBorder } from "../utils/base-utils";
+import { baseBackground, baseBorder, baseText } from "../utils/base-utils";
+import { floatingBorder } from "../utils/floating-utils";
 
 const config = defineStyleConfig({
   baseStyle: (props: any) => ({
@@ -14,6 +15,15 @@ const config = defineStyleConfig({
     // Except for white cards, all cards are light mode always
     color: "text.default.light",
     ...getColorSchemeBaseProps(props),
+    ...getColorSchemeClickableProps(props),
+    ...getColorSchemeActiveProps(props),
+    _hover: getColorSchemeHoverProps(props),
+    _disabled: {
+      ...baseBackground("disabled", props),
+      ...baseBorder("disabled", props),
+      ...baseText("disabled", props),
+      pointerEvents: "none",
+    },
   }),
 });
 
@@ -61,6 +71,66 @@ const getColorSchemeBaseProps = (props: CardThemeProps) => {
     default:
       return {
         backgroundColor: colors[props.colorScheme]?.[100] ?? "platinum",
+      };
+  }
+};
+
+function getColorSchemeClickableProps(props: CardThemeProps) {
+  switch (props.colorScheme) {
+    case "white":
+      return {
+        ...floatingBorder("default", props),
+      };
+    case "grey":
+      return {
+        outlineColor: "steel",
+      };
+    default:
+      return {
+        backgroundColor: colors[props.colorScheme]?.[100] ?? "platinum",
+        outlineColor: colors[props.colorScheme]?.[200] ?? "silver",
+      };
+  }
+}
+
+const getColorSchemeHoverProps = (props: CardThemeProps) => {
+  switch (props.colorScheme) {
+    case "white":
+      return {
+        backgroundColor: mode(
+          "white",
+          `color-mix(in srgb, white 20%, var(--spor-colors-bg-default-dark))`,
+        )(props),
+        ...floatingBorder("hover", props),
+      };
+    case "grey":
+      return {
+        outlineColor: "osloGrey",
+      };
+    default:
+      return {
+        backgroundColor: colors[props.colorScheme]?.[200] ?? "silver",
+        outlineColor: colors[props.colorScheme]?.[400] ?? "silver",
+      };
+  }
+};
+const getColorSchemeActiveProps = (props: CardThemeProps) => {
+  const { colorScheme } = props;
+  switch (colorScheme) {
+    case "white":
+      return {
+        backgroundColor: mode("bg.tertiary.light", `bg.default.dark`)(props),
+        ...floatingBorder("active", props),
+      };
+    case "grey":
+      return {
+        backgroundColor: "white",
+        outlineColor: "steel",
+      };
+    default:
+      return {
+        backgroundColor: colors[colorScheme]?.[50] ?? "lightGrey",
+        outlineColor: colors[colorScheme]?.[100] ?? "silver",
       };
   }
 };

6fe75c630e97db059a949cc4c40991e8c3be32cd
Alice Clausen <42611957+alicemacl@users.noreply.github.com>
2024-05-16 12:23:47 +0200

    Add title on ClosableAlert

--- packages/spor-react/src/alert/BaseAlert.tsx
+++ packages/spor-react/src/alert/BaseAlert.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps, useMultiStyleConfig } from "@chakra-ui/react";
+import { Box, BoxProps, Flex, useMultiStyleConfig } from "@chakra-ui/react";
 import React from "react";
 
 export type BaseAlertProps = BoxProps & {

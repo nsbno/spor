82edee9e52eb0794c4daa748ef41701d7989c509
faergestad <georg.f.persen@gmail.com>
2024-07-30 15:26:06 +0200

    test terraform module bugfix for auto scaling configuration name

--- apps/docs/terraform/template/main.tf
+++ apps/docs/terraform/template/main.tf
@@ -46,7 +46,7 @@ resource "aws_ssm_parameter" "sanity_preview_secret" {
 #                                #
 ##################################
 module "app_runner" {
-  source                = "github.com/nsbno/terraform-digitalekanaler-modules//apprunner-internal?ref=2.2.3"
+  source                = "github.com/nsbno/terraform-digitalekanaler-modules?ref=autoscaling-name-bug-fix//apprunner-internal"
   application_port      = 3000
   application_name      = var.application_name
   ecr_repository_name   = var.application_name

39f1531af1bf05f981114e7612b2baad5822011b
Marte Solli Vågen <marte.vagen@vy.no>
2024-08-08 13:51:45 +0200

    Removed edit to service-variant

--- packages/spor-react/src/theme/components/alert-expandable.ts
+++ packages/spor-react/src/theme/components/alert-expandable.ts
@@ -73,7 +73,6 @@ const config = helpers.defineMultiStyleConfig({
     },
     service: {
       container: {
-        paddingX: 0,
         _hover: {
           outlineColor: "blueGreen",
         },

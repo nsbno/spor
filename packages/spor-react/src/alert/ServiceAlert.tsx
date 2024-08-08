1a33244a926ce667a28dc5f294c057e14af63130
Marte Solli Vågen <marte.vagen@vy.no>
2024-08-08 13:53:15 +0200

    Updated design

--- packages/spor-react/src/alert/ServiceAlert.tsx
+++ packages/spor-react/src/alert/ServiceAlert.tsx
@@ -6,6 +6,7 @@ import {
     AccordionPanel,
     Box,
     Flex,
+    Stack,
     Text,
     useMultiStyleConfig,
   } from "@chakra-ui/react";
@@ -19,6 +20,10 @@ import {
     title: string;
     /** The number of notifications when there is a list of multiple alerts */
     notification: number;
+    /** The maximum width to display the service message 
+     * 
+     * Defaults to container.md */
+    contentWidth: string
     /** Callback for when the expandable panel is opened or closed */
     onToggle?: (isOpen: boolean) => void;
     /** Whether or not the default state of the alert is open */
@@ -35,8 +40,8 @@ import {
    * A regular alert with an expandable body, used to show service messages. The expandable body can be used to provide more information about the alert(s).
    *
    * ```tsx
-   * <ServiceAlert variant="service" title="Error with Vipps">
-   *   Some customers are experiencing issues logging in with Vipps. Vipps is working to resolve the issue. Try logging in with email instead.
+   * <ServiceAlert title="Error with Vipps" notification={1} contentWidth="container.md">
+   *   <Text>Some customers are experiencing issues logging in with Vipps. Vipps is working to resolve the issue. Try logging in with email instead.</Text>
    * </ServiceAlert>
    * ```
    */
@@ -45,6 +50,7 @@ import {
     children,
     title,
     notification,
+    contentWidth = "container.md",
     headingLevel = "h3",
     defaultOpen = false,
     onToggle = () => {},
@@ -52,59 +58,75 @@ import {
   }: ServiceAlertProps) => {
     variant = "service";
     const { t } = useTranslation();
-    const styles = useMultiStyleConfig("AlertExpandable", { variant });
+    const styles = useMultiStyleConfig("AlertService");
     return (
-      <BaseAlert variant={variant} {...boxProps} paddingX={0} paddingY={0}>
-        <Accordion
-          onChange={(expandedIndex) => onToggle(expandedIndex === 0)}
-          defaultIndex={defaultOpen ? 0 : -1}
-          allowToggle
-          flexGrow="1"
-        >
-          <AccordionItem>
-            <AccordionButton sx={styles.container} >
+      <Box flexDirection="column" sx={styles.box}>
+        <BaseAlert variant={variant} {...boxProps} paddingX={0} paddingY={0} sx={styles.box}>
+          <Accordion
+            onChange={(expandedIndex) => onToggle(expandedIndex === 0)}
+            defaultIndex={defaultOpen ? 0 : -1}
+            allowToggle
+            flexGrow="1"
+          >
+            <AccordionItem>
+
+              <AccordionButton sx={styles.container}>
+                <Stack flexDirection="row" justifyContent="center" width="100%" paddingX="12px">
+
+                  <Flex
+                    justifyContent="space-between"
+                    alignItems="center"
+                    flexGrow="1"
+                    maxWidth={contentWidth}
+                  >
+                    <Flex as={headingLevel} alignItems="center">
+                      <AlertIcon variant={variant} />
+      
+                      <Box
+                        as="span"
+                        sx={{
+                          // Truncate the title to one line
+                          display: "-webkit-box",
+                          overflow: "hidden",
+                          "-webkit-line-clamp": "1",
+                          "-webkit-box-orient": "vertical",
+                        }}
+                        color="white"
+                      >
+                        {title}
+                      </Box>
+                    </Flex>
+      
+                    <Flex alignItems="center">
+                      {notification && <Text sx={styles.notificationText}>
+                        {t(texts.notification(notification))}
+                      </Text>}
+      
+                    <AccordionIcon color="white" />
+                    </Flex>
+                    
+                    
+                  </Flex>
+                </Stack>
+              </AccordionButton>
+
+              <AccordionPanel sx={styles.serviceMessageContent}>
+              <Stack flexDirection="row" justifyContent="center" width="100%" paddingX="12px">
               <Flex
-                justifyContent="space-between"
-                alignItems="center"
-                flexGrow="1"
-              >
-                <Flex as={headingLevel} alignItems="center">
-                  <AlertIcon variant={variant} />
-  
-                  <Box
-                    as="span"
-                    sx={{
-                      // Truncate the title to one line
-                      display: "-webkit-box",
-                      overflow: "hidden",
-                      "-webkit-line-clamp": "1",
-                      "-webkit-box-orient": "vertical",
-                    }}
-                    color="white"
-                  >
-                    {title}
-                  </Box>
+                  justifyContent="space-between"
+                  alignItems="center"
+                  flexGrow="1"
+                  maxWidth={contentWidth}
+                >
+                <Stack >
+                {children}</Stack>
                 </Flex>
-  
-                <Flex alignItems="center">
-                  {notification && <Text 
-                    color="white"
-                    fontWeight={400}
-                    fontSize={16}
-                    pr="6px"
-                  >
-                    {t(texts.notification(notification))}
-                  </Text>}
-  
-                <AccordionIcon color="white" />
-                </Flex>
-                
-              </Flex>
-            </AccordionButton>
-            <AccordionPanel color="white">{children}</AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      </BaseAlert>
+              </Stack></AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        </BaseAlert>
+
+      </Box>    
     );
   };
   

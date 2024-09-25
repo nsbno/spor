0650c0058f13bbe065875e97a9ac029fb99b8d34
Alice Clausen <42611957+alicemacl@users.noreply.github.com>
2024-09-25 15:33:26 +0200

    Update styling for divider

--- packages/spor-react/src/theme/components/divider.ts
+++ packages/spor-react/src/theme/components/divider.ts
@@ -1,42 +1,40 @@
-import { defineStyle, defineStyleConfig } from "@chakra-ui/styled-system";
+import { defineStyleConfig } from "@chakra-ui/styled-system";
 import { mode } from "@chakra-ui/theme-tools";
 
-const baseStyle = defineStyle((props) => ({
-  borderColor: mode("blackAlpha.300", "whiteAlpha.300")(props),
-}));
-
-const variantSolid = defineStyle({
-  borderStyle: "solid",
-});
-
-const variantDashed = defineStyle({
-  borderStyle: "dashed",
-});
-
-const variants = {
-  solid: variantSolid,
-  dashed: variantDashed,
-};
-
-const sizes = {
-  sm: {
-    borderWidth: "1px",
-    borderRadius: "0.5px",
-  },
-  md: {
-    borderWidth: "2px",
-    borderRadius: "1px",
-  },
-  lg: {
-    borderWidth: "3px",
-    borderRadius: "1.5px",
-  },
-};
-
 export default defineStyleConfig({
-  baseStyle,
-  variants,
-  sizes,
+  baseStyle: (props) => ({
+    borderColor: mode("blackAlpha.300", "whiteAlpha.300")(props),
+  }),
+  variants: {
+    solid: {
+      borderStyle: "solid",
+    },
+    dashed: (props) => ({
+      backgroundImage: `repeating-linear-gradient(90deg, ${mode("blackAlpha.300", "whiteAlpha.300")(props)}, ${mode("blackAlpha.300", "whiteAlpha.300")(props)} 4px, transparent 4px, transparent 10px)`,
+      backgroundPosition: "left bottom",
+      backgroundRepeat: "repeat-x",
+      backgroundSize: "100% 3px",
+      borderRadius:
+        props.size === "sm" ? "0.5px" : props.size === "md" ? "1px" : "1.5px",
+    }),
+  },
+  sizes: {
+    sm: (props) => ({
+      borderWidth: props.variant === "solid" ? "1px" : undefined,
+      borderRadius: props.variant === "solid" ? "0.5px" : undefined,
+      height: props.variant === "dashed" ? "1px" : undefined,
+    }),
+    md: (props) => ({
+      borderWidth: props.variant === "solid" ? "2px" : undefined,
+      borderRadius: props.variant === "solid" ? "1px" : "10px",
+      height: props.variant === "dashed" ? "2px" : undefined,
+    }),
+    lg: (props) => ({
+      borderWidth: props.variant === "solid" ? "3px" : undefined,
+      borderRadius: props.variant === "solid" ? "1.5px" : undefined,
+      height: props.variant === "dashed" ? "3px" : undefined,
+    }),
+  },
   defaultProps: {
     variant: "solid",
     size: "md",

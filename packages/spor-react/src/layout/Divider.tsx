e25a7f651ad8769dd23dc5586b6934a51fcdeee8
Alice Clausen <42611957+alicemacl@users.noreply.github.com>
2024-11-26 16:44:34 +0100

    Add styles to divider

--- packages/spor-react/src/layout/Divider.tsx
+++ packages/spor-react/src/layout/Divider.tsx
@@ -1,14 +1,15 @@
-import { Box, BoxProps, RecipeVariantProps } from "@chakra-ui/react";
+import { Box, BoxProps, RecipeVariantProps, useRecipe } from "@chakra-ui/react";
 import React, { forwardRef, PropsWithChildren } from "react";
 import { dividerRecipe } from "../theme/components";
 
 type DividerVariantProps = RecipeVariantProps<typeof dividerRecipe>;
 
-export type DividerProps = PropsWithChildren<DividerVariantProps> & {
-  size?: "sm" | "md" | "lg";
-  variant?: "solid" | "dashed";
-  orientation?: "horizontal" | "vertical";
-};
+export type DividerProps = PropsWithChildren<DividerVariantProps> &
+  BoxProps & {
+    size?: "sm" | "md" | "lg";
+    variant?: "solid" | "dashed";
+    orientation?: "horizontal" | "vertical";
+  };
 /** A dividing line, used to divide content.
  *
  * You can specify margins if you need to give the content some space, or use a `Stack` component to do it for you
@@ -20,6 +21,15 @@ export type DividerProps = PropsWithChildren<DividerVariantProps> & {
  * There are three different sizes available: `sm`, `md` and `lg`. The default is `md`.
  * There are two different variants available: `solid` and `dashed`. The default is `solid`.
  */
-export const Divider = forwardRef<HTMLDivElement, BoxProps>((props, ref) => {
-  return <Box as="hr" {...props} ref={ref} />;
-});
+export const Divider = forwardRef<HTMLDivElement, DividerProps>(
+  (props, ref) => {
+    const {
+      size = "md",
+      variant = "solid",
+      orientation = "horizontal",
+    } = props;
+    const recipe = useRecipe({ recipe: dividerRecipe });
+    const styles = recipe({ size, variant, orientation });
+    return <Box as="hr" css={styles} {...props} ref={ref} />;
+  },
+);

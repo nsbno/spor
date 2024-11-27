ef951f964b71df1b8c92de5ac6db8bd940464ee3
cibietici <carloalberto.burato@gmail.com>
2024-11-27 10:23:00 +0100

    Fix css styling

--- packages/spor-react/src/theme/components/close-button.ts
+++ packages/spor-react/src/theme/components/close-button.ts
@@ -1,48 +1,54 @@
-import { defineStyleConfig } from "@chakra-ui/react";
-import { cssVar, mode } from "@chakra-ui/theme-tools";
+import { defineRecipe } from "@chakra-ui/react";
+import { useColorModeValue } from "../../color-mode";
+import { cssVar } from "@chakra-ui/theme-tools";
 import { focusVisibleStyles } from "../utils/focus-utils";
 import { ghostBackground } from "../utils/ghost-utils";
 
 const $size = cssVar("close-button-size");
 
-const config = defineStyleConfig({
-  baseStyle: (props) => ({
+const config = defineRecipe({
+  base: {
     w: [$size.reference],
     h: [$size.reference],
     transitionProperty: "common",
     transitionDuration: "normal",
     borderRadius: "md",
     backgroundColor: "transparent",
-    color: mode("darkGrey", "white")(props),
+    color: useColorModeValue("darkGrey", "white"),
     fontWeight: "normal",
-    ...focusVisibleStyles(props),
+    ...focusVisibleStyles(),
     _hover: {
-      ...ghostBackground("hover", props),
+      ...ghostBackground("hover"),
       _disabled: {
         color: "dimGrey",
       },
     },
     _active: {
-      ...ghostBackground("active", props),
-    },
-  }),
-  sizes: {
-    lg: {
-      [$size.variable]: "40px",
-      fontSize: "xs",
-    },
-    md: {
-      [$size.variable]: "32px",
-      fontSize: "0.75rem",
-    },
-    sm: {
-      [$size.variable]: "24px",
-      fontSize: "0.625rem",
+      ...ghostBackground("active"),
     },
   },
-  defaultProps: {
-    size: "md",
+  variants: {
+    size: {
+      lg: {
+        width: "40px",
+        fontSize: "xs",
+      },
+      md: {
+        width: "32px",
+        fontSize: "0.75rem",
+      },
+      sm: {
+        width: "24px",
+        fontSize: "0.625rem",
+      },
+    },
   },
+  compoundVariants: [
+    {
+      size: "md",
+      css: {},
+    },
+  ],
 });
 
 export default config;

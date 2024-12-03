ae85009f7a88cc415bfd987f7839ca7b809d270c
Alice Clausen <42611957+alicemacl@users.noreply.github.com>
2024-12-03 13:09:42 +0100

    Update tabs

--- packages/spor-react/src/tab/index.tsx
+++ packages/spor-react/src/tab/index.tsx
@@ -1,9 +1,1 @@
-export { Tab, TabList, TabPanel, TabPanels } from "@chakra-ui/react";
-export type {
-  TabListProps,
-  TabPanelProps,
-  TabPanelsProps,
-  TabProps,
-} from "@chakra-ui/react";
-export { Tabs } from "./Tabs";
-export type { TabsProps } from "./Tabs";
+export * from "./Tabs";

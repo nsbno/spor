5894783a24abd2fd05c68970ef1ddceaea7ad896
Alice Clausen <42611957+alicemacl@users.noreply.github.com>
2024-12-03 14:29:34 +0100

    Add style to popover

--- packages/spor-react/src/popover/index.tsx
+++ packages/spor-react/src/popover/index.tsx
@@ -1,9 +1,19 @@
-import { BoxProps, Popover as ChakraPopover, Portal } from "@chakra-ui/react";
+import {
+  BoxProps,
+  Popover as ChakraPopover,
+  Portal,
+  RecipeVariantProps,
+} from "@chakra-ui/react";
 import { CloseButton } from "../components/ui/close-button";
 import * as React from "react";
+import { popoverSlotRecipe } from "../theme/components/popover";
+import { PropsWithChildren } from "react";
+
+type PopoverVariantProps = RecipeVariantProps<typeof popoverSlotRecipe>;
 
 type PopoverContentProps = ChakraPopover.ContentProps &
-  BoxProps & {
+  BoxProps &
+  PropsWithChildren<PopoverVariantProps> & {
     portalled?: boolean;
     portalRef?: React.RefObject<HTMLElement>;
     children?: React.ReactNode;

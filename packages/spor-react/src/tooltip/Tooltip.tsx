dcb2c7baabb94ee811a56646b816bdb10732ef7a
Alice Clausen <42611957+alicemacl@users.noreply.github.com>
2024-12-03 14:30:31 +0100

    Chakra 3 - Tooltip (#1385)
    * Update changeset
    
    * Fix Tooltip and Nudge, add Popover
    
    * remove old documentation
    
    * Update props and changeset
    
    * update changeset and style
    
    * Add style to popover

--- packages/spor-react/src/tooltip/Tooltip.tsx
+++ packages/spor-react/src/tooltip/Tooltip.tsx
@@ -1,29 +1,23 @@
+import React, { forwardRef } from "react";
+import { PopoverRootProps as ChakraPopoverRootProps } from "@chakra-ui/react";
 import {
-  Popover,
   PopoverArrow,
   PopoverBody,
-  PopoverCloseButton,
+  PopoverCloseTrigger,
   PopoverContent,
-  PopoverProps,
+  PopoverRoot,
   PopoverTrigger,
-} from "@chakra-ui/react";
-import React from "react";
+} from "../popover";
 
-export type TooltipProps = PopoverProps & {
+export type TooltipProps = ChakraPopoverRootProps & {
   /**
    * Whatever is supposed to trigger the tooltip.
    * Must be focusable - like a link or button */
   children: React.ReactNode;
-  /** Callback for when the tooltip is requested to close */
-  onClose?: () => void;
   /** Should the tooltip have a close button? */
   withCloseButton?: boolean;
   /** The content of the tooltip */
   content: React.ReactNode;
-  /** Use this prop if you want to control the open state */
-  isOpen?: boolean;
-  /** Whether or not the tooltip is open by default */
-  defaultIsOpen?: boolean;
   /**
    * Where the tooltip should be placed by default.
    *
@@ -37,34 +31,31 @@ export type TooltipProps = PopoverProps & {
   size?: "sm" | "lg";
 };
 /** A tooltip component. */
-export const Tooltip = ({
-  children,
-  content,
-  onClose,
-  isOpen,
-  defaultIsOpen,
-  placement = "bottom",
-  size = "sm",
-  withCloseButton = false,
-  ...props
-}: TooltipProps) => {
-  return (
-    <Popover
-      onClose={onClose}
-      isOpen={isOpen}
-      defaultIsOpen={defaultIsOpen}
-      placement={placement}
-      size={size}
-      arrowSize={12}
-      arrowShadowColor="none"
-      {...props}
-    >
-      <PopoverTrigger>{children}</PopoverTrigger>
-      <PopoverContent>
-        <PopoverArrow />
-        {withCloseButton && <PopoverCloseButton />}
-        <PopoverBody>{content}</PopoverBody>
-      </PopoverContent>
-    </Popover>
-  );
-};
+export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
+  function Tooltip(props, ref) {
+    const {
+      children,
+      content,
+      placement = "bottom",
+      size = "sm",
+      withCloseButton = false,
+    } = props;
+    return (
+      <PopoverRoot
+        placement={placement}
+        size={size}
+        arrowSize={12}
+        arrowShadowColor="none"
+        ref={ref}
+        {...props}
+      >
+        <PopoverTrigger>{children}</PopoverTrigger>
+        <PopoverContent>
+          <PopoverArrow />
+          {withCloseButton && <PopoverCloseTrigger />}
+          <PopoverBody>{content}</PopoverBody>
+        </PopoverContent>
+      </PopoverRoot>
+    );
+  },
+);

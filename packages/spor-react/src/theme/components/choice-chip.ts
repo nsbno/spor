c67eda21a7447350c72fee2733c7b3e338c1ad41
Alice Clausen <42611957+alicemacl@users.noreply.github.com>
2024-12-04 12:14:33 +0100

    Fix ChoiceChip

--- packages/spor-react/src/theme/components/choice-chip.ts
+++ packages/spor-react/src/theme/components/choice-chip.ts
@@ -1,18 +1,15 @@
-import { createMultiStyleConfigHelpers } from "@chakra-ui/react";
-import { anatomy } from "@chakra-ui/theme-tools";
+import { defineSlotRecipe } from "@chakra-ui/react";
 import { accentBackground, accentText } from "../utils/accent-utils";
 import { baseBackground, baseBorder, baseText } from "../utils/base-utils";
 import { brandBackground } from "../utils/brand-utils";
 import { floatingBackground, floatingBorder } from "../utils/floating-utils";
 import { focusVisibleStyles } from "../utils/focus-utils";
 
-const parts = anatomy("choice-chip").parts("container", "icon", "label");
-
-const helpers = createMultiStyleConfigHelpers(parts.keys);
-
-const config = helpers.defineMultiStyleConfig({
-  baseStyle: (props) => ({
-    container: {
+export const choiceChipSlotRecipe = defineSlotRecipe({
+  slots: ["root", "icon", "label"],
+  className: "spor-choice-chip",
+  base: {
+    root: {
       display: "inline-flex",
       alignItems: "center",
       fontSize: "xs",
@@ -22,140 +19,142 @@ const config = helpers.defineMultiStyleConfig({
       transitionDuration: "fast",
       _checked: {
         outlineColor: "transparent",
-        ...accentText("selected", props),
-        ...accentBackground("selected", props),
+        ...accentText("selected"),
+        ...accentBackground("selected"),
         _hover: {
-          ...brandBackground("hover", props),
-          ...baseText("selected", props),
+          ...brandBackground("hover"),
+          ...baseText("selected"),
           outlineColor: "transparent",
         },
         _active: {
-          ...baseText("selected", props),
-          ...brandBackground("active", props),
+          ...baseText("selected"),
+          ...brandBackground("active"),
         },
       },
       _disabled: {
         pointerEvents: "none",
         boxShadow: "none",
-        ...baseText("disabled", props),
-        ...baseBackground("disabled", props),
+        ...baseText("disabled"),
+        ...baseBackground("disabled"),
         _hover: {
-          ...baseBackground("disabled", props),
+          ...baseBackground("disabled"),
           boxShadow: "none",
-          ...baseText("disabled", props),
+          ...baseText("disabled"),
         },
         _checked: {
           cursor: "not-allowed",
           boxShadow: "none",
-          ...baseText("disabled", props),
-          ...baseBackground("disabled", props),
+          ...baseText("disabled"),
+          ...baseBackground("disabled"),
           _hover: {
-            ...baseBackground("disabled", props),
+            ...baseBackground("disabled"),
             boxShadow: "none",
-            ...baseText("disabled", props),
+            ...baseText("disabled"),
           },
         },
       },
-      "input:focus-visible + &": focusVisibleStyles(props)._focusVisible,
+      "input:focus-visible + &": focusVisibleStyles()._focusVisible,
     },
-    icon: {
-      marginRight: props.hasLabel ? 1 : 0,
+    icon: {},
+    label: {
+      marginLeft: 1,
     },
-  }),
+  },
   variants: {
-    base: (props) => ({
-      container: {
-        ...baseBorder("default", props),
-        ...baseText("default", props),
-        _hover: {
-          ...baseText("default", props),
-          ...baseBorder("hover", props),
+    variant: {
+      base: {
+        root: {
+          ...baseBorder("default"),
+          ...baseText("default"),
+          _hover: {
+            ...baseText("default"),
+            ...baseBorder("hover"),
+          },
+          _active: {
+            ...baseBackground("active"),
+            ...baseBorder("default"),
+          },
+        },
+      },
+      accent: {
+        root: {
+          ...accentBackground("default"),
+          ...accentText("default"),
+          _hover: {
+            ...accentBackground("hover"),
+            ...accentText("default"),
+          },
+          _active: {
+            ...accentText("default"),
+            ...accentBackground("active"),
+          },
         },
         _active: {
-          ...baseBackground("active", props),
-          ...baseBorder("default", props),
+          ...accentText("default"),
+          ...accentBackground("active"),
         },
       },
-    }),
-    accent: (props) => ({
-      container: {
-        ...accentBackground("default", props),
-        ...accentText("default", props),
-        _hover: {
-          ...accentBackground("hover", props),
-          ...accentText("default", props),
-        },
-        _active: {
-          ...accentText("default", props),
-          ...accentBackground("active", props),
-        },
-      },
-      _active: {
-        ...accentText("default", props),
-        ...accentBackground("active", props),
-      },
-    }),
-    floating: (props) => ({
-      container: {
-        ...floatingBackground("default", props),
-        ...baseText("default", props),
-        ...floatingBorder("default", props),
-        boxShadow: "sm",
-        _hover: {
-          ...floatingBackground("hover", props),
-          ...floatingBorder("hover", props),
-          ...baseText("default", props),
-          boxShadow: "md",
-        },
-        _active: {
-          ...floatingBackground("active", props),
-          ...floatingBorder("active", props),
-          ...baseText("default", props),
+      floating: {
+        root: {
+          ...floatingBackground("default"),
+          ...baseText("default"),
+          ...floatingBorder("default"),
+          boxShadow: "sm",
+          _hover: {
+            ...floatingBackground("hover"),
+            ...floatingBorder("hover"),
+            ...baseText("default"),
+            boxShadow: "md",
+          },
+          _active: {
+            ...floatingBackground("active"),
+            ...floatingBorder("active"),
+            ...baseText("default"),
+          },
         },
       },
-    }),
+    },
+    size: {
+      xs: {
+        root: {
+          _checked: {
+            borderRadius: "0.563rem",
+          },
+          height: 5,
+          paddingX: 1.5,
+        },
+      },
+      sm: {
+        root: {
+          _checked: {
+            borderRadius: "sm",
+          },
+          height: 6,
+          paddingX: 2,
+        },
+      },
+      md: {
+        root: {
+          _checked: {
+            borderRadius: "sm",
+          },
+          height: 7,
+          paddingX: 2,
+        },
+      },
+      lg: {
+        root: {
+          _checked: {
+            borderRadius: "md",
+          },
+          height: 8,
+          paddingX: 3,
+        },
+      },
+    },
   },
-  sizes: {
-    xs: {
-      container: {
-        _checked: {
-          borderRadius: "0.563rem",
-        },
-        height: 5,
-        paddingX: 1.5,
-      },
-    },
-    sm: {
-      container: {
-        _checked: {
-          borderRadius: "sm",
-        },
-        height: 6,
-        paddingX: 2,
-      },
-    },
-    md: {
-      container: {
-        _checked: {
-          borderRadius: "sm",
-        },
-        height: 7,
-        paddingX: 2,
-      },
-    },
-    lg: {
-      container: {
-        _checked: {
-          borderRadius: "md",
-        },
-        height: 8,
-        paddingX: 3,
-      },
-    },
-  },
-  defaultProps: {
+  defaultVariants: {
+    variant: "base",
     size: "sm",
   },
 });
-
-export default config;

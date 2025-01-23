329d72909da2cd8586a85d1ca220f41c255b8bd3
cibietici <carloalberto.burato@gmail.com>
2025-01-23 14:25:29 +0100

    Fix lists update to chakra 3

---
+++ packages/spor-react/src/theme/slot-recipes/anatomy.ts
@@ -0,0 +1,8 @@
+import { createAnatomy } from "@ark-ui/react/anatomy";
+
+export const listAnatomy = createAnatomy("list").parts(
+  "root",
+  "item",
+  "icon",
+  "indicator",
+);
--- packages/spor-react/src/theme/slot-recipes/list.ts
+++ packages/spor-react/src/theme/slot-recipes/list.ts
@@ -1,19 +1,46 @@
+import { listAnatomy } from "./anatomy";
 import { defineSlotRecipe } from "@chakra-ui/react";
 
 export const listSlotRecipe = defineSlotRecipe({
-  slots: ["container", "item", "icon"],
+  slots: listAnatomy.keys(),
   className: "spor-list",
   base: {
-    container: {
-      fontSize: ["mobile.sm", "desktop.sm"],
-    },
     item: {
       fontFamily: "body",
+      whiteSpace: "normal",
+      display: "list-item",
+      fontSize: ["mobile.sm", "desktop.sm"],
     },
     icon: {
       marginEnd: "2",
       display: "inline",
       verticalAlign: "text-bottom",
     },
+  },
+  variants: {
+    variant: {
+      marker: {
+        root: {
+          listStyle: "revert",
+          listStylePosition: "inside",
+        },
+        item: {
+          _marker: {
+            color: "inherit",
+          },
+        },
+      },
+
+      plain: {
+        item: {
+          alignItems: "flex-start",
+          display: "inline-flex",
+        },
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: "marker",
   },
 });

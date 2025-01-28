f2e52a2713860fb70ad712cf3696fd6e286c2eec
Alice Clausen <42611957+alicemacl@users.noreply.github.com>
2025-01-28 12:26:00 +0100

    Chakra 3: Update Textarea (#1480)
    * Update textarea. Put in temporary solution for color mode
    
    * Move slots to anatomy
    
    * Pirk on spacing
    
    * Add changeset

---
+++ packages/spor-react/src/theme/slot-recipes/anatomy.ts
@@ -0,0 +1,9 @@
+import { createAnatomy } from "@ark-ui/react/anatomy";
+
+export const fieldAnatomy = createAnatomy("field").parts(
+  "root",
+  "label",
+  "requiredIndicator",
+  "helperText",
+  "errorText",
+);

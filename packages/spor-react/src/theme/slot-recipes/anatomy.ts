61a666b7c119cbc428342f4d20e3fcc754d39c51
Carlo Alberto Burato <carloalberto.burato@gmail.com>
2025-01-31 12:11:12 +0100

    Fix lists update to chakra 3 (#1474)
    * Fix lists update to chakra 3
    
    * Improve css and distance
    
    * Add changeset

--- packages/spor-react/src/theme/slot-recipes/anatomy.ts
+++ packages/spor-react/src/theme/slot-recipes/anatomy.ts
@@ -1,5 +1,12 @@
 import { createAnatomy } from "@ark-ui/react/anatomy";
 
+export const listAnatomy = createAnatomy("list").parts(
+  "root",
+  "item",
+  "icon",
+  "indicator",
+);
+
 export const fieldAnatomy = createAnatomy("field").parts(
   "root",
   "label",

14b5907d928b2152e22168723d180258f051824d
Alice Clausen <42611957+alicemacl@users.noreply.github.com>
2025-02-06 15:38:49 +0100

    Chakra 3: Update RadioCardGroup, RadioCard and add RadioCardLabel (#1491)
    * Rename
    
    * Update radiocard
    
    * Update RadioCard syntax and styling
    
    * Update docs in file
    
    * Add changeset
    
    * Update some styling and add aria-label
    
    * Delete components file
    
    * Move slots and update style
    
    * update package

--- packages/spor-react/src/theme/slot-recipes/anatomy.ts
+++ packages/spor-react/src/theme/slot-recipes/anatomy.ts
@@ -33,3 +33,12 @@ export const checkboxAnatomy = createAnatomy("checkbox").parts(
   "indicator",
   "content",
 );
+
+export const radioCardAnatomy = createAnatomy("radio-card").parts(
+  "root",
+  "item",
+  "label",
+  "itemText",
+  "itemDescription",
+  "itemContent",
+);

0d0b2330bccbf0b0667646d498b0deaf9801c52a
Asgeir Hunshamar <asgeirhuns@gmail.com>
2025-02-20 12:42:28 +0100

    Add skeleton recipe and enhance Skeleton components with styling

--- packages/spor-react/src/loader/Skeleton.tsx
+++ packages/spor-react/src/loader/Skeleton.tsx
@@ -1,40 +1,66 @@
 "use client";
+import { skeletonRecipe } from "@/theme/recipes/skeleton";
 import type {
   SkeletonProps as ChakraSkeletonProps,
   CircleProps,
+  RecipeVariantProps,
 } from "@chakra-ui/react";
-import { Skeleton as ChakraSkeleton, Circle, Stack } from "@chakra-ui/react";
+import {
+  Skeleton as ChakraSkeleton,
+  Circle,
+  Stack,
+  useRecipe,
+} from "@chakra-ui/react";
 import * as React from "react";
 import { forwardRef } from "react";
 
-export interface SkeletonCircleProps extends ChakraSkeletonProps {
-  size?: CircleProps["size"];
-}
+type SkeletonVariantProps = RecipeVariantProps<typeof skeletonRecipe>;
+
+export type SkeletonCircleProps = ChakraSkeletonProps &
+  SkeletonVariantProps & {
+    size?: CircleProps["size"];
+  };
 
 export const SkeletonCircle = React.forwardRef<
   HTMLDivElement,
   SkeletonCircleProps
 >(function SkeletonCircle(props, ref) {
-  const { size, ...rest } = props;
+  const recipe = useRecipe({ recipe: skeletonRecipe });
+
+  const [recipeProps, restProps] = recipe.splitVariantProps(props);
+
+  const styles = recipe(recipeProps);
+
+  const { size, ...rest } = restProps;
+
   return (
     <Circle size={size} asChild ref={ref}>
-      <ChakraSkeleton {...rest} />
+      <ChakraSkeleton {...rest} loading={false} css={styles} />
     </Circle>
   );
 });
 
-export interface SkeletonTextProps extends ChakraSkeletonProps {
-  noOfLines?: number;
-}
+export type SkeletonTextProps = ChakraSkeletonProps &
+  SkeletonVariantProps & {
+    noOfLines?: number;
+  };
 
 export const SkeletonText = forwardRef<HTMLDivElement, SkeletonTextProps>(
   function SkeletonText(props, ref) {
-    const { noOfLines = 3, gap, ...rest } = props;
+    const recipe = useRecipe({ recipe: skeletonRecipe });
+
+    const [recipeProps, restProps] = recipe.splitVariantProps(props);
+
+    const styles = recipe(recipeProps);
+
+    const { noOfLines = 3, gap, ...rest } = restProps;
+
     return (
       <Stack gap={gap} width="full" ref={ref}>
         {Array.from({ length: noOfLines }).map((_, index) => (
           <ChakraSkeleton
-            height="4"
+            height="0.5rem"
+            css={styles}
             key={index}
             {...props}
             _last={{ maxW: "80%" }}
@@ -46,4 +72,19 @@ export const SkeletonText = forwardRef<HTMLDivElement, SkeletonTextProps>(
   },
 );
 
-export const Skeleton = ChakraSkeleton;
+export type SkeletonProps = ChakraSkeletonProps & SkeletonVariantProps;
+
+export const Skeleton = forwardRef<HTMLDivElement, SkeletonTextProps>(
+  function SkeletonText(props, ref) {
+    const recipe = useRecipe({ recipe: skeletonRecipe });
+
+    const [recipeProps, restProps] = recipe.splitVariantProps(props);
+    const styles = recipe(recipeProps);
+
+    return (
+      <>
+        <ChakraSkeleton {...restProps} height="6" ref={ref} css={styles} />
+      </>
+    );
+  },
+);
---
+++ packages/spor-react/src/theme/recipes/skeleton.ts
@@ -0,0 +1,52 @@
+import { defineRecipe } from "@chakra-ui/react";
+
+export const skeletonRecipe = defineRecipe({
+  className: "chakra-skeleton",
+  variants: {
+    loading: {
+      true: {
+        borderRadius: "xs", 
+        boxShadow: "none",
+        backgroundClip: "padding-box",
+        cursor: "default",
+        color: "transparent",
+        pointerEvents: "none",
+        userSelect: "none",
+        flexShrink: "0",
+        "&::before, &::after, *": {
+          visibility: "hidden",
+        },
+      },
+      false: {
+        background: "unset",
+        animation: "fade-in var(--fade-duration, 0.1s) ease-out !important",
+      },
+    },
+    variant: {
+      pulse: {
+        background: "steel",
+        animation: "pulse",
+        animationDuration: "var(--duration, 1.2s)",
+      },
+      shine: {
+        "--animate-from": "200%",
+        "--animate-to": "-200%", 
+        "--start-color": "colors.lightGrey",
+        "--end-color": "colors.steel",
+        backgroundImage:
+        "linear-gradient(270deg,var(--start-color),var(--end-color),var(--end-color),var(--start-color))",
+        backgroundSize: "400% 100%",
+        animation: "bg-position var(--duration, 5s) ease-in-out infinite",
+      },
+      none: {
+        animation: "none",
+        background: "steel",
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: "pulse",
+    loading: true,
+  },
+})

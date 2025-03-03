25304feba6bd26bbf1878ef70effe8faae654fa9
Asgeir Hunshamar <asgeirhuns@gmail.com>
2025-03-03 13:23:03 +0100

    Add placeholder text in root component for testing purposes

--- apps/docs/app/root.tsx
+++ apps/docs/app/root.tsx
@@ -192,6 +192,7 @@ const Document = withEmotionCache(
             brand={brand}
           >
             <SkipToContent />
+            hei hei hei
             {children}
           </SporProvider>
           <ScrollRestoration />

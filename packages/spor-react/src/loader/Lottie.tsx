beb7281c1cd4f6277fb16e702c9f4faf995433bb
Asgeir Hunshamar <asgeirhuns@gmail.com>
2025-03-06 12:22:48 +0100

    refactor: remove looping from Lottie component

--- packages/spor-react/src/loader/Lottie.tsx
+++ packages/spor-react/src/loader/Lottie.tsx
@@ -6,5 +6,5 @@ import ReactLottie from "lottie-react";
  * A wrapper around Lottie to make it tree-shakeable for SSR.
  */
 export default function Lottie({ animationData }: { animationData: any }) {
-  return <ReactLottie animationData={animationData} loop={true} />;
+  return <ReactLottie animationData={animationData} />;
 }

76e7a9afb7c8fd8a45993c8567d45c5a4a18670e
Asgeir Hunshamar <asgeirhuns@gmail.com>
2025-03-07 14:34:43 +0100

    Remove error code and add todo to fix it later

--- apps/docs/app/routes/_base.resources.icon-library/SearchBar.tsx
+++ apps/docs/app/routes/_base.resources.icon-library/SearchBar.tsx
@@ -1,9 +1,3 @@
-import {
-  Flex,
-  Input,
-  NativeSelectField,
-  NativeSelect,
-} from "@vygruppen/spor-react";
 import { useSearchFilter } from "./SearchFilterContext";
 
 /**
@@ -12,39 +6,42 @@ import { useSearchFilter } from "./SearchFilterContext";
 export function SearchBar() {
   const { searchFilter, setSearchString, setSize, setVariant } =
     useSearchFilter();
-  return (
-    <Flex as="form" gap={2}>
-      <Input
-        label="Look up icon"
-        onChange={(e) => {
-          setSearchString(e.target.value);
-        }}
-        value={searchFilter.searchString}
-      />
 
-      <NativeSelect>
-        <NativeSelectField
-          placeholder="Size"
-          onChange={(e) => setSize(e.target.value)}
-          value={searchFilter.size}
-        >
-          <option value="18">18 x 18px</option>
-          <option value="24">24 x 24px</option>
-          <option value="30">30 x 30px</option>
-        </NativeSelectField>
-      </NativeSelect>
+  return null; // Todo: fix this searchBar
 
-      <NativeSelect>
-        <NativeSelectField
-          placeholder="Variant"
-          value={searchFilter.variant}
-          onChange={(e) => setVariant(e.target.value)}
-        >
-          <option value="">All</option>
-          <option value="outline">Outline</option>
-          <option value="fill">Fill</option>
-        </NativeSelectField>
-      </NativeSelect>
-    </Flex>
-  );
+  // return (
+  //   <Flex as="form" gap={2}>
+  //     <Input
+  //       label="Look up icon"
+  //       onChange={(e) => {
+  //         setSearchString(e.target.value);
+  //       }}
+  //       value={searchFilter.searchString}
+  //     />
+
+  //     <NativeSelect>
+  //       <NativeSelectField
+  //         placeholder="Size"
+  //         onChange={(e) => setSize(e.target.value)}
+  //         value={searchFilter.size}
+  //       >
+  //         <option value="18">18 x 18px</option>
+  //         <option value="24">24 x 24px</option>
+  //         <option value="30">30 x 30px</option>
+  //       </NativeSelectField>
+  //     </NativeSelect>
+
+  //     <NativeSelect>
+  //       <NativeSelectField
+  //         placeholder="Variant"
+  //         value={searchFilter.variant}
+  //         onChange={(e) => setVariant(e.target.value)}
+  //       >
+  //         <option value="">All</option>
+  //         <option value="outline">Outline</option>
+  //         <option value="fill">Fill</option>
+  //       </NativeSelectField>
+  //     </NativeSelect>
+  //   </Flex>
+  // );
 }

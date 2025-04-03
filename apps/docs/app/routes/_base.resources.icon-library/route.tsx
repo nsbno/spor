cc421b6a2d8dfc173ffb3d6f9f0cbb848f5f6035
cibietici <carloalberto.burato@gmail.com>
2025-04-03 14:08:23 +0200

    Fix download for single icons and all icons

--- apps/docs/app/routes/_base.resources.icon-library/route.tsx
+++ apps/docs/app/routes/_base.resources.icon-library/route.tsx
@@ -1,18 +1,15 @@
 import { DownloadOutline24Icon } from "@vygruppen/spor-icon-react";
-import { Button, Heading, Separator, Stack, Text } from "@vygruppen/spor-react";
+import {
+  Box,
+  Button,
+  Heading,
+  Separator,
+  Stack,
+  Text,
+} from "@vygruppen/spor-react";
 import { SearchBar } from "./SearchBar";
 import { SearchFilterProvider } from "./SearchFilterContext";
 import { SearchResults } from "./SearchResults";
-
-const downloadAllIcons = async () => {
-  const response = await fetch("/resources/icon-library/all-icons.zip");
-  const blob = await response.blob();
-  const url = window.URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "/resources/icon-library/all-icons.zip";
-  a.click();
-};
 
 export default function IconsPage() {
   return (
@@ -26,17 +23,19 @@ export default function IconsPage() {
         identify and differentiate between different content so that they create
         value for the user.
       </Text>
-      <Button
-        variant="primary"
-        size="md"
-        leftIcon={<DownloadOutline24Icon />}
-        onClick={() => downloadAllIcons()}
-      >
-        Download all icons
-      </Button>
-
+      <Box maxWidth={["100%", null, null, "50%"]} marginX="auto">
+        <Button
+          variant="primary"
+          as="a"
+          size="md"
+          leftIcon={<DownloadOutline24Icon />}
+          download="all-icons.zip"
+          href="/resources/icon-library/all-icons.zip"
+        >
+          Download all icons
+        </Button>
+      </Box>
       <Separator />
-
       <SearchFilterProvider>
         <SearchBar />
         <SearchResults />

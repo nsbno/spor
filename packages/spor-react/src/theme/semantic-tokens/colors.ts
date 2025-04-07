23df3291043d6f2ae2f16b6d47b7b92fb62dd4b6
Asgeir Hunshamar <asgeirhuns@gmail.com>
2025-04-07 09:51:09 +0200

    new colors

--- packages/spor-react/src/theme/semantic-tokens/colors.ts
+++ packages/spor-react/src/theme/semantic-tokens/colors.ts
@@ -1,5 +1,6 @@
 import { defineSemanticTokens } from "@chakra-ui/react";
 import vyDigitalJson from "@vygruppen/spor-design-tokens/tokens/color/vy-digital.json";
+import VyUtviklingJson from "@vygruppen/spor-design-tokens/tokens/color/vy-utvikling.json";
 import cargonetJson from "@vygruppen/spor-design-tokens/tokens/color/cargonet.json";
 
 export enum Brand {
@@ -8,12 +9,12 @@ export enum Brand {
   CargoNet = "CargoNet",
 }
 
-export const colors = defineSemanticTokens.colors({
+export const vyDigitalColors = defineSemanticTokens.colors({
   ...vyDigitalJson.color.vyDigital,
 });
 
-export const vyDigitalColors = defineSemanticTokens.colors({
-  ...vyDigitalJson.color.vyDigital,
+export const vyUtviklingColors = defineSemanticTokens.colors({
+  ...VyUtviklingJson.color.vyUtvikling,
 });
 
 export const cargonetColors = defineSemanticTokens.colors({

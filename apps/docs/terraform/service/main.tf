c44c7b90ad6c43bf9abf1ba68541efec2f5a9583
Carlo Alberto Burato <carloalberto.burato@gmail.com>
2025-04-08 14:46:48 +0200

    Add test account (#1601)
    ## Background
    
    Missing test account
    
    ## Solution
    
    add

--- apps/docs/terraform/service/main.tf
+++ apps/docs/terraform/service/main.tf
@@ -13,11 +13,12 @@ locals {
   ecr_repository_name = "spor"
   service_account     = "637423315721"
   prod_account        = "471112960535"
+  test_account        = "590183702222"
 }
 
 module "ecr" {
   source              = "github.com/nsbno/terraform-aws-ecr?ref=1.4.1"
   name_prefix         = local.ecr_repository_name
-  trusted_accounts    = [local.prod_account, local.service_account]
+  trusted_accounts    = [local.prod_account, local.service_account, local.test_account]
   max_images_retained = 10
 }

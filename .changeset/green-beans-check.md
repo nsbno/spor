a0c33c53dcb725d9f3686e3f42c4a1456c852ceb
Asgeir Hunshamar <asgeir.hunshamar@vy.no>
2025-04-15 08:34:37 +0200

    Chore: Update CardSelect, fix issues from test. Also update header in spor docs to be more responsive. center searchfield, and use new CardSelect. (#1619)

--- .changeset/green-beans-check.md
+++ .changeset/green-beans-check.md
@@ -44,6 +44,7 @@ Spor is getting a major update with Chakra 3.
 - `Nudge` has different props. `WizardNudge`is no longer its own component, but can be easily built with `Nudge`and `NudgeWizardStep`s
 - `ButtonGroup` has deprecate variant and size since the reference component in Chakra is now Group, variant and size is sent as children
 - `Alert` Rename props in alert from `indicator` to `showIndicator`
+- `CardSelect` new implementation. Uses same setup as `Popover`. See docs
 
 ### Darkmode
 
--- apps/docs/app/root/layout/SiteSettings.tsx
+++ apps/docs/app/root/layout/SiteSettings.tsx
@@ -1,53 +1,49 @@
 import { SettingsX1Fill24Icon } from "@vygruppen/spor-icon-react";
 import {
-  Box,
   CardSelect,
-  createListCollection,
-  Flex,
+  CardSelectContent,
+  CardSelectTrigger,
   Heading,
-  Select,
-  SelectItem,
   Stack,
   Switch,
-  Text,
   useColorMode,
+  Text,
 } from "@vygruppen/spor-react";
 import { BrandSwitcher } from "~/features/brand-switcher/BrandSwitcher";
 
-type SiteSettingsProps = {
-  showLabel: boolean;
-};
-export const SiteSettings = ({ showLabel }: SiteSettingsProps) => {
+export const SiteSettings = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <CardSelect
-      variant="ghost"
-      size="md"
-      label={showLabel ? "Settings" : ""}
-      icon={<SettingsX1Fill24Icon />}
-      withChevron={false}
-      fontWeight="bold"
-      position="relative"
-      className="dark"
-      marginRight={[0, 0, 0, 4]}
-    >
-      <Stack gap="4">
-        <Heading as="h2" variant="md">
-          Site settings
-        </Heading>
-        <Text variant="sm">
-          Change the appearance of the site to suit your preferences
-        </Text>
-        <BrandSwitcher />
-        <Switch
-          id="site-settings-dark-mode"
-          size="sm"
-          onChange={() => toggleColorMode()}
-          defaultChecked={colorMode === "dark"}
-          label="Dark mode"
-        />
-      </Stack>
+    <CardSelect>
+      <CardSelectTrigger
+        icon={<SettingsX1Fill24Icon />}
+        variant="ghost"
+        withChevron={false}
+        fontWeight={"bold"}
+        className="dark"
+        title="Site settings"
+      >
+        <Text display={{ base: "none", lg: "block" }}>Settings</Text>
+      </CardSelectTrigger>
+      <CardSelectContent>
+        <Stack gap="4">
+          <Heading as="h2" variant="md">
+            Site settings
+          </Heading>
+          <Text variant="sm">
+            Change the appearance of the site to suit your preferences
+          </Text>
+          <BrandSwitcher />
+          <Switch
+            id="site-settings-dark-mode"
+            size="sm"
+            onChange={() => toggleColorMode()}
+            defaultChecked={colorMode === "dark"}
+            label="Dark mode"
+          />
+        </Stack>
+      </CardSelectContent>
     </CardSelect>
   );
 };

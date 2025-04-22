f8e879fe4848a781ddbe58e3f6b6f7c0c92a471a
Carlo Alberto Burato <carloalberto.burato@gmail.com>
2025-04-22 15:35:15 +0200

    Fix list unordered (#1630)
    ## Background
    
    Unordered list was not proper styled since the distance between the disc
    and the item was too large
    
    ## Solution
    
    clean up the css and remove what was causing the issue
    
    **Delete this checklist if you are not working on Chakra 3/Spor 12**

--- packages/spor-react/src/theme/slot-recipes/list.ts
+++ packages/spor-react/src/theme/slot-recipes/list.ts
@@ -1,29 +1,21 @@
-import { listAnatomy } from "./anatomy";
 import { defineSlotRecipe } from "@chakra-ui/react";
+import { listAnatomy } from "./anatomy";
 
 export const listSlotRecipe = defineSlotRecipe({
   slots: listAnatomy.keys(),
   className: "spor-list",
   base: {
-    root: {
-      listStyle: "none",
-      position: "relative",
-    },
     item: {
       fontFamily: "body",
-      position: "relative",
       whiteSpace: "normal",
-      display: "list-item",
-      fontSize: "mobile.md",
-      paddingLeft: 0,
+      fontSize: "inherit",
+      marginLeft: "2",
       _marker: {
         display: "inline-block",
       },
     },
     icon: {
-      marginEnd: "2",
-      display: "inline",
-      verticalAlign: "text-bottom",
+      display: "inline-block",
     },
   },
   variants: {
@@ -31,7 +23,6 @@ export const listSlotRecipe = defineSlotRecipe({
       marker: {
         root: {
           listStyle: "revert",
-          listStylePosition: "inside",
         },
         item: {
           _marker: {

7388744dee25ec1a4233b44317a054a52043cf64
Marius Kluften <marius@kluften.dev>
2025-05-21 15:03:50 +0200

    feat(lottie): add build script and fix export of lottie files

--- packages/spor-loader/src/index.ts
+++ packages/spor-loader/src/index.ts
@@ -12,3 +12,32 @@ export * from "./animation-data/spinner-color-pride";
 export * from "./animation-data/spinner-dark";
 export * from "./animation-data/spinner-light";
 export * from "./animation-data/vy-logo-pride";
+
+export const lottieAssets = {
+  contentLoader: 'content-loader.lottie',
+  contentLoaderPride: 'content-loader-pride.lottie',
+  fullScreenLoaderBlack: 'full-screen-loader-black.lottie',
+  fullScreenLoaderWhite: 'full-screen-loader-white.lottie',
+  inlineLoaderColor: 'inline-loader-color.lottie',
+  inlineLoaderColorPride: 'inline-loader-color-pride.lottie',
+  inlineLoaderDark: 'inline-loader-dark.lottie',
+  inlineLoaderLight: 'inline-loader-light.lottie',
+  inlineLoaderLightColor: 'inline-loader-light-color.lottie',
+  spinnerColor: 'spinner-color.lottie',
+  spinnerColorPride: 'spinner-color-pride.lottie',
+  spinnerDark: 'spinner-dark.lottie',
+  spinnerLight: 'spinner-light.lottie',
+  vyLogoPride: 'vy-logo-pride.lottie'
+} as const;
+
+// Works in dev, but it wont build with this URL stuff
+// Not sure how to include the lottie files in a meaningful way. Perhaps store them on a cdn instead of as a npm package?
+export function getLottiePath(filename: string) {
+  return new URL(`../assets/${filename}`, import.meta.url).href;
+}
+
+export const lottieFiles = Object.fromEntries(
+  Object.entries(lottieAssets).map(
+    ([key, filename]) => [key, getLottiePath(filename)]
+  )
+) as Record<keyof typeof lottieAssets, string>;

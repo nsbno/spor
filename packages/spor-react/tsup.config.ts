3ea98987ebd388f2241b515b3ce8e34fe7ced65f
Asgeir Hunshamar <asgeirhuns@gmail.com>
2025-06-02 09:21:26 +0200

    chore: clean up tsup.config.ts by removing unnecessary comment

--- packages/spor-react/tsup.config.ts
+++ packages/spor-react/tsup.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.tsx", "src/icons/index.tsx"], // Added icons entry
+  entry: ["src/index.tsx", "src/icons/index.tsx"],
   format: ["cjs", "esm"],
   dts: true,
   treeshake: true,
-
   sourcemap: true,
 });

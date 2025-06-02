144aca1d99066730d65537f779fd8e360dc4ad73
Asgeir Hunshamar <asgeir.hunshamar@vy.no>
2025-06-02 14:08:27 +0200

    Feat: Export icons directly from spor #1706 (#1709)
    Seems to fix issue with SSR of icons

--- packages/spor-react/src/theme/tokens/colors.ts
+++ packages/spor-react/src/theme/tokens/colors.ts
@@ -1,7 +1,7 @@
 import { defineTokens } from "@chakra-ui/react";
-import aliasJson from "@vygruppen/spor-design-tokens/tokens/color/alias.json";
-import linjetagJson from "@vygruppen/spor-design-tokens/tokens/color/linjetag.json";
-import paletteJson from "@vygruppen/spor-design-tokens/tokens/color/palette.json";
+import aliasJson from "@vygruppen/spor-design-tokens/tokens/color/alias.json" assert { type: "json" };
+import linjetagJson from "@vygruppen/spor-design-tokens/tokens/color/linjetag.json" assert { type: "json" };
+import paletteJson from "@vygruppen/spor-design-tokens/tokens/color/palette.json" assert { type: "json" };
 
 export const colors = defineTokens.colors({
   ...aliasJson.color.alias,
--- packages/spor-react/tsup.config.ts
+++ packages/spor-react/tsup.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.tsx"],
+  entry: ["src/index.tsx", "src/icons/index.tsx"],
   format: ["cjs", "esm"],
   dts: true,
   treeshake: true,
   sourcemap: true,
+  esbuildOptions(options) {
+    // Ensure JSON files are treated as JSON modules
+    options.loader = {
+      ...options.loader,
+      ".json": "json",
+    };
+    options.target = "esnext";
+    options.platform = "node";
+  },
 });

8178892f44604f69e7122c8f3349f032c56f1254
Asgeir Hunshamar <asgeirhuns@gmail.com>
2025-06-03 11:28:01 +0200

    export raw tokens

--- packages/spor-react/src/theme/tokens/colors.ts
+++ packages/spor-react/src/theme/tokens/colors.ts
@@ -1,10 +1,8 @@
 import { defineTokens } from "@chakra-ui/react";
-import aliasJson from "@vygruppen/spor-design-tokens/tokens/color/alias.json" assert { type: "json" };
-import linjetagJson from "@vygruppen/spor-design-tokens/tokens/color/linjetag.json" assert { type: "json" };
-import paletteJson from "@vygruppen/spor-design-tokens/tokens/color/palette.json" assert { type: "json" };
+import tokens from "@vygruppen/spor-design-tokens/raw-tokens";
 
 export const colors = defineTokens.colors({
-  ...aliasJson.color.alias,
-  ...paletteJson.color.palette,
-  linjetag: linjetagJson.color.linjetag,
+  ...tokens.color.alias.color.alias,
+  ...tokens.color.palette.color.palette,
+  linjetag: tokens.color.linjetag.color.linjetag,
 });
--- packages/spor-react/tsup.config.ts
+++ packages/spor-react/tsup.config.ts
@@ -6,13 +6,4 @@ export default defineConfig({
   dts: true,
   treeshake: true,
   sourcemap: true,
-  esbuildOptions(options) {
-    // Ensure JSON files are treated as JSON modules
-    options.loader = {
-      ...options.loader,
-      ".json": "json",
-    };
-    options.target = "esnext";
-    options.platform = "node";
-  },
 });

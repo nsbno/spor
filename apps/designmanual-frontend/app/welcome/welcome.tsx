f7e67c1c414759de84c626444effe1c49b438a55
cibietici <carloalberto.burato@gmail.com>
2025-08-04 09:19:25 +0200

    Add remix route v7 empty app

---
+++ apps/designmanual-frontend/app/welcome/welcome.tsx
@@ -0,0 +1,13 @@
+//import { Button } from "@vygruppen/spor-react";
+export function Welcome() {
+  return (
+    <main>
+      <div>
+        <header>
+          <div>Vy Designmanual</div>
+        </header>
+        <main>spor element here:</main>
+      </div>
+    </main>
+  );
+}
---
+++ apps/designmanual-frontend/vite.config.ts
@@ -0,0 +1,11 @@
+import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+  },
+  plugins: [reactRouter(), tsconfigPaths()],
+});

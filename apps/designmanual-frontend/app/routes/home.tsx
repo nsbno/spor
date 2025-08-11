83dd34b4f8bf454b37ce9bcd8d112c5292e9cf6e
cibietici <carloalberto.burato@gmail.com>
2025-08-11 14:15:23 +0200

    Fix linting problems

--- apps/designmanual-frontend/app/routes/home.tsx
+++ apps/designmanual-frontend/app/routes/home.tsx
@@ -1,7 +1,6 @@
-import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },

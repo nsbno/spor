a41c4d4df6c679f98c9dfc92f6cbd7f8f0c9bad1
Asgeir Hunshamar <asgeirhuns@gmail.com>
2025-08-12 09:16:44 +0200

    fix: Update tsup configuration to conditionally clean output directory based on watch mode

--- packages/spor-react/tsup.config.ts
+++ packages/spor-react/tsup.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
+export default defineConfig(({ watch }) => ({
   entry: ["src/index.tsx", "src/icons/index.tsx"],
   format: ["esm", "cjs"],
   dts: true,
   treeshake: true,
   sourcemap: true,
   splitting: false,
-  clean: true,
+  clean: !watch, // avoid deleting ./dist when dev/watch starts
   outDir: "dist",
   outExtension: ({ format }) => ({
     js: format === "esm" ? ".mjs" : ".cjs",
   }),
-});
+}));

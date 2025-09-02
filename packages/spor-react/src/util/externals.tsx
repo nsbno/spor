9032d9bf075dfb14ba0aa5369558005a2023fa2f
Asgeir Hunshamar <asgeirhuns@gmail.com>
2025-09-02 10:37:12 +0200

    fix

--- packages/spor-react/src/util/externals.tsx
+++ packages/spor-react/src/util/externals.tsx
@@ -1,13 +1,16 @@
 /** This file works as a proxy for all Chakra UI exports */
 export type { PortalProps, UseDisclosureProps } from "@chakra-ui/react";
 export {
+  AspectRatio,
   ClientOnly,
+  createIcon,
   createListCollection,
   defineRecipe,
   defineStyle,
   For,
   FormatByte,
   FormatNumber,
+  Icon,
   LocaleProvider,
   Portal,
   Show,
@@ -18,6 +21,8 @@ export {
   useControllableProp,
   useDisclosure,
   useMediaQuery,
+  useRecipe,
+  useSlotRecipe,
   useToken,
   VisuallyHidden,
 } from "@chakra-ui/react";

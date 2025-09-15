7978272c802fc41509a2b42f0dac077c204206d8
cibietici <carloalberto.burato@gmail.com>
2025-09-15 15:52:37 +0200

    Improve menu headings hook to work with useeffect

--- apps/designmanual-frontend/app/utils/useHeadingsMenu.ts
+++ apps/designmanual-frontend/app/utils/useHeadingsMenu.ts
@@ -1,4 +1,5 @@
-"use client";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 
 export type HeadingsMenu = {
   text: string;
@@ -11,24 +12,33 @@ export type HeadingsMenu = {
  */
 
 export const useHeadingsMenu = (): Array<HeadingsMenu> | [] => {
-  const headings: Array<{ text: string; id?: string }> = [];
-  const h2Elements =
-    typeof document === "undefined" ? [] : document.querySelectorAll("h2");
+  const location = useLocation();
 
-  if (h2Elements.length > 0) {
-    for (const element of h2Elements) {
-      const text = element.textContent?.trim() || "";
+  const [headingsMenu, setHeadingsMenu] = useState<Array<HeadingsMenu> | []>(
+    [],
+  );
 
-      const id =
-        element.id ||
-        text
-          .toLowerCase()
-          .replaceAll(/\s+/g, "-")
-          .replaceAll(/[^a-z0-9-]/g, "");
+  useEffect(() => {
+    const headings: Array<{ text: string; id?: string }> = [];
+    const h2Elements =
+      typeof document === "undefined" ? [] : document.querySelectorAll("h2");
 
-      headings.push({ text, id });
+    if (h2Elements.length > 0) {
+      for (const element of h2Elements) {
+        const text = element.textContent?.trim() || "";
+
+        const id =
+          element.id ||
+          text
+            .toLowerCase()
+            .replaceAll(/\s+/g, "-")
+            .replaceAll(/[^a-z0-9-]/g, "");
+
+        headings.push({ text, id });
+      }
     }
-  }
+    setHeadingsMenu(headings);
+  }, [location]);
 
-  return headings;
+  return headingsMenu;
 };

070fbb85007933fb0ea180f236bd3e6975aa3d01
Asgeir Hunshamar <asgeirhuns@gmail.com>
2025-09-15 08:41:45 +0200

    fix: ensure newline at end of file in build-tokens.js

--- packages/spor-react/src/button/IconButton.tsx
+++ packages/spor-react/src/button/IconButton.tsx
@@ -64,6 +64,7 @@ export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
         aria-label={props["aria-label"]}
         size={size}
         position={"relative"}
+        bg="red"
         {...rest}
         ref={ref}
       >

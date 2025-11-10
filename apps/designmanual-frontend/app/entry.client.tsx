a701721e8c105f54b904334449eaea0d20ba9160
Asgeir Hunshamar <asgeirhuns@gmail.com>
2025-11-10 09:28:08 +0100

    update emotion cache

---
+++ apps/designmanual-frontend/app/entry.client.tsx
@@ -0,0 +1,16 @@
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+import { HydratedRouter } from "react-router/dom";
+
+import { ClientCacheProvider } from "./emotion/emotion-client";
+
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <ClientCacheProvider>
+        <HydratedRouter />
+      </ClientCacheProvider>
+    </StrictMode>,
+  );
+});

6eb9eb2f54b328d49eade04e607b5be7ee945b7f
Asgeir Hunshamar <asgeirhuns@gmail.com>
2025-11-18 13:57:22 +0100

    refactor: simplify illustration loading and enhance rendering with ClientOnly component

--- apps/designmanual-frontend/app/routes/_base.$section.illustration-library/illustration-grid.tsx
+++ apps/designmanual-frontend/app/routes/_base.$section.illustration-library/illustration-grid.tsx
@@ -1,9 +1,11 @@
 import { DownloadOutline18Icon } from "@vygruppen/spor-icon-react";
 import {
   Box,
+  ClientOnly,
   Flex,
   IconButton,
   Image,
+  Skeleton,
   slugify,
   StaticCard,
   Text,
@@ -33,43 +35,47 @@ export const IllustationGrid = ({
       .url();
 
     return (
-      <StaticCard
-        colorScheme="white"
-        bg="bg"
+      <ClientOnly
+        fallback={<Skeleton height="236px" borderRadius="md" />}
         key={illustration._id}
-        padding={2}
-        border="1px solid"
-        borderColor="outline"
-        position="relative"
       >
-        <Flex flexDirection="column" height="100%">
-          <Flex gap={1} alignItems="center" flexDirection={"column"}>
-            <Text variant="sm">{illustration.title}</Text>
-            <Image
-              src={urlBuilder.image(imageUrl).url()}
-              alt={illustration.description}
-              width="100%"
-              minHeight={12}
-              maxHeight={15}
-              objectFit="contain"
-              objectPosition="center"
-              flex={1}
-            />
+        <StaticCard
+          colorScheme="white"
+          bg="bg"
+          padding={2}
+          border="1px solid"
+          borderColor="outline"
+          position="relative"
+        >
+          <Flex flexDirection="column" height="100%">
+            <Flex gap={1} alignItems="center" flexDirection={"column"}>
+              <Text variant="sm">{illustration.title}</Text>
+              <Image
+                src={urlBuilder.image(imageUrl).url()}
+                alt={illustration.description}
+                width="100%"
+                minHeight={12}
+                maxHeight={15}
+                objectFit="contain"
+                objectPosition="center"
+                flex={1}
+              />
+            </Flex>
           </Flex>
-        </Flex>
-        <Box position="absolute" bottom="1" right="1">
-          <IconButton
-            variant="ghost"
-            size="sm"
-            icon={<DownloadOutline18Icon />}
-            as="a"
-            download={`${slugify(illustration.title)}.svg`}
-            href={downloadUrl}
-            aria-label="Download SVG"
-            title="Download SVG"
-          />
-        </Box>
-      </StaticCard>
+          <Box position="absolute" bottom="1" right="1">
+            <IconButton
+              variant="ghost"
+              size="sm"
+              icon={<DownloadOutline18Icon />}
+              as="a"
+              download={`${slugify(illustration.title)}.svg`}
+              href={downloadUrl}
+              aria-label="Download SVG"
+              title="Download SVG"
+            />
+          </Box>
+        </StaticCard>
+      </ClientOnly>
     );
   });
 };
--- apps/designmanual-frontend/app/routes/_base.$section.illustration-library/route.tsx
+++ apps/designmanual-frontend/app/routes/_base.$section.illustration-library/route.tsx
@@ -67,17 +67,17 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const draftMode =
     url.searchParams.get("sanity-preview-perspective") === "drafts";
 
-  const illustrationsPromise = getIllustrationsQuery(url.toString(), draftMode);
+  const illustrations = await getIllustrationsQuery(url.toString(), draftMode);
   const article = await getArticlesQuery(draftMode);
 
   return {
-    illustrationsPromise,
+    illustrations,
     article,
   };
 };
 
 export default function IllustrationLibraryPage() {
-  const { article, illustrationsPromise } = useLoaderData<typeof loader>();
+  const { article, illustrations } = useLoaderData<typeof loader>();
 
   const location = useLocation();
 
@@ -102,59 +102,43 @@ export default function IllustrationLibraryPage() {
         </Box>
       )}
 
-      <React.Suspense fallback={<SkeletonGrid />}>
-        <Await resolve={illustrationsPromise}>
-          {(illustrations) => (
-            <Stack gap="2">
-              <Flex gap="2">
-                <Button
-                  variant="primary"
-                  width="fit-content"
-                  as="a"
-                  download="illustrations.zip"
-                  href="/resources/illustration-library/all"
-                  leftIcon={<DownloadOutline24Icon />}
-                >
-                  Download entire illustration library
-                </Button>
+      <Stack gap="2">
+        <Flex gap="2">
+          <Button
+            variant="primary"
+            width="fit-content"
+            as="a"
+            download="illustrations.zip"
+            href="/resources/illustration-library/all"
+            leftIcon={<DownloadOutline24Icon />}
+          >
+            Download entire illustration library
+          </Button>
 
-                <Button
-                  variant="primary"
-                  width="fit-content"
-                  as="a"
-                  download="illustrations.zip"
-                  href={allHref}
-                  leftIcon={<DownloadOutline24Icon />}
-                >
-                  Download illustrations shown ({illustrations.total})
-                </Button>
-              </Flex>
+          <Button
+            variant="primary"
+            width="fit-content"
+            as="a"
+            download="illustrations.zip"
+            href={allHref}
+            leftIcon={<DownloadOutline24Icon />}
+          >
+            Download illustrations shown ({illustrations.total})
+          </Button>
+        </Flex>
 
-              <Separator marginY={4} />
+        <Separator marginY={4} />
 
-              <Filters />
-              <SimpleGrid columns={[1, 2, 3]} gap={2}>
-                <IllustationGrid illustrations={illustrations.items} />
-              </SimpleGrid>
-              <Pagination
-                total={illustrations.total}
-                page={illustrations.page}
-                pageSize={illustrations.pageSize}
-              />
-            </Stack>
-          )}
-        </Await>
-      </React.Suspense>
+        <Filters />
+        <SimpleGrid columns={[1, 2, 3]} gap={2}>
+          <IllustationGrid illustrations={illustrations.items} />
+        </SimpleGrid>
+        <Pagination
+          total={illustrations.total}
+          page={illustrations.page}
+          pageSize={illustrations.pageSize}
+        />
+      </Stack>
     </Box>
-  );
-}
-
-function SkeletonGrid({ count = 12 }: { count?: number }) {
-  return (
-    <SimpleGrid columns={[1, 2, 3]} gap={2}>
-      {Array.from({ length: count }).map((_, n) => (
-        <Skeleton key={n} height="260px" borderRadius="md" />
-      ))}
-    </SimpleGrid>
   );
 }

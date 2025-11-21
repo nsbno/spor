1ed4bc2387492f82714887a2f2ab06c4cf06891e
marenhkj-vy <maren.kjensli@vy.no>
2025-11-21 12:23:38 +0100

    Add darkmode-color on shadow (#2015)
    ## Background
    
    Shadow har ikke hatt en farge for darkmode. 
    
    ---
    
    ## Solution
    
    Skille mellom _light og _dark farge. 
    
    ---
    
    ## General Checklist
    
    - [ ] I have updated documentation if necessary
    - [ ] I have verified the design aligns with the latest Figma sketches.
    - [ ] I have created a changeset if publishing is required
    
    ## Accessibility checklist
    
    For changes impacting the user interface or functionality, ensure the
    following:
    
    - [ ] It is possible to use the keyboard to reach your changes
    - [ ] It is possible to enlarge the text 400% without losing
    functionality
    - [ ] It works on both mobile and desktop
    - [ ] It works in both Chrome, Safari and Firefox
    - [ ] It works with VoiceOver
    - [ ] There are no errors in aXe / SiteImprove-plugins / Wave
    
    ---
    
    ## Screenshots
    <img width="904" height="544" alt="Skjermbilde 2025-11-21 kl 09 57 37"
    src="https://github.com/user-attachments/assets/3249c495-22b0-47fc-b9df-679ac8f3052d"
    />
    
    ---------
    
    Co-authored-by: Asgeir Hunshamar <asgeirhuns@gmail.com>

--- apps/designmanual-frontend/app/routes/_base.$resources.design-tokens/route.tsx
+++ apps/designmanual-frontend/app/routes/_base.$resources.design-tokens/route.tsx
@@ -1,7 +1,6 @@
 import {
   Box,
   ClientOnly,
-  ContentLoader,
   Flex,
   Heading,
   Separator,
--- packages/spor-react/src/theme/semantic-tokens/shadows.ts
+++ packages/spor-react/src/theme/semantic-tokens/shadows.ts
@@ -2,16 +2,23 @@ import { defineSemanticTokens } from "@chakra-ui/react";
 import tokens from "@vygruppen/spor-design-tokens";
 
 export const shadows = defineSemanticTokens.shadows({
-  none: {
-    value: "none",
-  },
+  none: { value: "none" },
   sm: {
-    value: tokens.depth.shadow.sm.value,
+    value: {
+      _light: tokens.depth.shadow.sm.value._light,
+      _dark: tokens.depth.shadow.sm.value._dark,
+    },
   },
   md: {
-    value: tokens.depth.shadow.md.value,
+    value: {
+      _light: tokens.depth.shadow.md.value._light,
+      _dark: tokens.depth.shadow.md.value._dark,
+    },
   },
   lg: {
-    value: tokens.depth.shadow.lg.value,
+    value: {
+      _light: tokens.depth.shadow.lg.value._light,
+      _dark: tokens.depth.shadow.lg.value._dark,
+    },
   },
 });

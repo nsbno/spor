2c41bd0847bc5122048d10b5ab8f5b91c16c8b1a
Eivind Yu Nilsen <eivindyunilsen@hotmail.com>
2026-02-11 15:18:40 +0100

    Update wrong import

--- packages/sidespor-config/types.ts
+++ packages/sidespor-config/types.ts
@@ -1,6 +1,5 @@
-import { defineConfig, defineStyle } from "@vygruppen/spor-react";
+import { defineStyle, SystemConfig } from "@vygruppen/spor-react";
 
-export type SystemConfig = ReturnType<typeof defineConfig>;
 export type SystemStyleObject = ReturnType<typeof defineStyle>;
 type ThemingConfig = NonNullable<SystemConfig["theme"]>;
 type Recipe = NonNullable<ThemingConfig["recipes"]>;

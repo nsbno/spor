b179f2f2a44a746b2adc55c3b8d270ab5f9a13ae
Eivind <31821339+EivindYN@users.noreply.github.com>
2026-02-13 12:39:05 +0100

    Add sort to Table (#2096)
    <!---
    Thanks for creating a Pull Request! 🎉
    
    ✅ Keep your PR as small as possible.
    📘 React component guide:
    https://spor.vy.no/guides/how-to-make-new-react-components
    📦 How to make a changeset:
    https://spor.vy.no/guides/how-to-make-new-react-components#creating-a-pr-and-publish-package
    💡 Preferably use Conventional Commits:
    https://www.conventionalcommits.org/en/v1.0.0/
    
    🚀 Deploy to environments:
    - Comment `.preview` to deploy to preview environment
    - Comment `.deploy test` to deploy to test environment
    
    This template serves as a guideline; feel free to remove sections that
    do not apply to your pull request.
    -->
    
    ## Background
    
    Closes
    [Slack](https://nsb-utvikling.slack.com/archives/CM9H2N39U/p1770887968443989)
    
    <!-- Why this task is needed, context, and problem it solves -->
    
    Adds sorting to table since many teams need it
    
    ---
    
    ## Solution
    
    <!-- What has been done and why this approach was chosen -->
    
    Sort the table if you include the boolean `sort`. It sorts by the string
    content but can also be directly fed using `data-sort` on the cell.
    
    
    ---
    
    ## General Checklist
    
    - [ ] I have updated documentation if necessary (The links at the top
    doesnt work so I'm not able to fix this)
    - [x] I have verified the design aligns with the latest Figma sketches.
    - [ ] I have created a changeset if publishing is required (The links at
    the top doesnt work so I'm not able to fix this)
    
    ## Accessibility checklist
    
    For changes impacting the user interface or functionality, ensure the
    following:
    
    - [ ] It is possible to use the keyboard to reach your changes (Its not
    possible since it would be confusing to tab to column headers, but you
    can change that if you find it necessary)
    - [x] It is possible to enlarge the text 400% without losing
    functionality
    - [ ] It works on both mobile and desktop (I dont have a mobile testing
    setup)
    - [x] It works in both Chrome, Safari and Firefox
    - [ ] It works with VoiceOver (I dont have this testing setup)
    - [ ] There are no errors in aXe / SiteImprove-plugins / Wave (I dont
    have this testing setup)
    
    ---
    
    ## Screenshots
    
    | Before | After | 
    
    
    Before:
    <img width="708" height="181" alt="image"
    src="https://github.com/user-attachments/assets/604d1c71-8720-48d5-a06c-680dde39f0fa"
    />
    
    After:
    <img width="709" height="181" alt="image"
    src="https://github.com/user-attachments/assets/6e7efc46-2438-4632-b3f6-dcd5e2b04667"
    />
    
    ---------
    
    Co-authored-by: Asgeir Hunshamar <asgeir.hunshamar@vy.no>

---
+++ .changeset/shiny-keys-wait.md
@@ -0,0 +1,5 @@
+---
+"@vygruppen/spor-react": minor
+---
+
+Added a props `sortable` to Table that lets you sort columns by clicking on them.
--- packages/spor-react/src/table/index.tsx
+++ packages/spor-react/src/table/index.tsx
@@ -1,24 +1,18 @@
 export * from "./Table";
 export type {
-  TableBodyProps,
   TableCaptionProps,
   TableCellProps,
-  TableColumnHeaderProps,
   TableColumnProps,
   TableFooterProps,
   TableHeaderProps,
   TableRootProps,
-  TableRowProps,
 } from "@chakra-ui/react";
 export {
-  TableBody,
   TableCaption,
   TableCell,
   TableColumn,
   TableColumnGroup,
-  TableColumnHeader,
   TableFooter,
   TableHeader,
   TableRoot,
-  TableRow,
 } from "@chakra-ui/react";

b6f92f33f4d1a8ca660899bcbfb586dfecbe686c
Eivind Yu Nilsen <eivindyunilsen@hotmail.com>
2026-02-17 15:20:21 +0100

    Remove unnecessary color fix

--- packages/sidespor-config/theme/colors.ts
+++ packages/sidespor-config/theme/colors.ts
@@ -1,21 +1,13 @@
 import { tokens } from "@vygruppen/spor-react";
 
 import { Grey, WhiteAlpha } from "../types";
-import { mapValues } from "../utility";
 import { appPalette } from "./appPalette";
 
 const { palette } = tokens.default.color;
 
-const alias = mapValues(tokens.default.color.alias, (value) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [_, color, strength] = value.split(".");
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return (palette as any)[color][strength];
-});
-
 const colors: Record<string, string | WhiteAlpha | Grey> = {
   ...appPalette,
-  ...alias,
+  ...tokens.default.color.alias,
   ...palette,
 };
 
--- packages/sidespor-config/utility.ts
+++ packages/sidespor-config/utility.ts
@@ -1,12 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const mapValues = <T>(object: T, function_: (value: any) => any): T =>
-  Object.fromEntries(
-    Object.entries(object as any).map(([key, value]) => [
-      key,
-      function_(value),
-    ]),
-  ) as T;
-
 // Same as Object.keys() but keeps the typing instead of making it to string[]
 export function keys<T extends object>(object: T): (keyof T)[] {
   return Object.keys(object) as (keyof T)[];

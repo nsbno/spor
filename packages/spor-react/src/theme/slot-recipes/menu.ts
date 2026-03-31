80a1fda0a2b4fa9eccce1dee56c93763915d006f
marenhkj <maren.kjensli@vy.no>
2026-03-31 09:35:39 +0200

    add chakra's menu component (#2148)
    ## Background
    
    Adds chakra's menu component. 
    Documentation is added in test dataset under ```/spor/components/menu```
    
    Kan testes her: https://pr-2148.test.design.vy.no/spor/components/menu
    
    ---
    
    ## General Checklist
    
    - [ ] I have updated documentation if necessary
    - [ ] I have verified the design aligns with the latest Figma sketches.
    - [ ] I have created a changeset if publishing is required
    
    ## Accessibility checklist
    
    For changes impacting the user interface or functionality, ensure the
    following:
    
    - [ ] It is possible to use the keyboard to reach your changes
    - [ ] It is possible to enlarge the text 400% without losing
    functionality
    - [ ] It works on both mobile and desktop
    - [ ] It works in both Chrome, Safari and Firefox
    - [ ] It works with VoiceOver
    - [ ] There are no errors in aXe / SiteImprove-plugins / Wave
    
    ---
    
    ## Screenshots
    
    | Before | After |
    | ------ | ----- |
    |        |       |
    
    ---------
    
    Co-authored-by: Asgeir Hunshamar <asgeir.hunshamar@vy.no>

---
+++ packages/spor-react/src/theme/slot-recipes/menu.ts
@@ -0,0 +1,111 @@
+import { defineSlotRecipe } from "@chakra-ui/react";
+
+import { menuAnatomy } from "./anatomy";
+
+export const menuSlotRecipe = defineSlotRecipe({
+  className: "menu",
+  slots: menuAnatomy.keys(),
+  base: {
+    content: {
+      bg: "bg",
+      borderRadius: "sm",
+      boxShadow: "sm",
+      width: "fit-content",
+      padding: "2",
+
+      display: "flex",
+      flexDirection: "column",
+      gap: "1",
+      zIndex: "dropdown",
+
+      _open: {
+        animationStyle: "slide-fade-in",
+        animationDuration: "fast",
+        zIndex: "popover",
+        outline: "none",
+      },
+    },
+    itemGroup: {
+      display: "flex",
+      flexDirection: "column",
+      gap: "1",
+    },
+    item: {
+      borderRadius: "sm",
+      paddingY: "1",
+      paddingX: "2",
+      display: "flex",
+      justifyContent: "space-between",
+      gap: 1.5,
+
+      _hover: {
+        backgroundColor: "accent.surface.hover",
+      },
+
+      "&:active": {
+        backgroundColor: "accent.surface.active",
+      },
+
+      _checked: {
+        backgroundColor: "accent.surface",
+      },
+
+      _highlighted: {
+        backgroundColor: "ghost.surface.hover",
+      },
+    },
+    itemGroupLabel: {
+      paddingY: "1",
+      fontWeight: "bold",
+    },
+    itemCommand: {
+      fontSize: "2xs",
+    },
+    separator: {
+      color: "outline",
+    },
+    radioItem: {
+      display: "flex",
+      justifyContent: "space-between",
+      gap: 2,
+    },
+    triggerItem: {
+      display: "flex",
+      justifyContent: "space-between",
+      gap: 1.5,
+      alignItems: "center",
+    },
+    checkboxItem: {
+      display: "flex",
+      gap: 2,
+      alignItems: "center",
+      width: "full",
+    },
+  },
+  variants: {
+    variant: {
+      core: {
+        content: {
+          border: "1px solid",
+          borderColor: "core.outline",
+        },
+      },
+      accent: {
+        content: {
+          border: "1px solid",
+          borderColor: "core.outline",
+        },
+      },
+      floating: {
+        content: {
+          border: "sm",
+          borderColor: "floating.outline",
+          boxShadow: "lg",
+        },
+      },
+    },
+  },
+  defaultVariants: {
+    variant: "core",
+  },
+});

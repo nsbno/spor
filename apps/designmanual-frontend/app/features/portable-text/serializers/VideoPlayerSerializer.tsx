b87923054ed7b41ec790cc9b021530aeeceb5422
cibietici <carloalberto.burato@gmail.com>
2026-04-10 16:42:29 +0200

    Add feature for the component to read video

--- apps/designmanual-frontend/app/features/portable-text/serializers/VideoPlayerSerializer.tsx
+++ apps/designmanual-frontend/app/features/portable-text/serializers/VideoPlayerSerializer.tsx
@@ -86,5 +86,6 @@ export interface Track {
 }
 
 export function VideoPlayerSerializer({ value }: { value: VideoPlayerValue }) {
+  console.log("VideoPlayerSerializer value:", value);
   return <VideoPlayer value={value} />;
 }

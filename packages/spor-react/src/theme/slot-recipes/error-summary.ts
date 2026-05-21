6bdd89f7ea491126f5e8e69eb8f9edafecea69d8
marenhkj <maren.kjensli@vy.no>
2026-05-21 14:20:04 +0200

    add ErrorSummary component (#2236)
    ## Background
    
    ErrorSummary component that can be used to show user-triggered errors,
    for example in forms.
    See documentation and test component here:
    https://pr-2236.test.design.vy.no/spor/components/errorsummary

---
+++ packages/spor-react/src/theme/slot-recipes/error-summary.ts
@@ -0,0 +1,32 @@
+import { defineSlotRecipe } from "@chakra-ui/react";
+
+import { errorSummaryAnatomy } from "./anatomy";
+
+export const errorSummarySlotRecipe = defineSlotRecipe({
+  slots: errorSummaryAnatomy.keys(),
+  className: "spor-error-summary",
+  base: {
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      padding: [3, 5],
+      backgroundColor: "surface.critical",
+      borderRadius: "md",
+    },
+    heading: {
+      fontSize: "md",
+      fontWeight: "bold",
+      color: "text.critical",
+      gap: [1, 1.5],
+      alignItems: "center",
+    },
+    list: {
+      alignItems: "flex-start",
+      paddingLeft: [4, 5],
+    },
+    item: {
+      color: "text.critical.subtle",
+      marginTop: [2, 1.5],
+    },
+  },
+});

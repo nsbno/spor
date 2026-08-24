ff288809b9902ca73437bb5e4903025129e35890
marenhkj <maren.kjensli@vy.no>
2026-08-24 13:22:46 +0200

    make children of radiocard take 100% width (#2370)
    ## Background
    
    It was not possible for the children of a radiocard to take 100% width.

--- packages/spor-react/src/layout/RadioCard.tsx
+++ packages/spor-react/src/layout/RadioCard.tsx
@@ -65,7 +65,9 @@ export const RadioCard = ({
       />
       <ChakraRadioCard.ItemControl id={itemControlId} aria-hidden>
         {showIndicator && <ChakraRadioCard.ItemIndicator />}
-        <Flex direction="column">{children}</Flex>
+        <Flex direction="column" width="100%">
+          {children}
+        </Flex>
       </ChakraRadioCard.ItemControl>
     </ChakraRadioCard.Item>
   );
